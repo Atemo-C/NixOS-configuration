65efa36248455b0e2126ee876d5161425ae43022
Atemo-C <c.roux1993@gmail.com>
2024-06-16 17:51:25 +0200

    Fixed a typo.

--- System/State-version.nix
+++ System/State-version.nix
@@ -2,6 +2,6 @@
 
 	# Which version of NixOS was installed on this computer. Important to read more at the link below.
 	# https://search.nixos.org/options?channel=24.05&show=system.stateVersion
-	system.stateversion = "24.05";
+	system.stateVersion = "24.05";
 
 }
