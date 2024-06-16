b74675391d2208a28dad11989cabdd97100ddc6a
Atemo-C <c.roux1993@gmail.com>
2024-06-16 18:33:52 +0200

    Updated the `NetworkManager.nix` module.

--- Networking/NetworkManager.nix
+++ Networking/NetworkManager.nix
@@ -1,7 +1,13 @@
-{ config, ... }: {
+{ config, pkgs, ... }: {
+
+	# Graphical applet to configure the network.
+	environment.systemPackages = with pkgs; [ networkmanagerapplet ];
 
 	# Whether to use NetworkManager to manage network interfaces.
 	# https://search.nixos.org/options?channel=24.05&show=networking.networkmanager.enable
 	networking.networkmanager.enable = true;
+
+	# Disable NetworkManager's "wait-online" service.
+	systemd.services.NetworkManager-wait-online.enable = false;
 
 }
