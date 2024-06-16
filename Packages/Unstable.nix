0cd1018c75cd44ef9d14daa65572045014c8e7e2
Atemo-C <c.roux1993@gmail.com>
2024-06-16 15:01:41 +0200

    Modified how the `Unstable.nix` module works.
    This should prevent the need to run commands before the module works properly.

--- Packages/Unstable.nix
+++ Packages/Unstable.nix
@@ -1,14 +1,16 @@
 { config, pkgs, ... }: {
 
-	# Allow the use of unstable packages by using `pkgs.unstable.package-name`.
+	# Allow the use of packages from the unstable branch by using `pkgs.unstable.package-name`.
 	# https://search.nixos.org/options?channel=24.05&show=nixpkgs.config
-	#
-	# Before this can be used, one must run the following commands once:
-	#   sudo nix-channel --add https://nixos.org/channels/nixos-untsable nixos-unstable
-	#   sudo nix-channel --update
-	nixpkgs.config.packageOverrides = pkgs: {
-		unstable = import <nixos-unstable> {
-			config = config.nixpkgs.config;
+	let
+		NixOS-unstable = fetchTarball https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz;
+	in {
+		nixpkgs.config = {
+			packageOverrides = pkgs: {
+				unstable = import NixOS-unstable {
+					config = config.nixpkgs.config;
+				};
+			};
 		};
 	};
 
