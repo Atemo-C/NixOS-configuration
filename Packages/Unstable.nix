8d2ed35555bc7775a09938b6375494460166824f
Atemo-C <c.roux1993@gmail.com>
2024-06-16 16:45:43 +0200

    The `Unstable.nix` may now work.
    Maybe. Perhaps.

--- Packages/Unstable.nix
+++ Packages/Unstable.nix
@@ -1,17 +1,15 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, ... }:
 
-	# Allow the use of packages from the unstable branch by using `pkgs.unstable.package-name`.
-	# https://search.nixos.org/options?channel=24.05&show=nixpkgs.config
-	let
-		NixOS-unstable = fetchTarball https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz;
-	in {
-		nixpkgs.config = {
-			packageOverrides = pkgs: {
-				unstable = import NixOS-unstable {
-					config = config.nixpkgs.config;
-				};
+# Allow the use of packages from the unstable branch by using `pkgs.unstable.package-name`.
+# https://search.nixos.org/options?channel=24.05&show=nixpkgs.config
+let
+	NixOS-unstable = fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz";
+in {
+	nixpkgs.config = {
+		packageOverrides = pkgs: {
+			unstable = import NixOS-unstable {
+				config = config.nixpkgs.config;
 			};
 		};
 	};
-
 }
