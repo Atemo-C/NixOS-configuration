6e5d40a04c39e8588f8cb152434a6ddd6a42ca55
Atemo-C <c.roux1993@gmail.com>
2024-06-16 17:57:40 +0200

    Fixed errors in the `Groups.nix` module.

--- User/Groups.nix
+++ User/Groups.nix
@@ -1,19 +1,25 @@
 { config, ... }: {
 
-	# The user's auxilary groups.
-	# https://search.nixos.org/options?channel=24.05&show=users.users.%3Cname%3E.extraGroups
-	users.users.username.extraGroups = [
-		"disk"
-		"input"
-		"lp"
-		"networkmanager"
-		"plugdev"
-		"renderer"
-		"scanner"
-		"storage"
-		"users"
-		"video"
-		"wheel"
-	];
+	users.users.username = {
+
+		# The user's primary group.
+		# https://search.nixos.org/options?channel=24.05&show=users.users.%3Cname%3E.group
+		group = "users";
+
+		# The user's auxilary groups.
+		# https://search.nixos.org/options?channel=24.05&show=users.users.%3Cname%3E.extraGroups
+		extraGroups = [
+			"disk"
+			"input"
+			"lp"
+			"networkmanager"
+			"plugdev"
+			"renderer"
+			"scanner"
+			"storage"
+			"video"
+			"wheel"
+		];
+	};
 
 }
