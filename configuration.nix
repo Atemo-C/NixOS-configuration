286bc0898bce752388632915f6b94c0530b051dc
Atemo-C <c.roux1993@gmail.com>
2024-06-16 13:50:19 +0200

    Created the `GPU` directory and `AMD.nix` module.

--- configuration.nix
+++ configuration.nix
@@ -8,6 +8,10 @@
 		./Boot/Security.nix
 		./Boot/SystemD-boot.nix
 		./Boot/Timeout.nix
+
+		./Input/Drawing/OpenTabletDriver.nix
+		./Input/Keyboard/Layout.nix
+		./Input/Keyboard/ZSA.nix
 	];
 
 }
