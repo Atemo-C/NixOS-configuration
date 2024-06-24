7b57841536b7137dd00e27fb7f041c2443c24325
Atemo-C <c.roux1993@gmail.com>
2024-06-24 18:48:48 +0200

    Icons now come from `kdePackages.breeze-icons`.

--- Theme/Icons.nix
+++ Theme/Icons.nix
@@ -3,7 +3,7 @@
 	home-manager.users.${config.Custom.Name}.gtk.iconTheme = {
 		# Package providing the icon theme.
 		# https://nix-community.github.io/home-manager/options.xhtml#opt-gtk.iconTheme.package
-		package = pkgs.libsForQt5.breeze-icons;
+		package = pkgs.kdePackages.breeze-icons;
 
 		# The name of the icon theme within the package.
 		# https://nix-community.github.io/home-manager/options.xhtml#opt-gtk.iconTheme.name
