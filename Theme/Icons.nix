d7717bb2483ff3daac8c706490e3231f1a87f036
Atemo-C <c.roux1993@gmail.com>
2024-06-28 13:29:02 +0200

    Now using Flat Remix icon theme instead of Breeze.
    This is to fix sizing issues. Getting Breeze to fully work outside of a KDE Plasma environment seems to be much more of a pain than I initially thought.

--- Theme/Icons.nix
+++ Theme/Icons.nix
@@ -3,11 +3,13 @@
 	home-manager.users.${config.Custom.Name}.gtk.iconTheme = {
 		# Package providing the icon theme.
 		# https://nix-community.github.io/home-manager/options.xhtml#opt-gtk.iconTheme.package
-		package = pkgs.kdePackages.breeze-icons;
+#		package = pkgs.kdePackages.breeze-icons;
+		package = pkgs.flat-remix-icon-theme;
 
 		# The name of the icon theme within the package.
 		# https://nix-community.github.io/home-manager/options.xhtml#opt-gtk.iconTheme.name
-		name = "breeze-dark";
+#		name = "breeze-dark";
+		name = "Flat-Remix-Blue-Dark";
 	};
 
 }
