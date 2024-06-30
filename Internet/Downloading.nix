5502a71e2500b7257215d696072dbfa8c3aa70a5
Atemo-C <c.roux1993@gmail.com>
2024-06-30 15:59:25 +0200

    `yt-dlp` is now configured within Home Manager.

--- Internet/Downloading.nix
+++ Internet/Downloading.nix
@@ -6,9 +6,22 @@
 
 		# Torrent manager.
 		qbittorrent
-
-		# Audio/Video downloader.
-		unstable.yt-dlp
 	];
+
+	# yt-dlp audio-video downloader.
+	home-manager.users.${config.Custom.Name}.programs.yt-dlp = {
+		# Whether to enable yt-dlp.
+		# https://nix-community.github.io/home-manager/options.xhtml#opt-programs.yt-dlp.enable
+		enable = true;
+
+		# Package providing yt-dlp.
+		# https://nix-community.github.io/home-manager/options.xhtml#opt-programs.yt-dlp.package
+		package = pkgs.unstable.yt-dlp;
+
+		# Configuration written to $XDG_CONFIG_HOME/yt-dlp/config.
+		# https://nix-community.github.io/home-manager/options.xhtml#opt-programs.yt-dlp.settings
+		# https://github.com/yt-dlp/yt-dlp#configuration
+		settings.output = ''"%(title)s.%(ext)s"'';
+	};
 
 }
