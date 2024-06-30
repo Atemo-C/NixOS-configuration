d15ae3b4e11f0ba35f7da4e5ac565acb0049dcc0
Atemo-C <c.roux1993@gmail.com>
2024-06-30 16:59:44 +0200

    `micro` is now configured in nix.

--- Text/Editor.nix
+++ Text/Editor.nix
@@ -1,11 +1,31 @@
 { config, pkgs, ... }: {
 
 	environment = {
-		# Micro text editor.
-		systemPackages = with pkgs; [ micro ];
-
 		# Set Micro as the default text editor.
 		variables = { EDITOR = "micro"; };
+	};
+
+	home-manager.users.${config.Custom.Name}.programs.micro = {
+		# Whether to enable micro, a terminal-based text editor.
+		# https://nix-community.github.io/home-manager/options.xhtml#opt-programs.micro.enable
+		enable = true;
+
+		# Configuration written to $XDG_CONFIG_HOME/micro/settings.json.
+		# https://nix-community.github.io/home-manager/options.xhtml#opt-programs.micro.settings
+		# https://github.com/zyedidia/micro/blob/master/runtime/help/options.md
+		settings = {
+			"*.conf" = { filetype = "shell"; };
+			"*.dirs" = { filetype = "shell"; };
+			"*.jsonc" = { filetype = "json"; };
+			"*.sav" = { filetype = "nix"; };
+			colorcolumn = 120;
+			colorscheme = "Atemos-colours";
+			relativeruler = true;
+			rmtrailingws = true;
+			softwrap = true;
+			tabhighlight = true;
+			wordwrap = true;
+		};
 	};
 
 	# Disabling the nano text editor enabled by default.
