5292699b195e5b5cbd70973796531d4c67eff238
Atemo-C <c.roux1993@gmail.com>
2024-07-15 18:39:46 +0200

    Added the `lofi-asian` shell abbreviation.

--- User/Shell.nix
+++ User/Shell.nix
@@ -32,6 +32,7 @@
 			upgrade-now = "sudo nixos-rebuild switch --upgrade";
 
 			# Online music abbreviations.
+			lofi-asian = ''mpv --no-video "https://www.youtube.com/watch?v=Na0w3Mz46GA"'';
 			lofi-escape = ''mpv --no-video "https://www.youtube.com/watch?v=S_MOd40zlYU"'';
 			lofi-medieval = ''mpv --no-video "https://www.youtube.com/watch?v=_uMuuHk_KkQ"'';
 			lofi-piano = ''mpv --no-video "https://www.youtube.com/watch?v=nWjC1RnLYbM"'';
