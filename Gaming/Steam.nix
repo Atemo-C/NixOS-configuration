299d051f15eb88f1b4dc875e45570f30cb362954
Atemo-C <c.roux1993@gmail.com>
2024-07-25 22:05:44 +0200

    Fixed missing `remotePlay.`

--- Gaming/Steam.nix
+++ Gaming/Steam.nix
@@ -7,7 +7,7 @@
 
 		# Open ports in the firewall for Steam Remote Play.
 		# https://search.nixos.org/options?channel=24.05&show=programs.steam.remotePlay.openFirewall
-		openFirewall = true;
+		remotePlay.openFirewall = true;
 	};
 
 }
