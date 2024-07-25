7f10dbb292bea15899d4b284a5a6f5f8e7475ea4
Atemo-C <c.roux1993@gmail.com>
2024-07-25 22:04:38 +0200

    Opened firewall ports for Steam Remote Play.

--- Gaming/Steam.nix
+++ Gaming/Steam.nix
@@ -4,6 +4,10 @@
 		# Whether to enable Steam.
 		# https://search.nixos.org/options?channel=24.05&show=programs.steam.enable
 		enable = true;
+
+		# Open ports in the firewall for Steam Remote Play.
+		# https://search.nixos.org/options?channel=24.05&show=programs.steam.remotePlay.openFirewall
+		openFirewall = true;
 	};
 
 }
