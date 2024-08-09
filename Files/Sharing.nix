edddd213a0a8fd2d42215b216c78a0d2e1677a7f
Atemo-C <c.roux1993@gmail.com>
2024-08-09 16:40:59 +0200

    Opened relevant ports for `Warpinator`.

--- Files/Sharing.nix
+++ Files/Sharing.nix
@@ -1,6 +1,12 @@
-{ pkgs, ... }: {
+{ config, pkgs, ... }: {
 
 	# Linux Mint's Warpinator local file sharing utility.
 	environment.systemPackages = with pkgs.unstable; [ warpinator ];
+
+	# Firewall port to open for firesharing.
+	networking.firewall.allowedTCPPorts = [
+		42000 # Incoming port for transfers.
+		42001 # Incoming port for registration.
+	];
 
 }
