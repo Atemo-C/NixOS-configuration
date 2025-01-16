375fd793ca3727d74405b262478dfaf5ba9765b7
Atemo-C <c.roux1993@gmail.com>
2025-01-16 21:07:27 +0100

    Region screenshots should no longer have a black border.

--- Scripts/Screenshots/region-copy.sh
+++ Scripts/Screenshots/region-copy.sh
@@ -2,4 +2,4 @@
 
 set -euo pipefail
 
-grim -g "$(slurp)" - | wl-copy && notify-send -t 1500 "Screenshot copied."
+grim -g "$(slurp -d -w 0)" - | wl-copy && notify-send -t 1500 "Screenshot copied."
--- Scripts/Screenshots/region-save.sh
+++ Scripts/Screenshots/region-save.sh
@@ -3,6 +3,6 @@
 set -euo pipefail
 
 IMAGE=$(date +'Screenshot_%d-%m-%Y_%H-%M-%S.jpg') && \
-	grim -t jpeg -q 100 -g "$(slurp)" && \
+	grim -t jpeg -q 100 -g "$(slurp -d -w 0)" && \
 	jpegoptim -sv ~/Images/Screenshots/"$IMAGE" && \
 	notify-send "Screenshot saved."
