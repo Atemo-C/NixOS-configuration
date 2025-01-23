1a047e7d5e138eb6d365272a3aecf3ff86bb60cb
Atemo-C <c.roux1993@gmail.com>
2025-01-23 17:53:10 +0100

    Galculator is back.

--- Programs/Accessories.nix
+++ Programs/Accessories.nix
@@ -1,10 +1,10 @@
 { pkgs, ... }: { environment.systemPackages = [
 
+	# GTK algebraic and RPN calculator.
+	pkgs.galculator
+
 	# A simple and elegant clock application from GNOME.
 	pkgs.gnome-clocks
-
-	# Fully-featured minimalist configurable calculator.
-	pkgs.mini-calc
 
 	# Offline password manager with many features.
 	pkgs.keepassxc
