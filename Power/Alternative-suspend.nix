681c028770a4da53a6eb547ad3ecff254576d91f
Atemo-C <c.roux1993@gmail.com>
2025-01-28 22:25:18 +0100

    Okay for real this time.

--- Power/Alternative-suspend.nix
+++ Power/Alternative-suspend.nix
@@ -1,12 +1,12 @@
 { config, lib, pkgs, ... }: { systemd.services = {
 
 	# Alternative suspend command.
-	sleep.serviceConfig.ExecStart = lib.mkForce "${pkgs.pm-utils}/bin/pm-suspend";
+	sleep.serviceConfig.ExecStart = lib.mkForce "${pkgs.pmutils}/bin/pm-suspend";
 
 	# Alternative hibernate command.
-	hibernate.serviceConfig.ExecStart = lib.mkForce "${pkgs.pm-utils}/bin/pm-hibernate";
+	hibernate.serviceConfig.ExecStart = lib.mkForce "${pkgs.pmutils}/bin/pm-hibernate";
 
 	# Alternative hybrid sleep command.
-	hybrid-sleep.serviceConfig.ExecStart = lib.mkForce "${pkgs.pm-utils}/bin/pm-suspend-hybrid";
+	hybrid-sleep.serviceConfig.ExecStart = lib.mkForce "${pkgs.pmutils}/bin/pm-suspend-hybrid";
 
 }; }
