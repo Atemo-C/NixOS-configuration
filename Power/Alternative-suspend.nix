3a940b7938204027a53ce5116fedabff2e8cc97a
Atemo-C <c.roux1993@gmail.com>
2025-01-28 22:22:59 +0100

    Fixed wrong package name (woopsies).

--- Power/Alternative-suspend.nix
+++ Power/Alternative-suspend.nix
@@ -1,12 +1,12 @@
 { config, lib, pkgs, ... }: { systemd.services = {
 
 	# Alternative suspend command.
-	sleep.serviceConfig.ExecStart = lib.mkForce "${pkgs.pm-suspend}/bin/pm-suspend";
+	sleep.serviceConfig.ExecStart = lib.mkForce "${pkgs.pm-utils}/bin/pm-suspend";
 
 	# Alternative hibernate command.
-	hibernate.serviceConfig.ExecStart = lib.mkForce "${pkgs.pm-suspend}/bin/pm-hibernate";
+	hibernate.serviceConfig.ExecStart = lib.mkForce "${pkgs.pm-utils}/bin/pm-hibernate";
 
 	# Alternative hybrid sleep command.
-	hybrid-sleep.serviceConfig.ExecStart = lib.mkForce "${pkgs.pm-suspend}/bin/pm-suspend-hybrid";
+	hybrid-sleep.serviceConfig.ExecStart = lib.mkForce "${pkgs.pm-utils}/bin/pm-suspend-hybrid";
 
 }; }
