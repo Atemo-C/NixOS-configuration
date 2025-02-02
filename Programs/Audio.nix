6f0422d639d2e167fd30031dc9ee1b7b0bb39d63
Atemo-C <c.roux1993@gmail.com>
2025-02-02 21:52:59 +0100

    EasyEffects is now handled through Home Manager + Fixed Bluetooth applet.

--- Programs/Audio.nix
+++ Programs/Audio.nix
@@ -1,28 +1,37 @@
-{ pkgs, ... }: { environment = {
+{ pkgs, ... }: {
 
-	# Adds MIDI soundfonts (if present) to /run/current-system/sw.
-	# Why this does not seem to be done by default is beyond me.
-	pathsToLink = [ "/share/soundfonts" ];
+	environment = {
+		# Adds MIDI soundfonts (if present) to /run/current-system/sw.
+		# Why this does not seem to be done by default is beyond me.
+		pathsToLink = [ "/share/soundfonts" ];
 
-	# Audio packages to install.
-	systemPackages = [
-		# A lightweight and versatile audio player.
-		pkgs.audacious
+		# Audio packages to install.
+		systemPackages = [
+			# A lightweight and versatile audio player.
+			pkgs.audacious
 
-		# View and edit tags for various audio files.
-		pkgs.easytag
+			# View and edit tags for various audio files.
+			pkgs.easytag
 
-		# Sound editor with graphical UI.
-		pkgs.audacity
+			# Sound editor with graphical UI.
+			pkgs.audacity
 
-		# Audio effects for PipeWire applications.
-		pkgs.unstable.easyeffects
+			# Midi sound fonts.
+			pkgs.soundfont-fluid
+			pkgs.soundfont-arachno
+			pkgs.soundfont-ydp-grand
+			pkgs.soundfont-generaluser
+		];
+	};
 
-		# Midi sound fonts.
-		pkgs.soundfont-fluid
-		pkgs.soundfont-arachno
-		pkgs.soundfont-ydp-grand
-		pkgs.soundfont-generaluser
-	];
+	# Audio effects for PipeWire applications.
+	home-manager.users.users.${config.custom.name}.services.easyeffects = {
+		# Whether to enable EasyEffects.
+		# Necessitates programs.dconf.enable to be true.
+		enable = true;
 
-}; }
+		# The EasyEffects package to use.
+		package = pkgs.unstable.easyeffects
+	};
+
+}
