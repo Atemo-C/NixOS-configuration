bd91bf5f242304b8b4fe8da04b75b94c9076c3bf
Atemo-C <c.roux1993@gmail.com>
2025-02-10 18:03:48 +0100

    Fixed `xcb` thingy in Kurso De Esperanto's command.

--- Scripts/Programs.sh
+++ Scripts/Programs.sh
@@ -298,7 +298,7 @@ Program=$(
 	nohup xclicker > /dev/null 2>&1 &
 
 [ "$Program" = "  Kurso de Esperanto   Esperanto learning program" ] &&
-	nohup steam-run "$HOME/Programs/Kurso de Esperanto/kursokape" > /dev/null 2>&1 &
+	nohup QT_QPA_PLATFORM=xcb steam-run "$HOME/Programs/Kurso de Esperanto/kursokape" > /dev/null 2>&1 &
 
 [ "$Program" = "  KeePassXC            Password manager" ] &&
 	nohup keepassxc > /dev/null 2>&1 &
