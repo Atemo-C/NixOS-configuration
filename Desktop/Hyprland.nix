fa95eda26d3c2199653f15b8d48a2b7214cf2fc7
Atemo-C <c.roux1993@gmail.com>
2025-02-12 14:20:14 +0100

    Enabled tearing for all full-screen proton games.

--- Desktop/Hyprland.nix
+++ Desktop/Hyprland.nix
@@ -549,6 +549,9 @@
 			"tile, class: (sober)"
 			"prop immediate, class: (sober)"
 
+			# Full-screen proton-specific settings.
+			"prop immediate, fullscreen:1, class: (steam_proton), "
+
 			# GIMP-specific settings.
 			"center, title: (Open Image), class: .*gimp.*"
 			"size 800 600, title: (Open Image), class: .*gimp.*"
