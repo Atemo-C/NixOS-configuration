099c420de153d625964df592e38ad0a275ba4973
Atemo-C <c.roux1993@gmail.com>
2025-03-13 17:15:28 +0100

    Applied the `sudo` border to some polkit dialogs.

--- Desktop/Hyprland.nix
+++ Desktop/Hyprland.nix
@@ -505,6 +505,8 @@
 			"bordercolor rgb(ffc000), title: ssh.*, tag: term"
 			"bordercolor rgb(ffc000), title: sftp.*, tag: term"
 			"bordercolor rgb(FF0000), title: sudo.*, tag: term"
+			"bordercolor rgb(FF0000), title: (Authentication Required)"
+			"prop bordersize 2, title: (Authentication Required)"
 			"prop bordersize 2, title: sudo.*, tag: term"
 			"prop rounding 8, floating:1, class: (vesktop)"
 		];
