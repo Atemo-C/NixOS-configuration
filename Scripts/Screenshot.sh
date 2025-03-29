6085f019f67cb3cc82d84acb3794a5c2a4c7bc91
Atemo-C <c.roux1993@gmail.com>
2025-03-29 23:21:31 +0100

    The Screenshot script is now POSIX compliant and uses the DASH shell.

--- Scripts/Screenshot.sh
+++ Scripts/Screenshot.sh
@@ -1,27 +1,27 @@
-#!/usr/bin/env bash
+#!/bin/dash
 
 # Loop that defines the type of screenshot being taken.
 for type in "$@"; do
-	case $type in
+	case "$type" in
 		area) shottype="area";;
-		monitor) shottype="output";;
+		monitor) shottype="monitor";;
 		all) shottype="screen";;
-		--copy|-c|--save|-s) ;; # This is handled in the later part of this script.
-		*) ;; # If the argument is not recognized, it is ignored.
+		--copy|-c|--save|-s);; # This is handled in the later part of this script.
+		*);; # If the argument is not recognized, it is ignored.
 	esac
 done
 
 # Copies the screenshot to the clipboard with the --copy or -c argument.
-if [[ " $* " == *"--copy"* || " $* " == *"-c"* ]]; then
+if echo "$*" | grep -q -- "--copy" || echo "$*" | grep -q -- "-c"; then
 	grimblast --notify --freeze copy "$shottype"
 
 # Saves the screenshot to a file with the --save or -s argument.
-elif [[ " $* " == *"--save"* || " $* " == *"-s"* ]]; then
+elif echo "$*" | grep -q -- "--save" || echo "$*" | grep -q -- "-s"; then
 	# Sets the screenshot's name.
-	IMAGE=$(date +'Screenshot_%d-%m-%Y_%H-%M-%S') &&
+	IMAGE=$(date +'Screenshot_%d-%m-%Y_%H-%M-%S')
 
 	# Changes the active directory to save the screenshot into.
-	cd "$HOME/Images/Screenshots" &&
+	cd "$HOME/Images/Screenshots" || exit 1
 
 	# Take the screenshot.
 	grimblast --freeze copysave "$shottype" "$IMAGE".png &&
@@ -32,9 +32,9 @@ elif [[ " $* " == *"--save"* || " $* " == *"-s"* ]]; then
 	# Remove the original screenshot.
 	rm "$IMAGE".png
 
-# Errors out if no argument or an unvalid one is given.
+# Errors out if no argument or an invalid one is given.
 else
-	echo "
+echo "
  $(tput setaf 1)$(tput bold)Error$(tput sgr0): Missing or invalid argument.
  You can use the $(tput setaf 2)--copy$(tput sgr0) / $(tput setaf 2)-c$(tput sgr0) or the $(tput setaf 2)--save$(tput sgr0) / $(tput setaf 2)-s$(tput sgr0) argument, followed or preceeded by one of:
    $(tput setaf 6)area$(tput sgr0)    $(tput setaf 13)(to select an area or individual window)$(tput sgr0)
