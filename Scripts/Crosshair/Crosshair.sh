aa0adc4234bf6dad7a8ba2dbdf26d02dcb20b6fe
Atemo-C <c.roux1993@gmail.com>
2025-03-30 23:33:39 +0200

    All scripts are now consistent.

--- Scripts/Crosshair/Crosshair.sh
+++ Scripts/Crosshair/Crosshair.sh
@@ -1,29 +1,53 @@
 #!/bin/dash
 
-# Executeables shortcut.
+# Shortcut for binaries.
 SW="/run/current-system/sw/bin"
-HM="$HOME/.nix-profile/bin"
 
-# Check if the requiered dependencies are installed.
-[ -f "$HM/hyprland" ] || [ -f "$SW/hyprland" ] || { notify-send "This crosshair is made for Hyprland."; exit 1; }
-[ -f "$SW/feh" ] || [ -f "$HM/feh"] || { notify-send "feh is not installed."; exit 1; }
-[ -f "$SW/magick" ] || { notify-send "imagemagick is not installed."; exit 1; }
-[ -f "$SW/notify-send" ] || { echo "notify-send is not installed."; exit 1; }
+# Check if libnotify is installed.
+[ -f "$SW/notify-send" ] || {
+	echo "libnotify could not be found. It is needed to display graphical notifications.";
+	exit 1;
+}
 
-# Shortcut for the path of the image.
-Crosshair="/etc/nixos/Scripts/Crosshair/Crosshair.png"
+# Check if Hyprland is the active desktop.
+[ "$XDG_CURRENT_DESKTOP" = "Hyprland" ] || {
+	notify-send "This wallpaper utility can only be used with Hyprland." &
+	echo "This wallpaper utility can only be used with Hyprland.";
+	exit 1;
+}
 
-# Check if the determined crosshair file exists.
-[ -f "$Crosshair" ] || { notify-send "$Crosshair does not exist."; exit 1; }
+# Check if imagemagick is installed.
+[ -f "$SW/magick" ] || {
+	notify-send "imagemagick could not be found. It is needed to get the properties of the crosshair image." &
+	echo "imagemagick could not be found. It is needed to get the properties of the crosshair image.";
+	exit 1;
+}
 
-# Command shortcuts.
+# Check if feh is installed.
+[ -f "$SW/feh" ] || {
+	notify-send "feh could not be found. It is needed to display the crosshair image." &
+	echo "feh could not be found. It is needed to display the crosshair image.";
+	exit 1;
+}
+
+# Shortcut for the path of the crosshair image.
+Crosshair="/etc/nixos/Scripts/Crosshair/Crosshair.png";
+
+# Check if the crosshair file exists.
+[ -f "$Crosshair" ] || {
+	notify-send "$Crosshair does not exist." &
+	echo "$Crosshair does not exist.";
+	exit 1;
+}
+
+# Shortcuts for hyprctl dispatch commands.
 Setprop="hyprctl dispatch setprop class:feh"
 Dispatch="hyprctl dispatch"
 
 case "$1" in
-	# Starts the crosshair. There is no need to notify the user, as the crosshair will appear right on the screen.
+	# Start the crosshair.
 	--start|-s)
-		# Displays the crosshair image.
+		# Display the crosshair image.
 		feh "$Crosshair" || exit 1 &
 
 		# Wait for the image to be loaded.
@@ -31,29 +55,29 @@ case "$1" in
 			sleep 0.05
 		done
 
-		# Makes the image floating.
+		# Make the image floating.
 		$Dispatch togglefloating class:feh &&
 
 		# Gather the size of the crosshair image.
 		Width=$(identify -format "%w" "$Crosshair")
 		Height=$(identify -format "%h" "$Crosshair")
 
-		# Sets the appropriate size for the image.
+		# Set the appropriate size for the image.
 		$Setprop minsize "$Width" "$Height" &&
 		$Setprop maxsize "$Width" "$Height" &&
 		$Dispatch resizewindowpixel "$Width" "$Height", class:feh &&
 
-		# Removes the border from the image.
+		# Remove the border from the image.
 		$Setprop bordersize 0 &&
 
-		# Rounds the image so that it is not a square.
+		# Round the image so that it is not a square.
 		# It uses the width of the image to determine the rounding value.
 		$Setprop rounding "$Width" &&
 
-		# Removes shadows from the image so it is less distracting.
+		# Remove shadows from the image so it is less distracting.
 		$Setprop noshadow 1 &&
 
-		# Centers the image on the screen (it is assumed that the program used with it is fullscreen).
+		# Center the image on the screen (it is assumed that the program used with it is fullscreen).
 		$Dispatch centerwindow class:feh &&
 
 		# Prevent the image from getting focused.
@@ -63,12 +87,12 @@ case "$1" in
 		$Dispatch pin class:feh
 	;;
 
-	# Kills the crosshair. The && here is normal.
+	# Kill the crosshair.
 	--kill|-k)
 		trap 'pkill feh && notify-send "Crosshair killed."' EXIT
 	;;
 
-	# Errors out if no argument or an invalid one is given.
+	# Error out if no argument or an invalid one is given.
 	*)
 		echo "
  $(tput setaf 1)$(tput bold)Error$(tput sgr0): Missing or invalid argument.
