c67b794f12428ded6e3b6070cf21c5b26fdc4ee1
Atemo-C <c.roux1993@gmail.com>
2025-03-30 15:54:28 +0200

    The crosshair now starts basically as fast as possible.

--- Scripts/Crosshair/Crosshair.sh
+++ Scripts/Crosshair/Crosshair.sh
@@ -3,16 +3,38 @@
 case "$1" in
 	# Starts the crosshair.
 	--start|-s)
-		feh /etc/nixos/Scripts/Crosshair/Crosshair.png & sleep 0.1 &&
+		# Displays the crosshair image.
+		feh /etc/nixos/Scripts/Crosshair/Crosshair.png &
+
+		# Wait for the image to be loaded.
+		while ! pgrep -x ".feh-wrapped" > /dev/null; do
+			sleep 0.05
+		done
+
+		# Makes the image floating.
 		hyprctl dispatch togglefloating class:feh &&
+
+		# Sets the appropriate size for the image (adjust it to however big the crosshair is).
 		hyprctl dispatch setprop class:feh minsize 4 4 &&
 		hyprctl dispatch setprop class:feh maxsize 4 4 &&
 		hyprctl dispatch resizewindowpixel 4 4, class:feh &&
+
+		# Removes the border from the image.
 		hyprctl dispatch setprop class:feh bordersize 0 &&
+
+		# Rounds the image so that it is not a square.
 		hyprctl dispatch setprop class:feh rounding 3 &&
+
+		# Removes shadows from the image so it is less distracting.
 		hyprctl dispatch setprop class:feh noshadow 1 &&
+
+		# Centers the image on the screen (it is assumed that the program used with it is fullscreen).
 		hyprctl dispatch centerwindow class:feh &&
+
+		# Prevent the image from getting focused.
 		hyprctl dispatch setprop class:feh nofocus true &&
+
+		# Pin the image.
 		hyprctl dispatch pin class:feh
 	;;
 
