304ed4e50c7a8beff1ba8c48377619a0dfe5d154
Atemo-C <c.roux1993@gmail.com>
2025-03-30 05:34:57 +0200

    Cleaned up shebangs.

--- Scripts/Picker.sh
+++ Scripts/Picker.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/dash
+#!/bin/dash
 
 # Pick a color on the screen, save it to the clipboard, and send a notification.
 Color="$(hyprpicker -f hex --autocopy)" && notify-send -t 1500 "$Color copied to the clipboard"
