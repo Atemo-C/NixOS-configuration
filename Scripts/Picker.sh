00501f0ab05c75f9a5c47a92c26289fda38cef3e
Atemo-C <c.roux1993@gmail.com>
2025-03-30 19:52:28 +0200

    Fixed Hyprland detection and neated things up.

--- Scripts/Picker.sh
+++ Scripts/Picker.sh
@@ -1,12 +1,27 @@
 #!/bin/dash
 
-# Executeables shortcut.
+# Shortcut for binaries.
 SW="/run/current-system/sw/bin"
 
-# Check if the required dependencies are installed.
-[ -f "$HM/hyprland" ] || [ -f "$SW/hyprland" ] || { notify-send "This color picker is made for Hyprland."; exit 1; }
-[ -f "$SW/hyprpicker" ] || { notify-send "hyprpicker is not installed."; exit 1; }
-[ -f "$SW/notify-send" ] || { echo "notify-send is not installed."; exit 1; }
+# Check if libnotify is installed.
+[ -f "$SW/notify-send" ] || {
+	echo "libnotify could not be found. It is needed to display graphical notifications.";
+	exit 1;
+}
+
+# Check if Hyprland is the active desktop.
+[ "$XDG_CURRENT_DESKTOP" = "Hyprland" ] || {
+	notify-send "This color picking utility can only be used with Hyprland." &
+	echo "This wallpaper utility can only be used with Hyprland.";
+	exit 1;
+}
+
+# Check if hyprpicker is installed.
+[ -f "$SW/hyprpicker" ] || {
+	notify-send "hyprpicker could not be found. It is needed to pick a color on the screen." &
+	echo "hyprpicker could not be found. It is needed to pick a color on the screen.";
+	exit 1;
+}
 
 # Pick a color on the screen, save it to the clipboard, and send a notification.
 Color="$(hyprpicker -f hex --autocopy)" && notify-send -t 1500 "$Color copied to the clipboard"
