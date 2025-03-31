96f59ab3c7ef30620288a44a0a4defaba16359a2
Atemo-C <c.roux1993@gmail.com>
2025-03-31 22:06:33 +0200

    Name for the storage mounts is now pulled from `${config.custom.name}`.

--- Storage/Storage-mounts.nix
+++ Storage/Storage-mounts.nix
@@ -1,7 +1,7 @@
 { config, ... }: {
 
 	# Mount for the 480GB internal SATA SSD.
-#	fileSystems."/run/media/atemo/480GB-SSD-INT" = {
+#	fileSystems."/run/media/${config.custom.name}/480GB-SSD-INT" = {
 #		device = "/dev/disk/by-label/480GB-SSD-INT";
 #		fsType = "btrfs";
 #		options = [
@@ -14,7 +14,7 @@
 #	};
 
 	# Mount for the 500GB internal SATA HDD.
-#	fileSystems."/run/media/atemo/500GB-HDD-INT" = {
+#	fileSystems."/run/media/${config.custom.name}/500GB-HDD-INT" = {
 #		device = "/dev/disk/by-label/500GB-HDD-INT";
 #		fsType = "btrfs";
 #		options = [
@@ -27,7 +27,7 @@
 #	};
 
 	# Mount for the 1TB internal SATA HDD.
-#	fileSystems."/run/media/atemo/1TB-HDD-INT" = {
+#	fileSystems."/run/media/${config.custom.name}/1TB-HDD-INT" = {
 #		device = "/dev/disk/by-label/1TB-HDD-INT";
 #		fsType = "btrfs";
 #		options = [
@@ -40,7 +40,7 @@
 #	};
 
 	# Mount for the 1TB external SATA HDD.
-#	fileSystems."/run/media/atemo/1TB-HDD-EXT" = {
+#	fileSystems."/run/media/${config.custom.name}/1TB-HDD-EXT" = {
 #		device = "/dev/disk/by-label/1TB-HDD-EXT";
 #		fsType = "btrfs";
 #		options = [
@@ -53,7 +53,7 @@
 #	};
 
 	# Mount for the 160GB internal SATA HDD.
-#	fileSystems."/run/media/atemo/150GB-HDD-5400RPM-INT" = {
+#	fileSystems."/run/media/${config.custom.name}/150GB-HDD-5400RPM-INT" = {
 #		device = "/dev/disk/by-label/150GB-HDD-5400RPM-INT";
 #		fsType = "btrfs";
 #		options = [
