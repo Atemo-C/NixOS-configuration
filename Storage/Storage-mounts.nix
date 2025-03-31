1ccf45690011eb9cb4c702d86ed02d2c1fabe4c2
Atemo-C <c.roux1993@gmail.com>
2025-03-31 22:04:11 +0200

    Added some more of my disks.

--- Storage/Storage-mounts.nix
+++ Storage/Storage-mounts.nix
@@ -1,7 +1,7 @@
 { config, ... }: {
 
 	# Mount for the 480GB internal SATA SSD.
-#	fileSystems."/run/media/user-name/480GB-SSD-INT" = {
+#	fileSystems."/run/media/atemo/480GB-SSD-INT" = {
 #		device = "/dev/disk/by-label/480GB-SSD-INT";
 #		fsType = "btrfs";
 #		options = [
@@ -14,7 +14,7 @@
 #	};
 
 	# Mount for the 500GB internal SATA HDD.
-#	fileSystems."/run/media/user-name/500GB-HDD-INT" = {
+#	fileSystems."/run/media/atemo/500GB-HDD-INT" = {
 #		device = "/dev/disk/by-label/500GB-HDD-INT";
 #		fsType = "btrfs";
 #		options = [
@@ -27,16 +27,42 @@
 #	};
 
 	# Mount for the 1TB internal SATA HDD.
-	fileSystems."/run/media/user-name/1TB-HDD-INT" = {
-		device = "/dev/disk/by-label/1TB-HDD-INT";
-		fsType = "btrfs";
-		options = [
-			"defaults"
-			"rw"
-			"nofail"
-			"users"
-			"exec"
-		];
-	};
+#	fileSystems."/run/media/atemo/1TB-HDD-INT" = {
+#		device = "/dev/disk/by-label/1TB-HDD-INT";
+#		fsType = "btrfs";
+#		options = [
+#			"defaults"
+#			"rw"
+#			"nofail"
+#			"users"
+#			"exec"
+#		];
+#	};
+
+	# Mount for the 1TB external SATA HDD.
+#	fileSystems."/run/media/atemo/1TB-HDD-EXT" = {
+#		device = "/dev/disk/by-label/1TB-HDD-EXT";
+#		fsType = "btrfs";
+#		options = [
+#			"defaults"
+#			"rw"
+#			"nofail"
+#			"users"
+#			"exec"
+#		];
+#	};
+
+	# Mount for the 160GB internal SATA HDD.
+#	fileSystems."/run/media/atemo/150GB-HDD-5400RPM-INT" = {
+#		device = "/dev/disk/by-label/150GB-HDD-5400RPM-INT";
+#		fsType = "btrfs";
+#		options = [
+#			"defaults"
+#			"rw"
+#			"nofail"
+#			"users"
+#			"exec"
+#		];
+#	};
 
 }
