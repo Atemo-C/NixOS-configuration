4f720524f7a2692c576bb1e67b1cd29172f13844
Atemo-C <c.roux1993@gmail.com>
2025-04-07 19:15:25 +0200

    Re-written the `Power.sh` script to be more consistent with others.

--- Scripts/Power.sh
+++ Scripts/Power.sh
@@ -4,6 +4,48 @@
 SW="/run/current-system/sw/bin"
 HM="$HOME/.nix-profile/bin"
 
+# Define the "about" message.
+About="
+Power.sh
+
+This script allows you to execute power actions within the Hyprland Wayland compositor, using Tofi to display the menu.
+
+It allows for:
+• Turning off the active monitor(s).
+• Suspending to RAM.
+• Hibernating (suspending to disk).
+• Hybrid sleep (suspend to RAM and disk).
+• Reboot.
+• Reboot to the UEFI firmware (only appears when booted in EFI mode).
+• Power off.
+• Halt (an archaic way to turn the device off, and power has to be manually turned off afterwards).
+
+Note that if the \"Reboot to the UEFI firmware\" option does not appear despite your computer supporting it, you might have accidentally installed your operating system in BIOS mode.
+
+• When using the --about argument, this message is displayed.
+• When no argument is given, the power selection starts.
+• When an invalid argument is given, it is ignored.
+
+Credits:
+• tofi: https://github.com/philj56/tofi
+"
+
+# Check for arguments.
+for argument in "$@"; do
+	case "$argument" in
+		--about);;
+		*);;
+	esac
+done
+
+# Show the "about" message when the --about argument is given.
+echo "$*" | grep -q -- "--about" &&
+	echo "$About" &&
+	exit ||
+
+# When no or an invalid argument is given, check for relevant depedencies and show the power menu.
+
+# Check if libnotify is installed.
 # Check if libnotify is installed.
 [ -f "$SW/notify-send" ] || {
 	echo "libnotify could not be found. It is needed to display graphical notifications.";
@@ -24,44 +66,7 @@ HM="$HOME/.nix-profile/bin"
 	exit 1;
 }
 
-# Give an --about argument.
-for type in "$@"; do
-	case "$type" in
-		--about);;
-		*);;
-	esac
-done
-
-# Define the about message.
-About="
-Power.sh
-
-This scripts allows you to execute power actions within the Hyprland Wayland compositor, using Tofi to display the menu.
-
-It allows for:
-• Turning off the active monitor(s).
-• Suspending to RAM.
-• Hibernating (suspending to disk).
-• Hybrid sleep (suspend to RAM and disk).
-• Reboot.
-• Reboot to the UEFI firmware (only appears when booted in EFI mode).
-• Power off.
-• Halt (an archaic way to turn the device off, and power has to be manually turned off afterwards).
-
-When using the $(tput setaf 2)--about$(tput sgr0) argument, this message is displayed.
-
-Credits:
-• $(tput bold)tofi$(tput sgr0): $(tput setaf 4)https://github.com/philj56/tofi$(tput sgr0)
-"
-
-# Shows the about message
-if echo "$*" | grep -q -- "--about"; then
-	echo "$About" && exit
-
-# Shows the menu normally if no argument is given.
-else
-
-# Create the Confirmation list, used in prompts where user confirmation is good to have.
+# Create the Confirmation list, used in prompts where user confirmation is preferrable.
 Confirmations="
   No
   Yes
@@ -74,11 +79,11 @@ Height="292"
 # Gather the number of active monitors.
 Count=$(hyprctl monitors | grep -c "Monitor")
 
-# Choose the correct orthography, depending on the monitor count.
+# Choose the correct orthography, depending on the monitor count, and add the relevant one to the Options list.
 [ "$Count" -eq 1 ] && Options="󰍹  Turn off display"
 [ "$Count" -ge 2 ] && Options="󰍹  Turn off displays"
 
-# Add some of the other entries to the options.
+# Add some of the other entries to the Options list.
 Options="
 $Options
 󰒲  Suspend
@@ -86,14 +91,14 @@ $Options
 󱌂  Hybrid sleep
 󰜉  Reboot"
 
-# If booted in EFI mode, add the option to reboot into the UEFI firmware and change the size of the Tofi menu.
+# If booted in EFI mode, add the relevant option to the Options list, and change the size of the Tofi menu.
 [ -d "/sys/firmware/efi" ] && Options="
 $Options
   Reboot to UEFI firmware"
 	Width="236"
 	Height="327"
 
-# Add the remaining entries to the options.
+# Add the remaining entries to the Options list.
 Options="
 $Options
   Power off
@@ -109,172 +114,123 @@ Choice=$(printf '%s\n' "$Options" | tofi \
 	"$@"
 )
 
-# Defines the actions for every choice.
-case "$Choice" in
-	"󰍹  Turn off display")
-		sleep 0.2 && hyprctl dispatcher dpms off
-	;;
+# Define the actions for every choice.
+[ "$Choice" = "󰍹  Turn off display" ] || [ "$Choice" = "󰍹  Turn off displays" ] &&
+	sleep 0.2 &&
+	hyprctl dispatcher dpms off && exit
 
-	"󰍹  Turn off displays")
-		sleep 0.2 && hyprctl dispatcher dpms off
-	;;
+[ "$Choice" = "󰒲  Suspend" ] &&
+	Answer=$(printf '%s\n' "$Confirmations" | tofi \
+		--width 140 \
+		--height 124 \
+		--prompt-text "Suspend?"
+	)
 
-	"󰒲  Suspend")
-		Choice=$(printf '%s\n' "$Confirmations" | tofi \
-			--width 140 \
-			--height 124 \
-			--prompt-text "Suspend?"
-		)
-		case "$Choice" in
-			"  No")
-				exit
-			;;
-
-			"  Yes")
-				systemctl suspend
-			;;
-
-			"  Back")
-				nohup dash "/etc/nixos/Scripts/Power.sh" > /dev/null 2>&1 &
-			;;
-		esac
-	;;
-
-	"󰒲  Hibernate")
-		Choice=$(printf '%s\n' "$Confirmations" | tofi \
-			--width 156 \
-			--height 124 \
-			--prompt-text "Hibernate?"
-		)
-		case "$Choice" in
-			"  No")
-				exit
-			;;
-
-			"  Yes")
-				systemctl hibernate
-			;;
-
-			"  Back")
-				nohup dash "/etc/nixos/Scripts/Power.sh" > /dev/null 2>&1 &
-			;;
-		esac
-	;;
-
-	"󱌂  Hybrid sleep")
-		Choice=$(printf '%s\n' "$Confirmations" | tofi \
-			--width 180 \
-			--height 124 \
-			--prompt-text "Hybrid sleep?"
-		)
-		case "$Choice" in
-			"  No")
-				exit
-			;;
-
-			"  Yes")
-				systemctl hybrid-sleep
-			;;
-
-			"  Back")
-				nohup dash "/etc/nixos/Scripts/Power.sh" > /dev/null 2>&1 &
-			;;
-		esac
-	;;
-
-	"󰜉  Reboot")
-		Choice=$(printf '%s\n' "Confirmations" | tofi \
-			--width 131 \
-			--height 124 \
-			--prompt-text "Reboot?"
-		)
-		case "$Choice" in
-			"  No")
-				exit
-			;;
-
-			"  Yes")
-				systemctl reboot
-			;;
-
-			"  Back")
-				nohup dash "/etc/nixos/Scripts/Power.sh" > /dev/null 2>&1 &
-			;;
-		esac
-	;;
-
-	"  Reboot to UEFI firmware")
-		Choice=$(printf '%s\n' "$Confirmations" | tofi \
-			--width 268 \
-			--height 124 \
-			--prompt-text "Reboot to UEFI firmware?"
-		)
-		case "$Choice" in
-			"  No")
-				exit
-			;;
-
-			"  Yes")
-				systemctl reboot --firmware-setup
-			;;
-
-			"  Back")
-				nohup dash "/etc/nixos/Scripts/Power.sh" > /dev/null 2>&1 &
-			;;
-		esac
-	;;
-
-	"  Power off")
-		Choice=$(printf '%s\n' "$Confirmations" | tofi \
-			--width 156 \
-			--height 124 \
-			--prompt-text "Power off?"
-		)
-		case "$Choice" in
-			"  No")
-				exit
-			;;
-
-			"  Yes")
-				systemctl poweroff
-			;;
-
-			"  Back")
-				nohup dash "/etc/nixos/Scripts/Power.sh" > /dev/null 2>&1 &
-			;;
-		esac
-	;;
-
-	"󰺟  Halt")
-		Choice=$(printf '%s\n' "$Confirmations" | tofi \
-			--width 204 \
-			--height 124 \
-			--prompt-text "Halt the system?" & \
-			notify-send "Manually turn off power once the system has halted. This is archaic."
-		)
-		case "$Choice" in
-			"  No")
-				exit
-			;;
-
-			"  Yes")
-				pkexec systemctl halt
-			;;
-
-			"  Back")
-				nohup dash "/etc/nixos/Scripts/Power.sh" > /dev/null 2>&1 &
-			;;
-		esac
-	;;
-
-	" ‎")
+	[ "$Answer" = "  No" ] &&
 		exit
-	;;
 
-	"󰗼  Exit")
+	[ "$Answer" = "  Yes" ] &&
+		systemctl suspend && exit
+
+	[ "$Answer" = "  Back" ] &&
+		nohup dash "/etc/nixos/Scripts/Power.sh" > /dev/null 2>&1 &
+
+[ "$Choice" = "󰒲  Hibernate" ] &&
+	Answer=$(printf '%s\n' "$Confirmations" | tofi \
+		--width 156 \
+		--height 124 \
+		--prompt-text "Hibernate?"
+	)
+
+	[ "$Answer" = "  No" ] &&
 		exit
-	;;
 
-esac
+	[ "$Answer" = "  Yes" ] &&
+		systemctl hibernate && exit
 
-fi
+	[ "$Answer" = "  Back" ] &&
+		nohup dash "/etc/nixos/Scripts/Power.sh" > /dev/null 2>&1 &
+
+[ "$Choice" = "󱌂  Hybrid sleep" ] &&
+	Answer=$(printf '%s\n' "$Confirmations" | tofi \
+		--width 180 \
+		--height 124 \
+		--prompt-text "Hybrid sleep?"
+	)
+
+	[ "$Answer" = "  No" ] &&
+		exit
+
+	[ "$Answer" = "  Yes" ] &&
+		systemctl hybrid-sleep && exit
+
+	[ "$Answer" = "  Back" ] &&
+		nohup dash "/etc/nixos/Scripts/Power.sh" > /dev/null 2>&1 &
+
+[ "$Choice" = "󰜉  Reboot" ] &&
+	Answer=$(printf '%s\n' "$Confirmations" | tofi \
+		--width 131 \
+		--height 124 \
+		--prompt-text "Reboot?"
+	)
+
+	[ "$Answer" = "  No" ] &&
+		exit
+
+	[ "$Answer" = "  Yes" ] &&
+		systemctl reboot
+
+	[ "$Answer" = "  Back" ] &&
+		nohup dash "/etc/nixos/Scripts/Power.sh" > /dev/null 2>&1 &
+
+[ "$Choice" = "  Reboot to UEFI firmware" ] &&
+	Answer=$(printf '%s\n' "$Confirmations" | tofi \
+		--width 268 \
+		--height 124 \
+		--prompt-text "Reboot to UEFI firmware?"
+	)
+
+	[ "$Answer" = "  No" ] &&
+		exit
+
+	[ "$Answer" = "  Yes" ] &&
+		systemctl reboot --firmware-setup
+
+	[ "$Answer" = "  Back" ] &&
+		nohup dash "/etc/nixos/Scripts/Power.sh" > /dev/null 2>&1 &
+
+[ "$Choice" = "  Power off" ] &&
+	Answer=$(printf '%s\n' "$Confirmations" | tofi \
+		--width 156 \
+		--height 124 \
+		--prompt-text "Power off?"
+	)
+
+	[ "$Answer" = "  No" ] &&
+		exit
+
+	[ "$Answer" = "  Yes" ] &&
+		systemctl poweroff
+
+	[ "$Answer" = "  Back" ] &&
+		nohup dash "/etc/nixos/Scripts/Power.sh" > /dev/null 2>&1 &
+
+[ "$Choice" = "󰺟  Halt" ] &&
+	Answer=$(printf '%s\n' "$Confirmations" | tofi \
+		--width 156 \
+		--height 124 \
+		--prompt-text "Halt the system?" & \
+		notify-send "Manually turn off power once the system is halted. This is archaic."
+	)
+
+	[ "$Answer" = "  No" ] &&
+		exit
+
+	[ "$Answer" = "  Yes" ] &&
+		pkexec systemctl halt
+
+	[ "$Answer" = "  Back" ] &&
+		nohup dash "/etc/nixos/Scripts/Power.sh" > /dev/null 2>&1 &
+
+[ "$Choice" = "󰗼  Exit" ] &&
+	exit
