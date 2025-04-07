52dd1fc57e71b3831fbf686ce63851e7727ba75a
Atemo-C <c.roux1993@gmail.com>
2025-04-07 18:48:31 +0200

    Re-ordered the `Hyprpaper.sh` script.

--- Scripts/Hyprpaper.sh
+++ Scripts/Hyprpaper.sh
@@ -1,11 +1,43 @@
 #!/bin/dash
 
-# Shortcut for the path where hyprpaper.conf is located.
+# Shortcut for hyprpaper's configuration file location.
 CF="$HOME/.config/hypr"
 
 # Shortcuts for binaries.
 SW="/run/current-system/sw/bin"
 HM="$HOME/.nix-profile/bin"
+
+# Define the "about" message.
+About="
+Hyprpaper.sh
+
+This script allows you to graphically select a background image within the Hyprland Wayland compositor, using hyprpaper and zenity.
+
+If image thumbnails do not show, you might want to open the relevant directory(ies) in a graphical file manager to let the desired thumbnailer service create thumbnails for the images.
+
+• When using the --about argument, this message is displayed.
+• When no argument is given, the background image selection starts.
+• When an invalid argument is given, it is ignored.
+
+Credits:
+• hyprpaper: https://github.com/hyprwm/hyprpaper
+• zenity: https://gitlab.gnome.org/GNOME/zenity.git
+"
+
+# Check for arguments.
+for argument in "$@"; do
+	case "$argument" in
+		--about);;
+		*);;
+	esac
+done
+
+# Show the "about" message when the --about argument is given.
+echo "$*" | grep -q -- "--about" &&
+	echo "$About" &&
+	exit ||
+
+# When no or an invalid argument is given, check for relevant depedencies and select a background image.
 
 # Check if libnotify is installed.
 [ -f "$SW/notify-send" ] || {
@@ -34,73 +66,35 @@ HM="$HOME/.nix-profile/bin"
 	exit 1;
 }
 
-# Give an --about argument.
-for type in "$@"; do
-	case "$type" in
-		--about);;
-		*);;
-	esac
-done
-
-# Define the about message.
-About="
-Hyprpaper.sh
-
-This script allows you to graphically select a wallpaper within the Hyprland Wayland compositor, using hyprpaper and zenity to do so.
-
-If image thumbnails do not show, you might wish to open the relevant directories in a graphical manager to let the desired thumbnailer service create thumbnails for the images.
-
-When using the $(tput setaf 2)--about$(tput sgr0) argument, this message is displayed.
-
-Credits:
-• $(tput bold)hyprpaper$(tput sgr0) $(tput setaf 4)https://github.com/hyprwm/hyprpaper$(tput sgr0)
-• $(tput bold)zenity$(tput sgr0) $(tput setaf 4)https://gitlab.gnome.org/GNOME/zenity.git$(tput sgr0)
-"
-
-# Show the about message when the --about argument is given.
-if echo "$*" | grep -q -- "--about"; then
-	echo "$About" && exit
-
-# Pick a wallpaper normally if no argument is given.
-else
-
-# Check if Hyprpaper's configuration file exists.
-# If it does not, it creates it in the expected directory.
-[ -f "$CF/hyprpaper.conf" ] || {
-	mkdir "$CF" &&
-	touch "$CF/hyprpaper.conf";
-}
-
-# Show the file selection dialogue and defines the wallpaper used.
-Wallpaper=$(zenity \
+# Show the file selection dialogue and define the the background image to be used.
+Background=$(zenity \
 	--file-selection \
 	--filename="$HOME/Images/Backgrounds/ /" \
 	--file-filter="*.png *.jpg *.jpeg *.webp *.jxl" \
-	--title="Select a wallpaper."
+	--title="Select a background image."
 )
 
 case $? in
-	# Write the select wallpaper's path to hyprpaper's configuration file.
+	# Write the select background image's path to hyprpaper's configuration file.
 	0) echo \
-		"preload = $Wallpaper
-		wallpaper = ,$Wallpaper
-		splash = false
-		ipc = on" \
+"preload = $Background
+wallpaper = ,$Background
+splash = false
+ipc = on" \
 		> "$CF/hyprpaper.conf";
 
-		# Kills hyprpaper if it is already running.
+		# Kill hyprpaper if it is already running.
 		pkill --exact "hyprpaper" || true;
 
-		# Wait until hypraper is no longer running.
+		# Wait until hyprpaper is no longer running.
 		while pgrep -x "hyprpaper" > /dev/null; do
 			sleep 0.05
 		done
 
-		# Starts hyprpaper with the updated configuration.
+		# Start hyprpaper with the updated configuration.
 		nohup hyprpaper > /dev/null 2>&1 & ;;
 
-	# Notify the sure in case an error occurs.
-	1) notify-send "An error occured." & echo "An error occured.";;
+	# Notify the user if an error occurs.
+	1) notify-send "An unknown error occured, or the file selection was closed." &
+	echo "An unknown error occured, or the file selection was closed." ;;
 esac
-
-fi
