134c1df6d7c559592d5d01f0beffb09c58315c14
Atemo-C <c.roux1993@gmail.com>
2025-04-08 15:55:46 +0200

    Fixed wording (again).

--- Scripts/Picker.sh
+++ Scripts/Picker.sh
@@ -40,7 +40,7 @@ Credits:
 	# Check if Hyprland is the active Wayland compositor.
 	[ "$XDG_CURRENT_DESKTOP" = "Hyprland" ] || {
 		notify-send "This color picking utility can only be used with Hyprland."
-		echo "This wallpaper utility can only be used with Hyprland."
+		echo "This color picking utility can only be used with Hyprland."
 		exit 1
 	}
 
