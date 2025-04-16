0765109af2eb6ff1b71eacb82417e6bc50170cfb
Atemo-C <c.roux1993@gmail.com>
2025-04-16 18:26:23 +0200

    `Crosshair.sh` is now compliant with https://github.com/Atemo-C/NixOS-configuration/issues/2.

--- Scripts/Crosshair/Crosshair.sh
+++ Scripts/Crosshair/Crosshair.sh
@@ -1,77 +1,64 @@
 #!/bin/dash
 
-# Shortcut for binaries.
-SW="/run/current-system/sw/bin"
-
-# Shortcut for the path of the crosshair image.
-Crosshair="/etc/nixos/Scripts/Crosshair/Crosshair.png"
-
-# Shortcut for the crosshair's PID file.
-Crosshair_PID_file="/tmp/crosshair_pid.txt"
-
-# Shortcuts for hyprctl dispatch commands.
-Setprop="hyprctl dispatch setprop"
-Dispatch="hyprctl dispatch"
+# Set some text formatting shortcuts.
+err=$(tput bold)$(tput setaf 1)Error$(tput sgr0)
+arg=$(tput bold)$(tput setaf 2)
+exe=$(tput bold)$(tput setaf 3)
+ico=$(tput bold)$(tput setaf 6)
+web=$(tput setaf 4)
+dim=$(tput dim)
+c=$(tput sgr0)
 
 # Check if the number of arguments is greater than 1.
 [ "$#" -gt 1 ] && {
-	echo "$(tput bold)$(tput setaf 1)Error$(tput sgr0): Invalid number of arguments.
+	echo \
+		"${err}: Invalid number of arguments (${arg}$#${c}).\n" \
+		"\nSee the ${arg}--about${c} and the ${arg}--help${c} arguments.\n"
 
-See the $(tput setaf 2)$(tput bold)--about$(tput sgr0) and $(tput setaf 2)$(tput bold)--help$(tput sgr0) arguments.
-"
-	exit 1
+	exit
 }
 
 # Check for the --about argument.
 [ "$1" = "--about" ] && {
-	echo "$(tput bold)$(tput setaf 6)  $(tput setaf 2)Crosshair.sh$(tput sgr0)
-
-This script allows you to summon and stop a single, simple dot crosshair within the $(tput bold)$(tput setaf 3)Hyprland$(tput sgr0) Wayland compositor, using $(tput bold)$(tput setaf 6)feh$(tput sgr0) to display the image, with a bit of help from $(tput bold)$(tput setaf 6)imagemagick$(tput sgr0).
-
-$(tput dim)Notes: • It assumes use in applications such as video-gaming in fullscreen/borderless fullscreen-only.
-       • Only a single crosshair can be spawned.$(tput sgr0)
-
-• When using the $(tput setaf 2)$(tput bold)--about$(tput sgr0) argument, this message is displayed.
-• When using the $(tput setaf 2)$(tput bold)--help$(tput sgr0) argument, help about this script is displayed.
-• When using the $(tput setaf 2)$(tput bold)--start$(tput sgr0) argument, a crosshair is started on the currently focused monitor.
-• When using the $(tput setaf 2)$(tput bold)--stop$(tput sgr0) argument, the active crosshair is closed.
-
-Credits:
-• $(tput bold)$(tput setaf 3)feh$(tput sgr0): $(tput setaf 4)https://feh.finalrewind.org/$(tput sgr0)
-• $(tput bold)$(tput setaf 3)imagemagick$(tput sgr0): $(tput setaf 4)https://imagemagick.org/$(tput sgr0)
-"
+	echo \
+		"${ico}  ${arg}Crosshair.sh${c}\n" \
+		"\nThis script creates a single, simple dot crosshair in the middle of the currently focused monitor within the ${exe}Hyprland${c} Wayland compositor using ${exe}feh${c} and ${exe}imagemagick${c}.\n" \
+		"\n${dim}It assumes use in applications such as video-gaming in fullscreen/borderless fullscreen-only.${c}\n" \
+		"\n• When using the ${arg}--about${c} argument, this message is displayed." \
+		"\n• When using the ${arg}--help${c} argument, help about this script is displayed." \
+		"\n• When using the ${arg}--start${c} argument, a crosshair appears on the currently focused monitor." \
+		"\n• when using the ${arg}--stop${c} argument, the active crosshair is closed.\n" \
+		"\nCredits:" \
+		"\n• ${exe}feh${c}: ${web}https://feh.finalrewind.org/${c}" \
+		"\n• ${exe}imagemagick${c}: ${web}https://imagemagick.org/${c}\n"
 
 	exit
 }
 
 # Check for the --help argument.
 [ "$1" = "--help" ] && {
-	echo "$(tput bold)$(tput setaf 6)  $(tput setaf 2)Crosshair.sh$(tput sgr0)
-
-$(tput bold)[ Arguments ]$(tput sgr0)
-  $(tput setaf 2)$(tput bold)--about$(tput sgr0)
-  Display information about this script.
-
-  $(tput setaf 2)$(tput bold)--help$(tput sgr0)
-  Display this message.
-
-  $(tput setaf 2)$(tput bold)--start$(tput sgr0)
-  · Gets the image size with imagemagick.
-  · Checks if a crosshair is already running.
-    · If yes, then it is closed to be replaced with the new one.
-  · Opens the crosshair image with feh.
-  · Sets it as a floating window.
-  · Resizes the window with the image's size.
-  · Removes its borders.
-  · Removes its shadows.
-  · Rounds it with the width of the image.
-  · Centers it on the screen.
-  · Prevents it from being focused / stealing focus.
-  · Pins it on the active monitor.
-
-  $(tput setaf 2)$(tput bold)--stop$(tput sgr0)
-  Stops the active crosshair.
-"
+	echo \
+		"${ico}  ${arg}Crosshair.sh${c}\n" \
+		"\n${arg}--about${c}" \
+		"\nDisplay information about this script.\n" \
+		"\n${arg}--help${c}" \
+		"\nDisplay this message.\n" \
+		"\n${arg}--start${c}" \
+		"\n· Get the image size with imagemagick." \
+		"\n· Check if a crosshair is already running." \
+		"\n  · If yes, it is closed to be replaced with the new one." \
+		"\n· Open the crosshair image with ${exe}feh${c}." \
+		"\n· Set it as a floating window." \
+		"\n· Gather the image's properties with ${exe}imagemagick${c}." \
+		"\n· Resize the window with the image's size." \
+		"\n· Remove its borders." \
+		"\n· Remove its shadows." \
+		"\n· Round it with the width of the image." \
+		"\n· Center it on the screen." \
+		"\n· Prevent it from being focused / stealing focus." \
+		"\n· Pin it on the active monitor.\n" \
+		"\n${arg}--stop${c}" \
+		"\nStop the active crosshair.\n"
 
 	exit
 }
@@ -79,36 +66,39 @@ $(tput bold)[ Arguments ]$(tput sgr0)
 # Check for the --start argument.
 [ "$1" = "--start" ] && {
 	# Check if libnotify is installed.
-	[ -f "$SW/notify-send" ] || {
-		echo "libnotify could not be found. It is needed to display graphical notifications."
+	command -v notify-send || {
+		echo "${err}: libnotify could not be found. It is required to display graphical notifications."
 		exit 1
 	}
 
 	# Check if Hyprland is the active Wayland compositor.
 	[ "$XDG_CURRENT_DESKTOP" = "Hyprland" ] || {
-		notify-send "This crosshair can only be used with Hyprland."
-		echo "This crosshair can only be used with Hyprland."
+		notify-send "Error: This crosshair can only be used with the Hyprland Wayland compositor."
+		echo "${err}: This crosshair can only be used with the ${exe}Hyprland${c} Wayland compositor."
 		exit 1
 	}
 
 	# Check if imagemagick is installed.
-	[ -f "$SW/magick" ] || {
-		notify-send "imagemagick could not be found. It is needed to convert the saved screenshot to a WEBP image."
-		echo "imagemagick could not be found. It is needed to convert the saved screenshot to a WEBP image."
+	command -v magick || {
+		notify-send "Error: imagemagick could not be found. It is required to get the crosshair image's properties."
+		echo "${err}: ${exe}imagemagick${c} could not be found. It is required to get the crosshair image's properties."
 		exit 1
 	}
 
 	# Check if feh is installed.
-	[ -f "$SW/feh" ] || {
-		notify-send "feh could not be found. It is needed to display the crosshair image."
-		echo "feh could not be found. It is needed to display the crosshair image."
+	command -v feh || {
+		notify-send "Error: feh could not be found. It is required to display the crosshair image."
+		echo "${err}: ${exe}feh${c} could not be found. It is required to display the crosshair image."
 		exit 1
 	}
 
+	# Shortcut to the crosshair image.
+	Crosshair="/etc/nixos/Scripts/Crosshair/Crosshair.png"
+
 	# Check if the crosshair image exists.
 	[ -f "$Crosshair" ] || {
-		notify-send "$Crosshair could not be found."
-		echo "$Crosshair could not be found."
+		notify-send "Error: $Crosshair could not be found."
+		echo "${err}: ${web}$Crosshair${c} could not be found."
 		exit 1
 	}
 
@@ -116,7 +106,10 @@ $(tput bold)[ Arguments ]$(tput sgr0)
 	Width=$(identify -format "%w" "$Crosshair")
 	Height=$(identify -format "%h" "$Crosshair")
 
-	# Check if a crosshair is already running, and if so, try to close it.
+	# Shortcut to the crosshair's PID file.
+	Crosshair_PID_file="/tmp/crosshair_pid.txt"
+
+	# Check if a crosshair is already running. If so, close it.
 	[ -f "$Crosshair_PID_file" ] && {
 		Crosshair_PID=$(cat "$Crosshair_PID_file")
 		kill "$Crosshair_PID"
@@ -131,6 +124,10 @@ $(tput bold)[ Arguments ]$(tput sgr0)
 	while ! pgrep -x ".feh-wrapped" > /dev/null; do
 		sleep 0.05
 	done
+
+	# Set some hyprctl dispatch commands shortcuts.
+	Setprop="hyprctl dispatch setprop"
+	Dispatch="hyprctl dispatch"
 
 	# Make the crosshair window floating.
 	$Dispatch togglefloating pid:"$Crosshair_PID"
@@ -163,7 +160,10 @@ $(tput bold)[ Arguments ]$(tput sgr0)
 
 # Check for the --stop argument.
 [ "$1" = "--stop" ] && {
-	# Check for the last crosshair's PID and try to close it.
+	# Shortcut to the crosshair's PID file.
+	Crosshair_PID_file="/tmp/crosshair_pid.txt"
+
+	# Check for the last crosshair's PID, and close the associated crosshair.
 	[ -f "$Crosshair_PID_file" ] && {
 		Crosshair_PID=$(cat "$Crosshair_PID_file")
 		kill "$Crosshair_PID"
@@ -172,8 +172,8 @@ $(tput bold)[ Arguments ]$(tput sgr0)
 }
 
 # Error out if an invalid argument is given.
-echo "$(tput bold)$(tput setaf 1)Error$(tput sgr0): Invalid argument '$(tput setaf 1)$*$(tput sgr0)'.
+echo \
+	"${err}: Invalid argument '${arg}$*${c}'.\n" \
+	"\nSee the ${arg}--about${c} and the ${arg}--help${c} arguments.\n"
 
-See the $(tput setaf 2)$(tput bold)--about$(tput sgr0) and $(tput setaf 2)$(tput bold)--help$(tput sgr0) arguments.
-"
 exit 1
