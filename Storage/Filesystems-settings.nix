82803339663760ca39ab6c552dd67501f5262775
Atemo-C <c.roux1993@gmail.com>
2025-04-21 16:30:43 +0200

    Added some drive-specific smartd settings.

--- Storage/Filesystems-settings.nix
+++ Storage/Filesystems-settings.nix
@@ -7,4 +7,23 @@
 	# May not be necessarry on filesystems with a similar function enabled.
 	fstrim.enable = false;
 
+	# smartd options for specific drives.
+#	smartd.devices = [
+#		{
+#			# Prevent smartd from waking up this drive during standby.
+#			device = "/dev/disk/by-id/ata-ST3500418AS_5VMETGG9";
+#			options = "-n standby";
+#		}
+#		{
+#			# Prevent smartd from waking up these drives during standby.
+#			device = "/dev/disk/by-id/ata-WDC_WD1600BEVS-08VAT2_WD-WXP1A30R8683";
+#			options = "-n standby";
+#		}
+#		{
+#			# Prevent smartd from waking up this drive during standby.
+#			device = "/dev/disk/by-id/ata-TOSHIBA_DT01ACA100_448164BNS";
+#			options = "-n standby";
+#		}
+#	];
+
 }; }
