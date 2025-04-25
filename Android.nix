88cf467da92fd0dea2bc6a184ae12fd7f5358a03
Atemo-C <c.roux1993@gmail.com>
2025-04-25 18:22:56 +0200

    Overhauled `Android.nix`.

--- Android.nix
+++ Android.nix
@@ -1,23 +1,25 @@
-{ config, lib, pkgs, ... }: {
+{ config, pkgs, ... }: rec {
 
-	environment.systemPackages = with pkgs; [
-		# Reverse tethering over ADB for Android.
-		gnirehtet
-
-		# Cross-platform tool suite to flash firmware onto Samsung Galaxy devices.
-#		heimdall
-
-		# Implementation of Microsoft's Media Transfer Protocol.
-		libmtp
-
-		# Display and control Android devices over USB or TCP/IP
-		scrcpy
-	];
-
-	# Whether to enable the use of the Android Debug Bridge (ADB).
+	# Whether to allow the use of the Android Debug Bridge (ADB).
 	programs.adb.enable = true;
 
-	# Adds the current user to the ADB users group.
-	users.users.${config.custom.name}.extraGroups = [ "adbusers" ];
+	# If ADB is enabled, add the user to the `adbusers` group.
+	users.users.${userName}.extraGroups = ( if programs.adb.enable then [ "adbusers" ] else null );
+
+	# Various Android packages and utilities.
+	# Some packages will only install if ADB is enabled.
+	environment.systemPackages = [
+		# Reverse tethering over ADB.
+		( if programs.adb.enable then pkgs.gnirehtet else null )
+
+		# Display and control Android devices over USB or TCP/IP.
+		( if programs.adb.enable then pkgs.scrcpy else null )
+
+		# Implementation of Microsoft's Media Transfer Protocol.
+		pkgs.libmtp
+
+		# FUSE filesystem for MTP devices like Android devices.
+		pkgs.jmtpfs
+	];
 
 }
