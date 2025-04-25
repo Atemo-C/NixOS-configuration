5a849c6691c203485b3c44e93b3d512fdbd39799
Atemo-C <c.roux1993@gmail.com>
2025-04-25 18:59:54 +0200

    Moved more settings to `Nix-settings.nix`.

--- Nix-settings.nix
+++ Nix-settings.nix
@@ -4,7 +4,15 @@
 	# There is no need to change it post-installation, even when upgrading to a newer NixOS version.
 	system.stateVersion = "24.11";
 
-	# Increase the size of the download buffer for Nix.
-	nix.settings.download-buffer-size = 524288000;
+	nix.settings = {
+		# Whether to save disk space by hard-linking files in the store that have identical contents.
+		auto-optimise-store = true;
+
+		# Increase the size of the download buffer for Nix.
+		download-buffer-size = 524288000;
+
+		# Enable the nix-command feature.
+		nix.settings.experimental-features = [ "nix-command" ]
+	};
 
 }
