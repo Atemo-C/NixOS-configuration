c9a5a779389c90c2de5ba5ce9ff26e027c9bd72b
Atemo-C <c.roux1993@gmail.com>
2025-04-25 18:57:07 +0200

    ZSA utilities are now only installed if the relevant hardware setting is enabled.

--- Input/ZSA.nix
+++ Input/ZSA.nix
@@ -1,10 +1,10 @@
-{ config, pkgs, ... }: {
-
-	# GUI and CLI flashing utilities for ZSA keyboards.
-	environment.systemPackages = [ pkgs.keymapp pkgs.wally-cli ];
+{ config, pkgs, ... }: rec {
 
 	# Whether to enable udev rules for ZSA keyboards.
 	# Needed to flash firmware on the keyboards or use their live training in a Chromium-based web browser.
 	hardware.keyboard.zsa.enable = true;
+
+	# GUI and CLI flashing utilities for ZSA keyboards.
+	environment.systemPackages = [ ( if hardware.keyboard.zsa.enable then pkgs.keymapp ++ pkgs.wally-cli else null ) ];
 
 }
