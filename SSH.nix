5a30e8f82a4ee4b2cfe857e2e2528b82cdb353e9
Atemo-C <c.roux1993@gmail.com>
2025-04-25 18:35:45 +0200

    Simplified SSH port configuration, and updated the meme port.

--- SSH.nix
+++ SSH.nix
@@ -1,19 +1,16 @@
-{ config, ... }: {
-
-	# List of TCP ports on which incoming SSH connections are accepted.
-	networking.firewall.allowedTCPPorts = [ 1263 ];
+{ config, ... }: rec {
 
 	services.openssh = {
 		# Whether to enable the OpenSSH daemon.
 		enable = true;
 
 		# Which ports the SSH daemon should listen to.
-		ports = [ 1263 ];
+		ports = [ 3621 ];
 
 		# OpenSSH settings.
 		settings = {
-			# Login is allowed only for the listed users:
-			AllowUsers = [ "${config.custom.name}" ];
+			# Login is allowed only for the listed users.
+			AllowUsers = [ "${config.userName}" ];
 
 			# Whether the root user can login with SSH.
 			PermitRootLogin = "no";
@@ -22,5 +19,8 @@
 		# Whether to only start an instance for each incoming connection.
 		startWhenNeeded = true;
 	};
+
+	# List of TCP ports on which incoming SSH connections are accepted.
+	networking.firewall.allowedTCPPorts = services.openssh.ports;
 
 }
