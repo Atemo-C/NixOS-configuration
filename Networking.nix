1ccb77a8de2f4f2d80d533c986a78a3fd3f642a3
Atemo-C <c.roux1993@gmail.com>
2025-04-25 18:32:05 +0200

    `Networking.nix` updated, and the hostname is now defined here.

--- Networking.nix
+++ Networking.nix
@@ -1,24 +1,29 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, ... }: rec {
 
 	networking = {
-		# Specifies when the dhcpcd device will fork to background.
-		# background, any, ipv4, ipv6, both, if-carrier-up
+		# Specifices when the dhcpcd device will fork to background.
+		# Can be one of: background, any, ipv6, both, if-carrier-up.
+		# Select `background` for faster boot times.
 		dhcpcd.wait = "background";
 
-		# Whether to enable networking using NetworkManager.
+		# Name of the system.
+		# [a-z] [A-Z] [0-9] [ - ]
+		hostName = "COMPUTER-NAME";
+
+		# Whether to enable easy networking using NetworkManager.
 		networkmanager.enable = true;
 	};
 
-	# Whether to enable the NetworkManager control applet.
-	programs.nm-applet.enable = true;
+	# If NetworkManager is enabled, disable NetworkManager's "wait-online" service.
+	# This improves boot times.
+	systemd.services.NetworkManager-wait-online.enable = ( if networking.networkmanager.enable then false else null );
 
-	# If you prefer to start it manually, you can install the package alone.
-#	environment.systemPackages = [ pkgs.networkmanagerapplet ];
+	# If NetworkManager is enabled, add the user to the NetworkManager group to allow managing it.
+	users.users.${config.userName}.extraGroups = [
+		( if networking.networkmanager.enable then "networkmanager" else null )
+	];
 
-	# Whether to enable NetworkManager's "wait-online" service.
-	systemd.services.NetworkManager-wait-online.enable = false;
-
-	# Adds the user in the "networkmanager" group for managing NetworkManager settings.
-	users.users.${config.custom.name}.extraGroups = [ "networkmanager" ];
+	# If NetworkManager is enabled, enable the NetworkManager control applet in graphical environments.
+	programs.nm-applet.enable = networking.networkmanager.enable;
 
 }
