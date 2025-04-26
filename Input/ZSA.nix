5dea64153476640e6746a2e0d9743682691d7ede
Atemo-C <c.roux1993@gmail.com>
2025-04-26 02:07:50 +0200

    Fixed `ZSA.nix` having a wrong syntax.

--- Input/ZSA.nix
+++ Input/ZSA.nix
@@ -5,6 +5,9 @@
 	hardware.keyboard.zsa.enable = true;
 
 	# GUI and CLI flashing utilities for ZSA keyboards.
-	environment.systemPackages = [ ( if hardware.keyboard.zsa.enable then pkgs.keymapp ++ pkgs.wally-cli else null ) ];
+	environment.systemPackages = [
+		( if hardware.keyboard.zsa.enable then pkgs.keymapp else null )
+		( if hardware.keyboard.zsa.enable then pkgs.wally-cli else null )
+	];
 
 }
