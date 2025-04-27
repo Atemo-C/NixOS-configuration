fd40b31b27ada2bc1ee5e83fdc60e870e0fb1d3b
Atemo-C <c.roux1993@gmail.com>
2025-04-27 02:43:31 +0200

    Re-arranged the `Audio.nix` module.

--- Audio.nix
+++ Audio.nix
@@ -1,56 +1,13 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, ... }: rec {
 
-	environment = {
-		# Link MIDI sound fonts (if present) to `/run/current-system/sw/share/soundfonts`.
-		pathsToLink = [ "/share/soundfonts" ];
-
-		# Packages for controlling general audio settings, as well as some audio utilities, such as a music player.
-		systemPackages = [
-			# Utilitties for ALSA.
-			pkgs.alsa-utils
-
-			# A lightweight and versatile audio player.
-			pkgs.audacious pkgs.audacious-plugins
-
-			# View and edit tags for various audio files.
-			pkgs.easytag
-
-			# Graphical volume control.
-			pkgs.pavucontrol
-
-			# PipeWire patchbay.
-			pkgs.qpwgraph
-
-			# MIDI sound fonts.
-			pkgs.soundfont-fluid
-			pkgs.soundfont-arachno
-			pkgs.soundfont-ydp-grand
-			pkgs.soundfont-generaluser
-
-			# Graphical sound editor.
-			pkgs.tenacity
-		];
-	};
-
-	home-manager.users.${config.userName} = rec {
-		# Whether to enable audio effects using EasyEffects.
-		# Necessitates `programs.dconf.enable` to be `true`.
-		services.easyeffects.enable = true;
-
-		# If `services.easyeffects.enable` is `true`, add it to startup programs in the Hyprland Wayland compositor.
-		wayland.windowManager.hyprland.settings.exec-once = [
-			( if services.easyeffects.enable then "easyeffects --gapplication-service" else null )
-		];
-	};
-
-	# Whether to enable the RealtimeKit system service.
-	# It hands out realtime scheduling priority to user processes on demand.
-	# PipeWire uses this to acquire realtime priority.
-	security.rtkit.enable = true;
-
+	# Audio & media-related services configuration.
 	services = {
 		# PipeWire multimedia framework.
 		pipewire = {
+			# Whether to enable PipeWire.
+			enable = true;
+
+			# ALSA emulation under PipeWire.
 			alsa = {
 				# Whether to enable ALSA support.
 				enable = true;
@@ -59,19 +16,67 @@
 				support32Bit = true;
 			};
 
-			# Whether to enable PipeWire.
-			enable = true;
-
-			# Whether to enable JACK support.
+			# Whether to enable support for JACK audio emulation support.
 			jack.enable = true;
 
-			# Whether to enable PulseAudio support.
+			# Whether to enable PulseAudio audio emulation support.
 			pulse.enable = true;
 		};
 
-		# Whether to enable the playerctl daemon.
-		# Some desktop environments provide it out of the box.
-		playerctld.enable = true;
+		# Whether to enable the playerctld daemon for easy multimedia control.
+		playerctld.enable = services.pipewire.enable;
+	};
+
+	# Whether to enable the RealtimeKit system service.
+	# It hands out realtime scheduling priority to user processes on demand.
+	# PipeWire uses this to acquire realtime priority.
+	security.rtkit.enable = services.pipewire.enable;
+
+	environment = {
+		# Link MIDI soundfonts to `/run/current-system/sw/share/soundfonts` to make them easily accessible.
+		pathsToLink = [ "/share/soundfonts" ];
+
+		# Audio-related packages.
+		systemPackages = [
+			# Utilities for ALSA, the Advanced Linux Sound Architecture utils.
+			(if services.pipewire.alsa.enable then pkgs.alsa-utils else null)
+
+			# Lightweight and versatile audio player & useful plugins.
+			pkgs.audacious
+			pkgs.audacious-plugins
+
+			# View and edit tags for various audio files.
+			pkgs.easytag
+
+			# Graphical volume control.
+			(if services.pipewire.enable then pkgs.pavucontrol else null)
+
+			# Qt graph manager for PipeWire, similar to QjackCtl.
+			(if services.pipewire.enable then pkgs.qpwgraph else null)
+
+			# MIDI sound fonts.
+			pkgs.soundfont-fluid
+			pkgs.soundfont-arachno
+			pkgs.soundfont-ydp-grand
+			pkgs.soundfont-generaluser
+
+			# Sound editor with graphical UI.
+			pkgs.tenacity
+		];
+	};
+
+	home-manager.users.${config.userName} = rec {
+		# Whether to enable audio effects for PipeWire applications using EasyEffects.
+		# It necessitates Dconf to be enabled.
+		services.easyeffects.enable = true;
+
+		# Enable Dconf if EasyEffects is enabled.
+		programs.dconf.enable = mkDefault services.easyeffects.enable;
+
+		# If EasyEffects is enabled, it is added to startup programs in the Hyprland Wayland compositor.
+		wayland.windowManager.hyprland.settings.exec-once = [
+			(if services.easyeffects.enable then "easyeffects --gapplication-service" else null)
+		];
 	};
 
 }
