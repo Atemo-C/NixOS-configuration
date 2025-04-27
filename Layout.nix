3972a9937cfcb9e59cc88ebdf69416ba4e98ea23
Atemo-C <c.roux1993@gmail.com>
2025-04-27 02:46:20 +0200

    Made keyboard configuration less repetitive.
    And linked to some nice help. https://github.com/NixOS/nixpkgs/issues/254523 https://github.com/NixOS/nixpkgs/issues/286283

--- Layout.nix
+++ Layout.nix
@@ -1,24 +1,28 @@
-{ config, ... }: {
+{ config, ... }: rec {
 
-	# Keyboard layout in the Linux console (TTY).
-	console.keyMap = "us-acentos";
-
-	# Keyboard layout in the Hyprland Wayland compositor.
-	home-manager.users.${config.userName}.wayland.windowManager.hyprland.settings.input = {
+	# Keyboard layout configuration to use in graphical environments.
+	# To see a complete list: https://gist.github.com/jatcwang/ae3b7019f219b8cdc6798329108c9aee
+	# And why this list cannot be seen within NixOS:
+	# • https://github.com/NixOS/nixpkgs/issues/254523
+	# • https://github.com/NixOS/nixpkgs/issues/286283
+	services.xserver.xkb = {
 		# Keyboard layout to use.
-		kb_layout = "us";
+		layout = "us";
 
 		# Keyboard layout variant to use.
-		kb_variant = "intl";
+		variant = "intl"
 	};
 
-#	# Keyboard layout to use in other graphical environments.
-#	services.xserver.xkb = {
-#		# Keyboard layout to use.
-#		layout = "us";
-#
-#		# Keyboard layout variant to use.
-#		variant = "intl";
-#	};
+	# Keyboard layout configuration to use in the Hyprland Wayland compositor.
+	home-manager.users.${config.userName}.wayland.windowManager.hyprland.settings.input = {
+		# Keyboard layout to use.
+		kb_layout = services.xserver.xkb.layout;
 
-}
+		# Keyboard layout variant to use.
+		kb_variant = services.xserver.xkb.variant;
+	};
+
+	# Whether to use the virtual console (TTY) keymap from the xserver keyboard settings.
+	console.useXkbConfig = true;
+
+};
