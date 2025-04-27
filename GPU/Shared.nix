f41ab07850b85e4a294167419c4752b986ac5c60
Atemo-C <c.roux1993@gmail.com>
2025-04-28 00:38:17 +0200

    Minor wording changes and conditional enablement of 32bit GPU acceleration drivers.

--- GPU/Shared.nix
+++ GPU/Shared.nix
@@ -1,14 +1,14 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, ... }: rec {
 
 	hardware.graphics = {
-		# Whether to enable hardware accelerated graphics drivers.
+		# Enable hardware accelerated graphics drivers.
 		enable = true;
 
-		# Whether to also install 32-bit drivers for 32-bit applications.
-		enable32Bit = true;
+		# Also install 32-bit drivers for 32-bit applications.
+		enable32Bit = hardware.graphics.enable;
 
 		# Extra packages for hardware acceleration.
-		# There should be little to no conflicts even with multiple of them installed.
+		# There should be little to no conflicts with multiple of them installed.
 		extraPackages = [
 			pkgs.intel-media-driver    # VA-API for modern Intel GPUs.
 			pkgs.intel-vaapi-driver    # VA-API for older Intel GPUs using the i965 driver.
