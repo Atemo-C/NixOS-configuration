d598fbafa0375e505085f0bb3b1c64f85a942f54
Atemo-C <c.roux1993@gmail.com>
2025-04-27 02:42:43 +0200

    Minor wording and spacing changes.

--- Android.nix
+++ Android.nix
@@ -1,19 +1,18 @@
 { config, pkgs, ... }: rec {
 
-	# Whether to allow the use of the Android Debug Bridge (ADB).
+	# Whether to configure system to use Android Debug Bridge (ADB).
 	programs.adb.enable = true;
 
 	# If ADB is enabled, add the user to the `adbusers` group.
-	users.users.${config.userName}.extraGroups = ( if programs.adb.enable then [ "adbusers" ] else null );
+	users.users.${config.userName}.extraGroups = (if programs.adb.enable then [ "adbusers" ] else null);
 
-	# Various Android packages and utilities.
-	# Some packages will only install if ADB is enabled.
+	# Android-related packages.
 	environment.systemPackages = [
-		# Reverse tethering over ADB.
-		( if programs.adb.enable then pkgs.gnirehtet else null )
+		# Reverse tethering over ADB for Android.
+		(if programs.adb.enable then pkgs.gnirehtet else null)
 
 		# Display and control Android devices over USB or TCP/IP.
-		( if programs.adb.enable then pkgs.scrcpy else null )
+		(if programs.adb.enable then pkgs.scrcpy else null)
 
 		# Implementation of Microsoft's Media Transfer Protocol.
 		pkgs.libmtp
