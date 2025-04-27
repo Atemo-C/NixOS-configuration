ab722d7ee3df9ec73f072e0a18bfd2a10df347cd
Atemo-C <c.roux1993@gmail.com>
2025-04-28 01:07:38 +0200

    `tlp` is now commented by default.
    In my testing, most modern laptops fair well without needing it. However, on older ones, it is advisable to enable it, and as such, it is kept here.

--- Power/Power.nix
+++ Power/Power.nix
@@ -11,24 +11,24 @@
 	];
 
 	# Power-saving with TLP.
-	services.tlp = {
-		# Whether to enable the TLP power management daemon.
-		enable = true;
-
-		# Options passed to TLP.
-		# See https://linrunner.de/tlp for all supported options.
-		settings = {
-			CPU_SCALING_GOVERNOR_ON_AC = "performance";
-			CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-			CPU_ENERGY_PREF_POLICY_ON_AC = "performance";
-			CPU_ENERGY_PREF_POLICY_ON_BAT = "power";
-			CPU_MIN_PERF_ON_AC = 20;
-			CPU_MIN_PERF_ON_BAT = 0;
-			CPU_MAX_PERF_ON_AC = 100;
-			CPU_MAX_PERF_ON_BAT = 96;
+#	services.tlp = {
+#		# Enable the TLP power management daemon.
+#		enable = true;
+#
+#		# Options passed to TLP.
+#		# See https://linrunner.de/tlp for all supported options.
+#		settings = {
+#			CPU_SCALING_GOVERNOR_ON_AC = "performance";
+#			CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+#			CPU_ENERGY_PREF_POLICY_ON_AC = "performance";
+#			CPU_ENERGY_PREF_POLICY_ON_BAT = "power";
+#			CPU_MIN_PERF_ON_AC = 20;
+#			CPU_MIN_PERF_ON_BAT = 0;
+#			CPU_MAX_PERF_ON_AC = 100;
+#			CPU_MAX_PERF_ON_BAT = 96;
 #			START_CHARGE_THRESH_BAT0 = 25;
-			STOP_CHARGE_THRESH_BAT0 = 90;
-		};
-	};
+#			STOP_CHARGE_THRESH_BAT0 = 90;
+#		};
+#	};
 
 }
