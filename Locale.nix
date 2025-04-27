7d71aeb3a6b991b106fc570a60a38f24c41b8f2f
Atemo-C <c.roux1993@gmail.com>
2025-04-27 02:47:11 +0200

    Simplified things for me.

--- Locale.nix
+++ Locale.nix
@@ -4,15 +4,15 @@
 	defaultLocale = "en_US.UTF-8";
 
 	# Additional locale settings other than the language.
-	extraLocaleSettings = {
+	extraLocaleSettings = rec {
 		LC_ADDRESS = "fr_FR.UTF-8";
-		LC_IDENTIFICATION = "fr_FR.UTF-8";
-		LC_MEASUREMENT = "fr_FR.UTF-8";
-		LC_MONETARY = "fr_FR.UTF-8";
-		LC_NAME = "fr_FR.UTF-8";
-		LC_NUMERIC = "fr_FR.UTF-8";
-		LC_PAPER = "fr_FR.UTF-8";
-		LC_TELEPHONE = "fr_FR.UTF-8";
+		LC_IDENTIFICATION = LC_ADDRESS;
+		LC_MEASUREMENT = LC_ADDRESS;
+		LC_MONETARY = LC_ADDRESS;
+		LC_NAME = LC_ADDRESS;
+		LC_NUMERIC = LC_ADDRESS;
+		LC_PAPER = LC_ADDRESS;
+		LC_TELEPHONE = LC_ADDRESS;
 		LC_TIME = "en_GB.UTF-8";
 	};
 
