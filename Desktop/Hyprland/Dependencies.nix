7cbac3a744b519eef3501290ee76158b613cb977
Atemo-C <c.roux1993@gmail.com>
2025-04-28 00:20:17 +0200

    Tweaked wording and automatic enablement of options.

--- Desktop/Hyprland/Dependencies.nix
+++ Desktop/Hyprland/Dependencies.nix
@@ -1,24 +1,25 @@
 { config, pkgs, ... }: {
 
-	# Whether to enable Dconf, needed by some programs.
-	programs.dconf.enable = true;
+	# Enable Dconf.
+	programs.dconf.enable = config.home-manager.users.${config.userName}.wayland.windowManager.hyprland.enable;
 
 	# XDG Desktop Portals settings.
-xdg.portal = {
-			# List of packages that provide XDG Desktop Portal configuration.
-			configPackages = [
-				( if config.home-manager.users.${config.userName}.wayland.windowManager.hyprland.enable then
-					pkgs.xdg-desktop-portal-hyprland else null )
-				pkgs.xdg-desktop-portal-gtk
-			];
+	xdg.portal = {
+		# List of packages that provide XDG Desktop Portal configuration.
+		configPackages = [
+			# Hyprland Desktop Portal.
+			(if config.home-manager.users.${config.userName}.wayland.windowManager.hyprland.enable then
+				pkgs.xdg-desktop-portal-hyprland else null)
+
+			# GTK Desktop Portal.
+			pkgs.xdg-desktop-portal-gtk
+		];
 
 		# Additional portals to add to the path.
 		extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
 
-		# Whether to enable XDG Desktop integration.
-		# Enabled by most desktop environments.
-		# Required for Flatpaks to work.
-		enable = true;
+		# Enable XDG Desktop integration, a must on Wayland.
+		enable = config.home-manager.users.${config.userName}.wayland.windowManager.hyprland.enable;
 	};
 
 }
