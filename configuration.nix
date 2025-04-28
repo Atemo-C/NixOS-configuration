a7b42c6b7000012f4143a9c4d8b649723136b158
Atemo-C <c.roux1993@gmail.com>
2025-04-28 02:09:11 +0200

    Re-arranged `configuration.nix`.

--- configuration.nix
+++ configuration.nix
@@ -1,70 +1,61 @@
 { config, ... }: { imports = [
 
+	# Android utilities.
+	./Android.nix
+
+	# Audio configuration & utilities.
+	./Audio.nix
+
+	# Bluetooth configuration.
+#	./Bluetooth.nix
+
+	# Boot configuration.
+	./Boot.nix
+
+	# Hyprland desktop modules (main desktop).
+	./Desktop/Hyprland/Dependencies.nix # XDG Desktop Portals & Dconf.
+	./Desktop/Hyprland/Dunst.nix        # Desktop notifications.
+	./Desktop/Hyprland/Hyprland.nix     # Hyprland Wayland compositor.
+	./Desktop/Hyprland/Menus.nix        # Tofi menu & Zenity graphical dialogue tool.
+	./Desktop/Hyprland/Utilities.nix    # Wallpaper, keyring…
+	./Desktop/Hyprland/Waybar.nix       # Desktop bar.
+
+	# XFCE desktop modules (mostly here for debugging/rescue and VMs).
+#	./Desktop/XFCE/Panel.nix
+#	./Desktop/XFCE/XFCE.nix
+
+	# Font settings.
+	./Fonts.nix
+
+	# GPU modules.
+	# If `NVIDIA.nix` is enabled, a boot entry for 16XX> NVIDIA GPUs is added.
+	# Unfortunately, it is currently impossible to make a specialisation be the default entry in the booloader.
+	# See https://github.com/NixOS/nixpkgs/issues/146641
+	./GPU/Shared.nix
+	./GPU/NVIDIA.nix
+
 	# Automatically-generated hardware setting module.
 	./hardware-configuration.nix
 
 	# Additional modifications/overrides to `hardware-configuration.nix`.
 #	./Hardware-overrides.nix
 
-	# Temporary fixes.
-	./Temporary.nix
-
-	# Support and interaction with Android devices.
-	./Android.nix
-
-	# Audio settings and packages.
-	./Audio.nix
-
-	# Bluetooth settings.
-	./Bluetooth.nix
-
-	# Boot settings.
-	./Boot.nix
-
-	# Linux Console (TTY) settings.
-	./Console.nix
-
-	# Desktop components.
-	./Desktop/Dependencies.nix # XDG-DP & Dconf.
-	./Desktop/Dunst.nix        # Notifications.
-	./Desktop/Hyprland.nix     # Wayland compositor.
-	./Desktop/Menus.nix        # Menus for scripts, run launchers, and file selection.
-	./Desktop/Utilities.nix    # Wallpaper, keyring, polkit agent…
-	./Desktop/Waybar.nix       # Desktop bar.
-
-	# Time settings.
-	./Time.nix
-
-	# Font settings.
-	./Fonts.nix
-
-	# GPU modules.
-	# A boot entry for 16XX> NVIDIA GPUs is added when `NVIDIA.nix` is used.
-	# Unfortunately, it is not currently possible to make a specialisation the default entry in the bootloader.
-	# https://github.com/NixOS/nixpkgs/issues/146641
-	#
-	# In the `Shared.nix` module, you can configure which non-NVIDIA elements you want to be installed.
-	./GPU/Shared.nix
-	./GPU/NVIDIA.nix
-
 	# Input modules.
 	./Input/Fcitx5.nix           # Input method framework.
-	./Input/OpenTabletDriver.nix # For drawing tablets.
+	./Input/Keyboard-layout.nix  # Keyboard layout.
+#	./Input/OpenTabletDriver.nix # For drawing tablets.
 	./Input/Power-button.nix     # Actions upon power buttons press.
 	./Input/Utilities.nix        # Game controller tester, mouse refresh rate settings…
-	./Input/ZSA.nix              # For ZSA keyboards.
+#	./Input/ZSA.nix              # For ZSA keyboards.
 
 	# Linux Kernel settings.
 	./Kernel.nix
-
-	# Keyboard layout.
-	./Layout.nix
 
 	# System locale.
 	./Locale.nix
 
 	# Monitor/s (screen/s) settings.
-#	./Monitors.nix
+	./Monitors.nix
 
 	# Networking settings.
 	./Networking.nix
@@ -108,17 +99,23 @@
 	./Storage/Filesystem-settings.nix
 	./Storage/Filesystem-support.nix
 	./Storage/Optical-media.nix
-	./Storage/Storage-mounts.nix
+#	./Storage/Storage-mounts.nix
+
+	# Temporary fixes.
+	./Temporary.nix
 
 	# Theming.
+	./Theming/Terminal-colors.nix
 	./Theming/Program-themes.nix
 	./Theming/Icon-themes.nix
 	./Theming/Settings.nix
 
+	# Time settings.
+	./Time.nix
+
 	# User modules.
 	./User/Home-manager.nix
 	./User/Name.nix
-	./User/Name-module.nix
 	./User/Settings.nix
 	./User/Shell.nix
 
