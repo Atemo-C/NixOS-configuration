1ad1c9dd6eab7c78e497e36f8629ad76830b1235
Atemo-C <c.roux1993@gmail.com>
2025-04-28 02:54:59 +0200

    Woops, it wasn't actually getting installed.

--- Polkit.nix
+++ Polkit.nix
@@ -1,11 +1,13 @@
-{ config, ... }: {
+{ config, pkgs, ... }: {
 
 	# Enable Polkit.
 	security.polkit.enable = true;
 
 	# QT Polkit agent for the Hyprland Wayland compositor.
-(if config.home-manager.users.${config.userName}.wayland.windowManager.hyprland.enable then
+	environment.systemPackages = with pkgs; [
+		(if config.home-manager.users.${config.userName}.wayland.windowManager.hyprland.enable then
 		pkgs.hyprpolkitagent else null)
+	];
 
 	# Start the Polkit agent in the Hyprland Wayland compositor.
 	home-manager.users.${config.userName}.wayland.windowManager.hyprland.settings.exec-once = [
