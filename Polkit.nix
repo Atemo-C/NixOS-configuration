6c702d4752675cb39150c7c79aa0bcd8983a42f3
Atemo-C <c.roux1993@gmail.com>
2025-04-28 03:10:05 +0200

    Okay, this time it should be good. Right?

--- Polkit.nix
+++ Polkit.nix
@@ -4,16 +4,16 @@
 	security.polkit.enable = true;
 
 	# QT Polkit agent for the Hyprland Wayland compositor.
-	environment.systemPackages = with pkgs; [
+	environment.systemPackages = [
 		(if config.home-manager.users.${config.userName}.wayland.windowManager.hyprland.enable then
-		pkgs.hyprpolkitagent else null)
+			pkgs.hyprpolkitagent else null)
 	];
 
 	# Start the Polkit agent in the Hyprland Wayland compositor.
-	home-manager.users.${config.userName}.wayland.windowManager.hyprland.settings.exec-once = [
+	home-manger.users.${config.userName}.wayland.windowManager.hyprland.settings.exec-once = [
 		(if config.home-manager.users.${config.userName}.wayland.windowManager.hyprland.enable then
 			"systemctl --user start hyprpolkitagent" else null
-		);
+		)
 	];
 
 }
