8f8a4600a0ce25892e4e1d23a874b56ce3efdba3
Atemo-C <c.roux1993@gmail.com>
2025-05-01 22:40:17 +0200

    Standarized colors.

--- Theming/Terminal-colors.nix
+++ Theming/Terminal-colors.nix
@@ -1,66 +1,80 @@
-{ config, ... }: {
+{ config, ... }: let
+
+	# Define the list of colors shared across terminals.
+	Background    = "000000"; Foreground    = "eeeeee";
+
+	normalBlack   = "161616";    dimBlack   = "060606";    brightBlack   = "333333";
+	normalBlue    = "0080ff";    dimBlue    = "005ab3";    brightBlue    = "66b3ff";
+	normalCyan    = "00ffff";    dimCyan    = "00b3b3";    brightCyan    = "66ffff";
+	normalGreen   = "00ff00";    dimGreen   = "00b300";    brightGreen   = "66ff66";
+	normalMagenta = "ff0080";    dimMagenta = "b3005a";    brightMagenta = "ff66b3";
+	normalRed     = "ff0000";    dimRed     = "b30000";    brightRed     = "ff6666";
+	normalWhite   = "e6e6e6";    dimWhite   = "b3b3b3";    brightWhite   = "ffffff";
+	normalYellow  = "ffc000";    dimYellow  = "b38600";    brightYellow  = "ffd966";
+
+in {
 
 	# Linux console (TTY) colors.
 	console = {
 		# The 16 colors palette used by the virtual consoles.
 		colors = [
-			"000000" # Black
-			"ff0000" # Red
-			"00ff00" # Green
-			"ffc000" # Yellow
-			"0080ff" # Blue
-			"ff00ff" # Magenta
-			"00ffff" # Cyan
-			"dddddd" # White
-			"333333" # Bright black
-			"ff4d4d" # Bright red
-			"80ff80" # Bright green
-			"ffd966" # Bright yellow
-			"66b3ff" # Bright blue
-			"ff66ff" # Bright magenta
-			"80ffff" # Bright cyan
-			"ffffff" # Bright white
+			Background
+			normalRed
+			normalGreen
+			normalYellow
+			normalBlue
+			normalMagenta
+			normalCyan
+			normalWhite
+			normalBlack
+			brightRed
+			brightGreen
+			brightYellow
+			brightBlue
+			brightMagenta
+			brightCyan
+			brightWhite
 		];
 
-		# Enable setting virtual console options as early as possible (in initrd).
+		# Enable setting virtual console options (such as colors) as early as possible (in initrd).
 		earlySetup = true;
 	};
 
 	# Alacritty colors.
 	home-manager.users.${config.userName}.programs.alacritty.settings.colors = {
 		primary = {
-			background = "#000000";
-			foreground = "#eeeeee";
+			background = "#${Background}";
+			foreground = "#${Foreground}";
 		};
 		dim = {
-			black = "#000000";
-			blue = "#005ab3";
-			cyan = "#00b3b3";
-			green = "#00b300";
-			magenta = "#b3005a";
-			red = "#b30000";
-			white = "#b3b3b3";
-			yellow = "#b38600";
+			black = "#${dimBlack}";
+			blue = "#${dimBlue}";
+			cyan = "#${dimCyan}";
+			green = "#${dimGreen}";
+			magenta = "#${dimMagenta}";
+			red = "#${dimRed}";
+			white = "#${dimWhite}";
+			yellow = "#${dimYellow}";
 		};
 		normal = {
-			black = "#141414";
-			blue = "#0080ff";
-			cyan = "#00ffff";
-			green = "#00ff00";
-			magenta = "#ff0080";
-			red = "#ff0000";
-			white = "#e6e6e6";
-			yellow = "#ffc000";
+			black = "#${normalBlack}";
+			blue = "#${normalBlue}";
+			cyan = "#${normalCyan}";
+			green = "#${normalGreen}";
+			magenta = "#${normalMagenta}";
+			red = "#${normalRed}";
+			white = "#${normalWhite}";
+			yellow = "#${normalYellow}";
 		};
 		bright = {
-			black = "#333333";
-			blue = "#66b3ff";
-			cyan = "#66ffff";
-			green = "#66ff66";
-			magenta = "#ff66b3";
-			red = "#ff6666";
-			white = "#ffffff";
-			yellow = "#ffd966";
+			black = "#${brightBlack}";
+			blue = "#${brightBlue}";
+			cyan = "#${brightCyan}";
+			green = "#${brightGreen}";
+			magenta = "#${brightMagenta}";
+			red = "#${brightRed}";
+			white = "#${brightWhite}";
+			yellow = "#${brightYellow}";
 		};
 	};
 
