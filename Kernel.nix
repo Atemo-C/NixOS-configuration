e276fa8b3a99d81dccc9b20ae6f7dee99c15efe4
Atemo-C <c.roux1993@gmail.com>
2025-05-01 18:11:00 +0200

    Fixed extra `boot.`

--- Kernel.nix
+++ Kernel.nix
@@ -2,7 +2,7 @@
 
 	# Change the compression type of the initramfs to improve boot times.
 	# The effect is only really noticeable on old hardware.
-	boot.initrd.compressor = "cat";
+	initrd.compressor = "cat";
 
 	# Override the Linux kernel used by NixOS.
 	kernelPackages = pkgs.linuxPackages_zen;
