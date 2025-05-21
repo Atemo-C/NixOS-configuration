be08e486adb483aa83bc8fa92f680b6e25416ead
Atemo-C <c.roux1993@gmail.com>
2025-05-21 15:58:08 +0200

    Added `poppler-utils` for more PDF manipulation.

--- Programs/Office.nix
+++ Programs/Office.nix
@@ -3,6 +3,9 @@
 	# Comprehensive, professional-quality productivity suite, a variant of openoffice.org.
 	pkgs.libreoffice-fresh
 
+	# PDF rendering library.
+	pkgs.poppler-utils
+
 	# A document viewer capable of displaying multiple and single page document formats like PDF and Postscript.
 	pkgs.xreader
 
