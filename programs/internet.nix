948ec715287cce9c318ac27f9b208b5cc7b43f98
Atemo-C <c.roux1993@gmail.com>
2025-09-02 01:58:26 +0200

    Vastly compacted the module.
    This is much more readable, but does not really change anything functionality-wise.

--- programs/internet.nix
+++ programs/internet.nix
@@ -1,42 +1,22 @@
-{ config, lib, pkgs, ... }: {
-	environment = {
-		systemPackages = with pkgs; [
-			# CLI Gemini client.
-			amfora
+{ config, lib, pkgs, ... }: { environment = rec {
+	# Non-graphical programs.
+	systemPackages = with pkgs; [
+		amfora # CLI Gemini client.
+		tor    # Anonymizing overlay network.
 
-			# Anonymizing ovelay network.
-			tor
-		] ++ lib.optionals config.programs.niri.enable (with pkgs; [
-			# Matrix client.
-			element-desktop
+	# Graphical programs.
+	] ++ lib.optionals config.programs.niri.enable (with pkgs; [
+		element-desktop # Matrix client.
+		lagrange        # Graphical Gemini client.
+		librewolf       # Fork of the Firefox web browser focused on privacy and security.
+		qbittorrent     # BitTorrent client.
+		revolt-desktop  # Open-source Discord-like chat platform.
+		ruffle          # Adobe Flash Player emulator.
+		speedtest       # Graphical librespeed client.
+		tor-browser     # Privacy-focused, Firefox-based browser routing traffic through the Tor network.
+		vesktop         # Alternative Discord client with Vencord built-in.
+	]);
 
-			# Graphical Gemini client.
-			lagrange
-
-			# Fork of the Firefox web browser, focused on privacy and security.
-			librewolf
-
-			# BitTorrent client.
-			qbittorrent
-
-			# Open-source Discord-like chat platform.
-			revolt-desktop
-
-			# Cross platform Adobe Flash Player emulator.
-			# Note: Not necessarily an "internet" app, but come on, Flash games!
-			ruffle
-
-			# Graphical librespeed client.
-			speedtest
-
-			# Privaly-focused, Firefox-based browser routing traffic through the Tor network.
-			tor-browser
-
-			# Alternate Discord client with Vencord built-in.
-			vesktop
-		]);
-
-		# Set the default web browser.
-		variables = { BROWSER = "librewolf"; };
-	};
-}
+	# Default web browser (change with the desired browser installed in `environment.systemPackages`).
+	variables = lib.mkIf (lib.elem pkgs.librewolf systemPackages) { BROWSER = "librewolf"; };
+}; }
