b68dd5fe35e6f5b0190fc1f41048949248f4c49a
Atemo-C <c.roux1993@gmail.com>
2025-09-01 19:56:38 +0200

    Keyboard layout configuration now probably slightly more painless.

--- input/keyboard-layout.nix
+++ input/keyboard-layout.nix
@@ -7,7 +7,7 @@
 	# • https://github.com/NixOS/nixpkgs/issues/254523
 	# • https://github.com/NixOS/nixpkgs/issues/286283
 	#
-	# For Niri, the keyboard layout needs to be manually configured in the `./desktop/files/niri.kdl` file.
+	# For Niri, the keyboard layout can be manually configured in the `./desktop/files/niri.kdl` file.
 	services.xserver.xkb = {
 		# Keyboard layout to use.
 		layout = "us";
@@ -15,6 +15,12 @@
 
 		# Keyboard layout variant to use.
 		variant = "intl";
+	};
+
+	# Export the used keyboard layout. Some programs rely on this setting for it to be applied automatically.
+	environment.variables = {
+		XKB_DEFAULT_LAYOUT = config.services.xserver.xkb.layout;
+		XKB_DEFAULT_VARIANT = config.services.xserver.xkb.variant;
 	};
 
 	# Whether to let the virtual console's (TTY's) keyboard layout be the same as the one configured above.
