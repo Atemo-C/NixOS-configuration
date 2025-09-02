68930d3f287731ef5b2d4fb253c59336c5dd3e18
Atemo-C <c.roux1993@gmail.com>
2025-09-02 18:10:07 +0200

    Compacted the module, and disabled v4l(2) by default.

--- programs/multimedia.nix
+++ programs/multimedia.nix
@@ -1,7 +1,6 @@
 { config, lib, pkgs, ... }: let
 	# Shortcuts to check if various programs are installed.
-	# All but `parallel` can be found in this module.
-	# `parallel` can be found in the `./programs/shell-utilities.nix` module.
+	# All but `parallel` can be found here; `parallel` can be found in the `./programs/shell-utilities.nix` module.
 	ffm = lib.elem pkgs.ffmpeg-full config.environment.systemPackages;
 	gal = lib.elem pkgs.gallery-dl  config.environment.systemPackages;
 	jpg = lib.elem pkgs.jpegoptim   config.environment.systemPackages;
@@ -10,16 +9,20 @@
 	png = lib.elem pkgs.oxipng      config.environment.systemPackages;
 	ytd = config.home-manager.users.${config.userName}.programs.yt-dlp.enable;
 in {
-
-	# Video4Linux2 Kernel module.
-	boot = {
-		extraModulePackages = [ config.boot.kernelPackages.v4l2loopback ];
-		initrd.kernelModules = [ "v4l2loopback" ];
-	};
+	# Video4Linux2 kernel module.
+	#boot.extraModulePackages = [ config.boot.kernelPackages.v4l2loopback ];
+	#boot.initrd.kernelModules = [ "v4l2loopback" ];
 
 	environment.systemPackages = with pkgs; [
-		# V4L utilities and libv4l, providing common image formats regardless of the v4l device.
-		v4l-utils
+		#v4l-utils   # V4L utilities and libv4l, providing common image formats regardless of the v4l device.
+		ffmpeg-full # A complete solution to record, convert, stream audio and video.
+		libde265    # Open h.265 video codec implementation.
+		dcmtk       # Collection of libraries and applications implementing large parts of the DICOM standard.
+		imagemagick # Software suite to create, edit, compose, and convert bitmap images.
+		oxipng      # PNG optimiser.
+		jpegoptim   # JPEG optimiser.
+		libjxl      # JPEL XL image format reference implementation.
+		libwebp     # Tools and library for the WebP image format.
 
 		# Open-source multimedia framework.
 		gst_all_1.gst-editing-services
@@ -32,64 +35,27 @@ in {
 		gst_all_1.gst-vaapi
 		gst_all_1.gstreamer
 
-		# A complete solution to record, convert, stream audio and video.
-		ffmpeg-full
-
-		# Open h.265 video codec implementation.
-		libde265
-
 		# General-purpose media player; Fork of MPlayer and mplayer2.
 		# Includes some plugins.
 		(pkgs.mpv.override { scripts = with pkgs.mpvScripts; [
 			mpris
 			sponsorblock
 		]; } )
-
-		# Collection of libraries and applications implementing large parts of the DICOM standard.
-		dcmtk
-
-		# Software suite to create, edit, compose, and convert bitmap images.
-		imagemagick
-
-		# PNG optimiser.
-		oxipng
-
-		# JPEG optimiser.
-		jpegoptim
-
-		# JPEL XL image format reference implementation.
-		libjxl
-
-		# Tools and library for the WebP image format.
-		libwebp
 	] ++ lib.optionals config.programs.niri.enable (with pkgs; [
-		# Color picker.
-		gcolor3
-
-		# The GNU Image Manipulation Program.
-		gimp3
-
-		# Vector graphics editor.
-		inkscape
-
-		# Video editor based on the MLT and KDE Frameworks.
-		kdePackages.kdenlive
-
-		# Painting and animation program.
-		krita
-
-		# Image viewer.
-		lxqt.lximage-qt
-
-		# AI image upscaler.
-		upscayl
+		gcolor3              # Color picker.
+		gimp3                # The GNU Image Manipulation Program.
+		inkscape             # Vector graphics editor.
+		kdePackages.kdenlive # Video editor based on the MLT and KDE Frameworks.
+		krita                # Painting and animation program.
+		lxqt.lximage-qt      # Image viewer.
+		upscayl              # AI image upscaler.
 	]);
 
 	home-manager.users.${config.userName} = {
 		# Whether to enable yt-dlp, a CLI tool to download videos from YouTube and other websites; Fork of yt-dl.
 		programs.yt-dlp.enable = true;
 
-		# Link the configuration files for yt-dlp and mpv to the right place.
+		# Link the configuration files for yt-dlp and mpv.
 		systemd.user.tmpfiles.rules = [
 			"L %h/.config/mpv/mpv.conf - - - - /etc/nixos/programs/files/mpv.conf"
 		] ++ lib.optional ytd
@@ -98,40 +64,30 @@ in {
 
 	# Shell abbreviations for various programs installed in this module.
 	programs.fish.shellAbbrs = lib.mkIf config.programs.fish.enable {
-		# Losslessly optimize all JPEG images in the current directory.
+		# Losslessly optimize all JPEG and PNG images in the current directory.
 		# The original files are overridden.
 		opti-jpg = lib.mkIf jpg "jpegoptim -s -v *.jp{,e}g";
-
-		# Losslessly optimize all PNG images in the current directory.
-		# The original files are overriden.
 		opti-png = lib.mkIf png "oxipng --strip all -v *.png";
 
-		# Losslessly convert all JPEG images to JPEG XL ones in the current directory.
+		# Losslessly convert all JPEG and PNG images to JPEG XL ones in the current directory.
 		# The original files are kept.
 		jpg-jxl = lib.mkIf (jxl && par) "parallel cjxl -e 9 '{}' '{.}'.jxl -v ::: *.jp{,e}g";
-
-		# Losslessly convert all PNG images to JPEG XL ones in the current directory.
-		# The original files are kept.
 		png-jxl = lib.mkIf (jxl && par) "parallel cjxl -e 9 '{}' '{.}'.jxl -d 0 -v ::: *.png";
 
-		# Convert `.jxl` images back to `.png` ones in the current directory.
+		# Convert `.jxl` images to `.png` ones in the current directory.
 		# The original files are kept.
 		jxl-png = lib.mkIf (jxl && par) "parallel djxl '{}' '{.}'.png -v ::: *.jxl";
 
-		# Batch download images.
-		imgdl = lib.mkIf gal "gallery-dl";
-
-		# Same as above, but through a Tor proxy (`tor --client socks5`).
+		# Batch download images normally and through a Tor proxy.
+		imgdl     = lib.mkIf gal "gallery-dl";
 		imgdl-tor = lib.mkIf gal "gallery-dl --proxy socks5://localhost:9050";
 
-		# Simple yt-dlp shortcut.
-		yt = lib.mkIf ytd "yt-dlp -t sleep";
+		# Simple yt-dlp shortcut normally and through a Tor proxy.
+		yt     = lib.mkIf ytd "yt-dlp -t sleep";
+		yt-tor = lib.mkIf ytd "yt-dlp -t sleep --proxy socks5://localhost:9050";
 
-		# Highest-quality MP3 audio-only download.
-		ytmp3 = lib.mkIf ytd "yt-dlp -t sleep -x --audio-format mp3 --audio-quality 0";
-
-		# Same two shortcuts as above, but through a Tor proxy (`tor --client socks5`).
-		yt-tor    = lib.mkIf ytd "yt-dlp -t sleep --proxy socks5://localhost:9050";
+		# Highest-quality MP3 audio-only download normally and through a Tor proxy.
+		ytmp3     = lib.mkIf ytd "yt-dlp -t sleep -x --audio-format mp3 --audio-quality 0";
 		ytmp3-tor = lib.mkIf ytd "yt-dlp -t sleep -x --audio-format mp3 --audio-quality 0 --proxy socks5://localhost:9050";
 
 		# Hide the banner when using FFmpeg.
