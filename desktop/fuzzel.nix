59c3630b5fda86d13e1029240547a663341ee621
Atemo-C <c.roux1993@gmail.com>
2025-09-02 15:16:29 +0200

    Minor formatting tweaks.

--- desktop/fuzzel.nix
+++ desktop/fuzzel.nix
@@ -1,49 +1,43 @@
-{ config, lib, ... }: {
-	home-manager.users.${config.userName}.programs.fuzzel = lib.mkIf config.programs.niri.enable {
+{ config, lib, ... }: lib.mkIf config.programs.niri.enable {
+	home-manager.users.${config.userName}.programs.fuzzel = {
 		# Whether to enable the Fuzzel launcher.
 		enable = true;
 
-		# Fuzzel settings, written to `~/.config.fuzzel/fuzzel.ini`.
-		settings = {
-			border = {
-				# Width of the border in pixels.
-				width = 2;
+		# Width of the border in pixels.
+		border.width = 2;
 
-				# Rounding of the corners in pixels.
-				radius = 0;
-			};
+		# Rounding of the corners in pixels.
+		border.radius = 0;
 
-			colors = {
-				background = "0d0d0dcc";
-				border = "0080ffcc";
-				input = "e6ffffff";
-				match = "ff0080ff";
-				prompt = "00ffffcc";
-				selection = "2073ff88";
-				selection-text = "ffffffff";
-				selection-match = "00ffffff";
-				text = "ffffffff";
-			};
+		colors = rec {
+			background      = "0d0d0dcc";
+			border          = "0080ffcc";
+			input           = "e6ffffff";
+			match           = "ff0080ff";
+			prompt          = "00ffffcc";
+			selection       = "2073ff88";
+			selection-text  = text;
+			selection-match = "00ffffff";
+			text            = "ffffffff";
+		};
 
-			main = rec {
-				# Use a font defined in the `./theming/fonts.nix` module.
-				font = "monospace:size=12.5";
+		main = rec {
+			# Use a font defined in the `./theming/fonts.nix` module.
+			font = "monospace:size=12.5";
 
-				# Horizontal, vertical, and inner padding in pixels.
-				horizontal-pad = 6;
-				inner-pad = horizontal-pad;
-				vertical-pad = horizontal-pad;
+			# Padding in pixels.
+			horizontal-pad = 6;
+			inner-pad = horizontal-pad;
+			vertical-pad = horizontal-pad;
 
-				# Use the icon theme set for the user in the `./theming/icons.nix` module.
-				icon-theme = "${config.home-manager.users.${config.userName}.gtk.iconTheme.name}";
+			# Use the icon theme set for the user in the `./theming/icons.nix` module.
+			icon-theme = "${config.home-manager.users.${config.userName}.gtk.iconTheme.name}";
 
-				# Line height in pixels.
-				line-height = 20;
+			# Line height in pixels.
+			line-height = 20;
 
-				# Whether to use bold text for selected items.
-				# Works best with mono-spaced fonts.
-				use-bold = "yes";
-			};
+			# Whether to use bold text for selected items. Works best with mono-spaced fonts.
+			use-bold = "yes";
 		};
 	};
 }
