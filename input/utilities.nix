586a0a54efbf692ffb3729d1b0f39a7c41c51d28
Atemo-C <c.roux1993@gmail.com>
2025-09-02 16:14:17 +0200

    Compacted.

--- input/utilities.nix
+++ input/utilities.nix
@@ -1,23 +1,8 @@
-{ config, lib, pkgs, ... }: {
-	environment.systemPackages = lib.optionals config.programs.niri.enable (with pkgs; [
-		# Show mouse refresh rate.
-		evhz
-
-		# Utility for mapping events from Linux event devices.
-		# e.g. `sudo evsieve --input /dev/input/by-id/… --map btn:extra key:f13 --output`
-		evsieve
-
-		# Graphical joystic tester.
-		jstest-gtk
-
-		# User-mode driver and GUI for configuring any controller.
-		# Note: Awesome, but way too buggy for me.
-		#sc-controller
-
-		# Tools for working with USB devices, such as `lsusb`.
-		usbutils
-
-		# Autoclicker for XWayland programs.
-		xclicker
-	]);
-}
+{ config, lib, pkgs, ... }: lib.optionals config.programs.niri.enable { environment.systemPackages = with pkgs; [
+	evhz          # Show mouse refresh rate.
+	evsieve       # Utility for mapping events from Linux event devices.
+	jstest-gtk    # Graphical joystic tester.
+	#sc-controller # User-mode driver and GUI for configuring any controller.
+	usbutils      # Tools for working with USB devices, such as `lsusb`.
+	xclicker      # Autoclicker for XWayland programs.
+]; }
