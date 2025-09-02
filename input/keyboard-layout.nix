a83762899fb62499b5d7a71e70be0d431131b59a
Atemo-C <c.roux1993@gmail.com>
2025-09-02 16:06:51 +0200

    Nicely compacted the module.

--- input/keyboard-layout.nix
+++ input/keyboard-layout.nix
@@ -1,29 +1,19 @@
-{ config, ... }: {
-	# Keyboard layout to use.
+{ config, ... }: rec {
+	# Keyboard layout settings.
 	# To see a complete list of layouts, variants, and other settings:
 	# • https://gist.github.com/jatcwang/ae3b7019f219b8cdc6798329108c9aee
 	#
-	# And why this list cannot be seen within NixOS:
+	# To see why this list cannot easily be seen within NixOS:
 	# • https://github.com/NixOS/nixpkgs/issues/254523
 	# • https://github.com/NixOS/nixpkgs/issues/286283
-	#
-	# For Niri, the keyboard layout can be manually configured in the `./desktop/files/niri.kdl` file.
-	services.xserver.xkb = {
-		# Keyboard layout to use.
-		layout = "us";
-		#layout = "fr";
+	services.xserver.xkb.layout = "us";
+	services.xserver.xkb.variant = "intl";
 
-		# Keyboard layout variant to use.
-		variant = "intl";
-	};
-
-	# Export the used keyboard layout. Some programs rely on this setting for it to be applied automatically.
-	environment.variables = {
-		XKB_DEFAULT_LAYOUT = config.services.xserver.xkb.layout;
-		XKB_DEFAULT_VARIANT = config.services.xserver.xkb.variant;
-	};
+	# Export the used keyboard layout. Some programs rely on this setting for it to be properly applied.
+	environment.variables.XKB_DEFAULT_LAYOUT = services.xserver.xkb.layout;
+	environment.variables.XKB_DEFAULT_VARIANT = config.services.xserver.xkb.variant;
 
 	# Whether to let the virtual console's (TTY's) keyboard layout be the same as the one configured above.
-	# If false, it needs to be set using the `console.keyMap` option.
+	# If false, it needs to be manually configured with the `console.keyMap` option.
 	console.useXkbConfig = true;
 }
