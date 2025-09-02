be1eed8ccd7e7f4703e5e37f53bb654b5a9d726c
Atemo-C <c.roux1993@gmail.com>
2025-09-02 18:28:07 +0200

    Compacted.

--- programs/shell-utilities.nix
+++ programs/shell-utilities.nix
@@ -1,34 +1,13 @@
-{ pkgs, ... }: {
-	environment.systemPackages = with pkgs; [
-		# A `cat`(1) clone with syntax highlighting and Git integration.
-		bat
-
-		# GNU software calculator.
-		bc
-
-		# A calendar and scheduling application for the command line.
-		calcurse
-
-		# A POSIX-compliant implementation of `/bin/sh` that aims to be as small as possible.
-		dash
-
-		# Distributed version control system & GitHub CLI tool.
-		git
-		gh
-
-		# Lightweight and flexible command-line JSON processor.
-		jq
-
-		# Shell tool for executing jobs in parallel.
-		parallel
-
-		# Shell script analysis tool.
-		shellcheck
-
-		# Simplified and community-driven man pages, written in Rust.
-		tlrc
-
-		# Tool for retrieving files using HTTP, HTTPS, and FTP.
-		wget
-	];
-}
+{ pkgs, ... }: { environment.systemPackages = with pkgs; [
+	bat        # A `cat`(1) clone with syntax highlighting and Git integration.
+	bc         # GNU software calculator.
+	calcurse   # A calendar and scheduling application for the command line.
+	dash       # A POSIX-compliant implementation of `/bin/sh` that aims to be as small as possible.
+	gh         # GitHub CLI tool.
+	git        # Distributed version control system.
+	jq         # Lightweight and flexible command-line JSON processor.
+	parallel   # Shell tool for executing jobs in parallel.
+	shellcheck # Shell script analysis tool.
+	tlrc       # Simplified and community-driven man pages, written in Rust.
+	wget       # Tool for retrieving files using HTTP, HTTPS, and FTP.
+]; }
--- programs/system-info.nix
+++ programs/system-info.nix
@@ -1,40 +1,24 @@
 { config, lib, pkgs, ... }: {
 	environment.systemPackages = with pkgs; [
-		# A monitor of resources.
-		btop
-
-		# Tools for reading hardware sensors.
-		lm_sensors
-
-		# Tools for monitoring the health of hard drives.
-		smartmontools
-
-		# Mersenne prime search / System stability tester (torture).
-		mprime
-
-		# Provide detailed information on the hardware configuration of the machine.
-		lshw
+		btop          # A monitor of resources.
+		lm_sensors    # Tools for reading hardware sensors.
+		smartmontools # Tools for monitoring the health of hard drives.
+		mprime        # Mersenne prime search / System stability tester (torture).
+		lshw          # Provide detailed information on the hardware configuration of the machine.
 	] ++ lib.optionals config.programs.niri.enable (with pkgs; [
-		# See information on CPU, motherboard and more.
-		cpu-x
-
-		# Monitor your CPU, Memory, Disk, Network and GPU usage graphically.
-		mission-center
-
-		# Vulkan tools and utilties.
-		vulkan-tools
-
-		# Collection of demos and test programs for OpenGL and Mesa.
-		mesa-demos
+		cpu-x          # See information on CPU, motherboard and more.
+		mission-center # Monitor your CPU, Memory, Disk, Network and GPU usage graphically.
+		vulkan-tools   # Vulkan tools and utilties.
+		mesa-demos     # Collection of demos and test programs for OpenGL and Mesa.
 	]);
 
-	home-manager.users.${config.userName} = {
+	home-manager.users.${config.userName} = rec {
 		# Whether to enable Fastfetch, a fast system information tool like Neofetch.
 		programs.fastfetch.enable = true;
 
-		# Link the configuration file to the right place.
-		systemd.user.tmpfiles.rules = lib.optional config.home-manager.users.${config.userName}.programs.fastfetch.enable
-			"L %h/.config/fastfetch/config.jsonc - - - - /etc/nixos/programs/files/fastfetch.jsonc";
+		# Link the configuration file of Fastfetch.
+		systemd.user.tmpfiles.rules = lib.opional programs.fastfetch.enable
+		"L %h/.config/fastfetch/config.jsonc - - - - /etc/nixos/programs/files/fastfetch.jsonc";
 	};
 
 	# Shell abbreviations for BTOP and Fastfetch.
