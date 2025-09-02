fde3baf9018d1c431b0316db1acdf5a85cc26f2f
Atemo-C <c.roux1993@gmail.com>
2025-09-02 15:41:59 +0200

    Minor formatting tweaks.

--- desktop/waybar.nix
+++ desktop/waybar.nix
@@ -1,14 +1,12 @@
-{ config, lib, ... }: {
-	home-manager.users.${config.userName} = lib.mkIf config.programs.niri.enable rec {
-		programs.waybar = {
-			# Whether to enable the Waybar bar.
-			enable = true;
+{ config, lib, ... }: lib.mkIf config.programs.niri.enable {
+	home-manager.users.${config.userName} = rec {
+		# Whether to enable the Waybar bar.
+		programs.waybar.enable = true;
 
-			# Whether to enable Waybar systemd integration; Useful for things like auto-starting itself.
-			systemd.enable = lib.mkIf programs.waybar.enable true;
-		};
+		# Whether to enable Waybar systemd integration; Useful for auto-starting with the desktop.
+		programs.waybar.systemd.enable = lib.mkIf enable true;
 
-		# Link the configuration file to the right place.
+		# Link the configuration file.
 		systemd.user.tmpfiles.rules = lib.optionals programs.waybar.enable [
 			"L %h/.config/waybar/config - - - - /etc/nixos/desktop/files/waybar/config.json"
 			"L %h/.config/waybar/style.css - - - - /etc/nixos/desktop/files/waybar/style.css"
