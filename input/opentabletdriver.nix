a6eb5ad5fd9a67b5bd2343e7a4d2413dc6752f63
Atemo-C <c.roux1993@gmail.com>
2025-09-02 16:11:32 +0200

    Minor formatting tweaks, and now no longer requires Niri to be enabled.
    The reasoning for the latter is that there are some graphical programs that can be started without Niri where I might want to use my graphics tablet with. Very rare, but it does happen.

--- input/opentabletdriver.nix
+++ input/opentabletdriver.nix
@@ -1,10 +1,8 @@
-{ config, lib, ... }: {
-	hardware.opentabletdriver = lib.mkIf config.programs.niri.enable rec {
-		# Whether to enable OpenTabletDriver, replacing the default drivers when conflicting.
-		enable = true;
+{ config, lib, ... }: { hardware.opentabletdriver = rec {
+	# Whether to enable OpenTabletDriver, replacing the default drivers when conflicting.
+	enable = true;
 
-		# Whether to start the OpenTabletDriver daemon as a systemd user service.
-		# It allows for it to start with the desktop.
-		daemon.enable = lib.mkIf enable true;
-	};
-}
+	# Whether to start the OpenTabletDriver daemon as a systemd user service.
+	# It allows for it to auto-start with the desktop.
+	daemon.enable = lib.mkIf enable true;
+}; }
