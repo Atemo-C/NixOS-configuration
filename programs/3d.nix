c236c3fb5d3c73d963318b1d4ef31b9c238fe0c0
Atemo-C <c.roux1993@gmail.com>
2025-09-02 16:18:20 +0200

    Slightly compacted module.

--- programs/3d.nix
+++ programs/3d.nix
@@ -1,20 +1,15 @@
-{ config, lib, pkgs, ... }: {
-	environment.systemPackages = lib.optionals config.programs.niri.enable (with pkgs; [
-		# 3D creation, animation, publishing system.
-		blender
+{ config, lib, pkgs, ... }: lib.mkIf config.programs.niri.enable { environment.systemPackages = with pkgs; [
+	blender       # 3D creation, animation, publishing system.
+	cura-appimage # 3D printer slicing utility.
 
-		# 3D printer slicing utility.
-		cura-appimage
+	# Disign and visualize your future home (version before it sucked).
+	# Warning: For some reason, installing it breaks Flatpaks???
+	# https://github.com/NixOS/nixpkgs/issues/365726
+	#sweethome3d.application
 
-		# Disign and visualize your future home (version before it sucked).
-		# Warning: For some reason, installing it breaks Flatpaks???
-		# https://github.com/NixOS/nixpkgs/issues/365726
-		#sweethome3d.application
+	# Easily create SH3T files and edit the properties of the texture images it contains.
+	#sweethome3d.textures-editor
 
-		# Easily create SH3T files and edit the properties of the texture images it contains.
-		#sweethome3d.textures-editor
-
-		# Quickly create SH3F files and edit the properties of the 3D models it contains.
-		#sweethome3d.furniture-editor
-	]);
-}
+	# Quickly create SH3F files and edit the properties of the 3D models it contains.
+	#sweethome3d.furniture-editor
+]; }
