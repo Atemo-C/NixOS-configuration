87714b5bdb14869e6087f3e36185dd5857876cb8
Atemo-C <c.roux1993@gmail.com>
2025-09-02 15:36:53 +0200

    Much more compact and neater.

--- desktop/utilities.nix
+++ desktop/utilities.nix
@@ -1,21 +1,7 @@
-{ config, lib, pkgs, ... }: {
-	environment.systemPackages = lib.optionals config.programs.niri.enable (with pkgs; [
-		# Polkit authentification utility to be used with dmenu-like menus.
-		cmd-polkit
-
-		# Keyring utility from the GNOME desktop.
-		gnome-keyring
-
-		# Wayland wallpaper utility.
-		hyprpaper
-
-		# Polkit authentification agent.
-		#hyprpolkitagent
-
-		# Nautilus file manager; Only here for file picking as Niri uses the GNOME XDG Portal.
-		nautilus
-
-		# Display graphical dialogs from the command-line and shell scripts.
-		zenity
-	]);
-}
+{ config, lib, pkgs, ... }: lib.mkIf config.programs.niri.enable { environment.systemPackages = with pkgs; [
+	cmd-polkit    # Polkit authentification utility to be used with dmenu-like menus.
+	gnome-keyring # Keyring utility from the GNOME desktop.
+	hyprpaper     # Wayland wallpaper utility.
+	nautilus      # Nautilus file manager; Only here for file picking as Niri uses the GNOME XDG Portal.
+	zenity        # Display graphical dialogs from the command-line and shell scripts.
+]; }
