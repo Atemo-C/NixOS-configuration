5e2300388bed9b703ede04e13573d4e3db832c84
Atemo-C <c.roux1993@gmail.com>
2025-09-02 18:16:55 +0200

    Compacted.

--- programs/office.nix
+++ programs/office.nix
@@ -1,13 +1,5 @@
-{ config, lib, pkgs, ... }: {
-	environment.systemPackages = lib.optionals config.programs.niri.enable (with pkgs; [
-		# Comprehensive, professional-quality productivity suite, a variant of openoffice.org.
-		# Did you know: LibreOffice is based on OpenOffice, which in itself is based on StarOffice?
-		libreoffice-fresh
-
-		# PDF rendering library.
-		poppler-utils
-
-		# A document viewer capable of displaying multiple and single page document formats like PDF and Postscript.
-		xreader
-	]);
-}
+{ config, lib, pkgs, ... }: lib.mkIf config.programs.niri.enable { environment.systemPackages = with pkgs; [
+	libreoffice-fresh # Comprehensive productivity suite; A variant of OpenOffice.org, itself of StarOffice.
+	poppler-utils     # PDF rendering library.
+	xreader           # Document viewer.
+]; }
