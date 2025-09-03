4d76a02da49fe8797f0db6c87a0d127d176f8cc0
Atemo-C <c.roux1993@gmail.com>
2025-09-03 13:59:41 +0200

    Removed useless `lib.mkIf`s and slightly compacted.

--- desktop/dunst.nix
+++ desktop/dunst.nix
@@ -1,15 +1,13 @@
-{ config, lib, ... }: lib.mkIf config.programs.niri.enable {
-	home-manager.users.${config.userName} = rec {
-		# Whether to display and send graphical notifications with Dunst.
-		services.dunst.enable = true;
+{ config, lib, ... }: lib.mkIf config.programs.niri.enable { home-manager.users.${config.userName} = rec {
+	# Whether to display and send graphical notifications with Dunst.
+	services.dunst.enable = true;
 
-		# Use the icon theme defined for the user in the `./theming/icons.nix` module.
-		services.dunst.iconTheme.package = lib.mkIf config.home-manager.users.${config.userName}.gtk.iconTheme.package;
-		services.dunst.iconTheme.name = lib.mkIf config.home-manager.users.${config.userName}.gtk.iconTheme.name;
-		services.dunst.settings.global.icon_path = "$HOME/.nix-profile/share/icons/${config.home-manager.users.${config.userName}.gtk.iconTheme.name}/status/scalable/16/:$HOME/.nix-profile/share/icons/${config.home-manager.users.${config.userName}.gtk.iconTheme.name}/devices/scalable/";
+	# Use the icon theme defined for the user in the `./theming/icons.nix` module.
+	services.dunst.iconTheme.package = config.home-manager.users.${config.userName}.gtk.iconTheme.package;
+	services.dunst.iconTheme.name = config.home-manager.users.${config.userName}.gtk.iconTheme.name;
+	services.dunst.settings.global.icon_path = "$HOME/.nix-profile/share/icons/${config.home-manager.users.${config.userName}.gtk.iconTheme.name}/status/scalable/16/:$HOME/.nix-profile/share/icons/${config.home-manager.users.${config.userName}.gtk.iconTheme.name}/devices/scalable/";
 
-		# Link the configuration file for Dunst.
-		systemd.user.tmpfiles.rules = lib.optional services.dunst.enable
-		"L %h/.config/dunst/dunstrc - - - - /etc/nixos/desktop/files/dunst.conf"
-	};
-}
+	# Link the configuration file for Dunst.
+	systemd.user.tmpfiles.rules = lib.optional config.service.dunst.enable
+	"L %h/.config/dunst/dunstrc - - - - /etc/nixos/desktop/files/dunst.conf"
+}; }
