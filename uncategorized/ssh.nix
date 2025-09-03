1410992b7a5ddb8eec64df3ae754cbc9bef82d91
Atemo-C <c.roux1993@gmail.com>
2025-09-03 15:58:01 +0200

    Compacted and simplified logic.

--- uncategorized/ssh.nix
+++ uncategorized/ssh.nix
@@ -1,21 +1,16 @@
-{ config, lib, ... }: {
-	services.openssh = rec {
-		# Whether to enable the OpenSSH daemon.
-		enable = true;
+{ config, lib, ... }: { services.openssh = {
+	# Whether to enable the OpenSSH daemon.
+	enable = true;
 
-		# Which ports the SSH daemon sohuld listen to.
-		ports = lib.optional enable 6942;
+	# Which ports the SSH daemon sohuld listen to.
+	ports = 2077;
 
-		# OpenSSH settings.
-		settings = lib.mkIf enable {
-			# Login is allowed only for the listed users.
-			AllowUsers = [ "${config.userName}" ];
+	# Login is allowed only for the listed users.
+	settings.AllowUsers = [ "${config.userName}" ];
 
-			# Whether the root user can login with SSH.
-			PermitRootLogin = "no";
-		};
+	# Whether the root user can login with SSH.
+	settings.PermitRootLogin = "no";
 
-		# Whether to only start an instance for each incoming connection.
-		startWhenNeeded = lib.mkIf enable true;
-	};
-}
+	# Whether to only start an instance for each incoming connection.
+	startWhenNeeded = true;
+}; }
