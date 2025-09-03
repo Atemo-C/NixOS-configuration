7533696ba4eba9f2aa6047e088a38df52c269e3b
Atemo-C <c.roux1993@gmail.com>
2025-09-03 16:14:14 +0200

    Compacted and neater.

--- user/home-manager.nix
+++ user/home-manager.nix
@@ -1,21 +1,14 @@
-{ config, ... }: let
-	# Home Manager tarball.
-	home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";
-
+{ config, ... }: let home-manager =
+	builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";
 in {
-	# Import the Home Manger module.
+	# Import the Home Manager module.
 	imports = [ (import "${home-manager}/nixos") ];
 
-	# Home Manager settings.
-	home-manager = {
-		# Backup file extension to use when backing up files replaced by Home Manager.
-		backupFileExtension = "HM-Backup";
+	# Backup file extension to use when backing up files replaced by Home Manager.
+	home-manager.backupFileExtension = "HM-Backup";
 
-		# State version of Home Manager.
-		# Here, it is assumed that Home Manager was installed with the same version of NixOS that was installed.
-		users = {
-			${config.userName}.home.stateVersion = "${config.system.stateVersion}";
-			root.home.stateVersion = "${config.system.stateVersion}";
-		};
-	};
+	# State version of Home Manager.
+	# Here, it is assumed that Home Manager was installed with the same version of NixOS that was installed.
+	home-manager.users.${config.userName}.home.stateVersion = "${config.system.stateVersion}";
+	home-manager.users.root.home.stateVersion = "${config.system.stateVersion}";
 }
