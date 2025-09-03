203c38451d2366e86b5644db9a67e27eb801f05d
Atemo-C <c.roux1993@gmail.com>
2025-09-03 16:34:09 +0200

    Same tweaks as the last two.

--- hardware/devices/thinkpad-l510.nix
+++ hardware/devices/thinkpad-l510.nix
@@ -1,70 +1,52 @@
 { config, lib, pkgs, ... }: {
-	boot = {
-		# Select the Linux Kernel version to use.
-		# https://wiki.nixos.org/wiki/Linux_kernel#List_available_kernels
-		kernelPackages = pkgs.linuxPackages_latest;
+	# Select the Linux Kernel version to use.
+	# https://wiki.nixos.org/wiki/Linux_kernel#List_available_kernels
+	boot.kernelPackages = pkgs.linuxPackages_latest;
 
-		loader.limine = {
-			# Set the drive to install Limine onto.
-			# You can see its nam eby running `ls /dev/disk/by-id`.
-			biosDevice = lib.mkIf config.boot.loader.limine.enable "/dev/disk/by-id/disk-stuff-here";
+	# Set the drive to install Limine onto.
+	# You can see its name by running `ls /dev/disk/by-id`.
+	boot.limine.biosDevice = "/dev/disk/by-id/disk-stuff-here";
 
-			# Disable support for EFI booting.
-			efiSupport = lib.mkIf config.boot.loader.limine.enable false;
-		};
-	};
+	# Disable support for EFI booting.
+	boot.limine.efiSupport = false;
 
 	# Name of the system over the network.
 	# [a-z] [A-Z] [0-9] [ - ]
 	networking.hostName = "ThinkPad-L510";
 
-	systemd.services = {
-		# Disable NetworkManager's `ModemManager` service to improve boot times.
-		# Only disable if not using cellular data (tethering from a mobile device does not count).
-		ModemManager.enable = lib.mkIf config.networking.networkmanager.enable false;
+	# Disable the ModemManager service to improve boot times.
+	# Only disable if not using cellular data (tethering from a mobile device does not count).
+	systemd.services.ModemManager.enable = false;
 
-		# Service that overrides the default suspend command.
-		# This is because the default `systemctl suspend` does not work on this laptop.
-		"systemd-suspend" = lib.mkIf lib.elem pkgs.pmutils config.environment.systemPackages {
-			description = "System suspend with pm-suspend";
-			serviceConfig = {
-				Type = "oneshot";
-				Environment = "PATH=${pkgs.pmutils}/bin";
-				ExecStart = [
-					""
-					"${pkgs.pmutils}/bin/pm-suspend"
-				];
-			};
-		};
+	# Service that overrides the default suspend command.
+	# This is because the default `systemctl suspend` command does not work onthis laptop.
+	systemd.services."systemd-suspend" = lib.mkIf (lib.elem pkgs.pmutils config.environment.systemPackages) {
+		description               = "System suspend with pm-suspend";
+		serviceConfig.Type        = "oneshot";
+		serviceConfig.Environment = "PATH=${pkgs.pmutils}/bin";
+		serviceConfig.ExecStart   = "" "${pkgs.pmutils}/bin/pm-suspend";
 	};
 
 	# Small collection of scripts that handle suspend and resume on behalf of HAL.
 	# The script above will only be enabled if it is installed.
 	environment.systemPackages = [ pkgs.pmutils ];
 
-	# Keyboard layout to use (priority over `./input/keybord-layout.nix`).
-	services.xserver.xkb = {
-		layout = lib.mkForce "fr";
-		variant = lib.mkForce "";
-	};
+	# Keyboard layout to use (priority over `./input/keyboard-layout.nix`).
+	services.xserver.xkb.layout = lib.mkForce "fr";
+	services.xserver.xkb.variant = lib.mkForce "";
 
-	# zram, because 2GB of RAM is not enough for tasks like web browsing or updating the system.
-	zramSwap = rec {
+	# zram swap (memory compresssion); 2GB of RAM is NOT enough for tasks like web browsing or updating the system.
+	zramSwap = {
 		# Whether to enable in-memory compression devices and swap space provided by the zram kernel module.
 		enable = true;
 
-		# Compression algorithm to use.
-		# `842`, `lzo`, `lzo-rle`, `lz4`, `lz4hc`, `zstd`, or other.
-		algorithm = lib.mkIf enable "zstd";
-
 		# Maximum total amount of memory that can be stored in the zram swap devices.
-		# Run `zramctl` to check how good memory is compressed.
-		memoryPercent = lib.mkIf enable 90;
+		# Run `zramctl` to check how much memory is compressed.
+		memoryPercent = 80;
 
 		# Priority of the zram swap device.
 		# It should be a number higher than the priority of your disk-based swap devices,
 		# so that the system will fill the zram swap device before falling bcak to disk swap.
-		priority = lib.mkIf enable 50;
+		priority = 50;
 	};
-
 }
