45964202703d6371c0469c2653aba886a0a0e29c
Atemo-C <c.roux1993@gmail.com>
2025-09-03 15:49:44 +0200

    Slightly compacted.

--- uncategorized/networking.nix
+++ uncategorized/networking.nix
@@ -1,11 +1,9 @@
 { config, lib, ... }: {
-	networking = {
-		# Fork the dhcpcd device to the background to improve boot times.
-		dhcpcd.wait = "background";
+	# Fork the dhcpcd device to the background to improve boot times.
+	networking.dhcpcd.wait = "background";
 
-		# Whether to enable NetworkManager for easy network management.
-		networkmanager.enable = true;
-	};
+	# Whether to enable NetworkManager for easy network management.
+	networking.networkmanager.enable = true;
 
 	# Add the user to the `networkmanager` group for NetworkManager control.
 	users.users.${config.userName}.extraGroups = lib.optional config.networking.networkmanager.enable "networkmanager";
