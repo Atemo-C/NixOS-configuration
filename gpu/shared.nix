e1e5a82235977bb185699b4f4433e987f0bd8894
Atemo-C <c.roux1993@gmail.com>
2025-09-03 14:07:00 +0200

    Removed useless `lib.mkIf`.

--- gpu/shared.nix
+++ gpu/shared.nix
@@ -3,7 +3,7 @@
 	enable = true;
 
 	# Whether to also install 32-bit drivers for 32-bit programs.
-	enable32Bit = lib.mkIf enable true;
+	enable32Bit = true;
 
 	# Extra packages for hardware acceleration.
 	# There should be little to no conflicts when some/all are installed at once.
