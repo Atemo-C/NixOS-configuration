1d61f8626b465632635ffe74f10ff09e22468a70
Atemo-C <c.roux1993@gmail.com>
2025-09-03 15:22:44 +0200

    Added more visual settings, and now letting QT apps follow gsettings stuff.

--- theming/programs.nix
+++ theming/programs.nix
@@ -11,8 +11,16 @@
 	home-manager.users = {
 		# GTK theming for the user.
 		${config.userName} = {
-			# Disable window buttons (minimize, maximize, close…) if desired.
-			dconf.settings = { "org/gnome/desktop/wm/preferences" = { button-layout = "icon,appmenu:"; }; };
+			dconf.settings = {
+				# Disable window buttons (minimize, maximize, close…) if desired.
+				"org/gnome/desktop/wm/preferences" = { button-layout = "icon,appmenu:"; };
+
+				# Whether to automatically hide scrollbars.
+				"org/gnome/desktop/interface" = { overlay-scrolling = false; };
+
+				# Whether to show status shapes in buttons etc.
+				"org/gnome/desktop/a11y/interface" = { show-status-shapes = true; };
+			};
 
 			gtk = {
 				# Enable GTK configuration.
@@ -42,6 +50,14 @@ gtk-enable-mnemonics = 0
 					gtk-menu-images = true;
 					gtk-toolbar-style = 3;
 				};
+			};
+
+			qt = {
+				# Whether to enable QT configuration through Home Manager.
+				enable = true;
+
+				# Make QT follow GSettings where possible.
+				platformTheme = "gnome";
 			};
 		};
 
@@ -80,6 +96,5 @@ gtk-enable-mnemonics = 0
 				};
 			};
 		};
-
 	};
 }
