03fd1a3f3e8e6fe994b59edd86785ec6e430cb9e
Atemo-C <c.roux1993@gmail.com>
2025-09-03 15:44:45 +0200

    Slightly compacted.

--- uncategorized/boot.nix
+++ uncategorized/boot.nix
@@ -1,12 +1,10 @@
 { ... }: { boot.loader = {
-	limine = {
-		# Whether to enable the Limine bootloader.
-		enable = true;
+	# Whether to enable the Limine bootloader.
+	limine.enable = true;
 
-		# Maximum number of latest NixOS generations to keep in the boot menu.
-		# This can help prevent the boot partition from running out of disk space.
-		maxGenerations = 16;
-	};
+	# Maximum number of latest NixOS generations to keep in the boot menu.
+	# This can help prevent the boot partition from running out of disk space.
+	limine.maxGenerations = 16;
 
 	# Timeout in seconds until the first entry in the bootloader is activated.
 	timeout = 1;
