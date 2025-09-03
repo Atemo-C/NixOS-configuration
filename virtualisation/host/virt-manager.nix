b724982c2644d305946d642ecce80d55c3d59f44
Atemo-C <c.roux1993@gmail.com>
2025-09-03 16:39:12 +0200

    Slightly compacted.

--- virtualisation/host/virt-manager.nix
+++ virtualisation/host/virt-manager.nix
@@ -1,12 +1,10 @@
 { config, lib, ... }: {
-	virtualisation.libvirtd = {
-		# Whether to enable libvirtd, a daemon that manages virtual machines.
-		enable = true;
-	};
+	# Whether to enable libvirtd, a daemon that manages virtual machines.
+	virtualisation.libvirtd.enable = true;
 
 	# Add the user to the `libvirtd` group.
 	users.users.${config.userName}.extraGroups = lib.optional config.virtualisation.libvirtd.enable "libvirtd";
 
 	# Whether to enable the Virt Manager virtual machine manager.
-	programs.virt-manager.enable = lib.mkIf (config.virtualisation.libvirtd.enable && config.programs.niri.enable) true;
+	programs.virt-manager.enable = lib.mkIf (config.virtualisation.libvirtd.enable && config.programs.niri.enable) true
 }
