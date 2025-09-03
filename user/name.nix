05277d3e4656e7b42f65ca36c45781ea847e5712
Atemo-C <c.roux1993@gmail.com>
2025-09-03 16:18:04 +0200

    Sligthly compacted.

--- user/name.nix
+++ user/name.nix
@@ -1,28 +1,26 @@
-{ lib, ... }: {
-	options = {
-		# Define the `userName` option.
-		userName = lib.mkOption {
-			# Set the type of the module (string).
-			type = lib.types.str;
+{ lib, ... }: { options = {
+	# Define the `userName` option.
+	userName = lib.mkOption {
+		# Set the type of the module (string).
+		type = lib.types.str;
 
-			# The default name of the user account.
-			# [a-z] [A-Z] [0-9] [ - _ ]
-			default = "your-name-here";
+		# The default name of the user account.
+		# [a-z] [A-Z] [0-9] [ - _ ]
+		default = "user-name-here";
 
-			# Description of the module.
-			description = "The name of the user account. Can be overriden with `config.userName`.";
-		};
-
-		# Define the `userTitle` option.
-		userTitle = lib.mkOption {
-			# Set the type of the module (string).
-			type = lib.types.str;
-
-			# The default title of the user.
-			default = "Your much fancier name here";
-
-			# Description of the module.
-			description = "The description (title) of the user. Can be overriden with `config.userTitle`.";
-		};
+		# Description of the module.
+		description = "The name of the user account. Can be overriden with `config.userName`.";
 	};
-}
+
+	# Define the `userTitle` option.
+	userTitle = lib.mkoption {
+		# Set the type of the module (string).
+		type = lib.types.str;
+
+		# The default title of the user.
+		default = "User title here";
+
+		# Description of the module.
+		description = "The description (title) of the user. Can be overriden with `config.userTitle`.";
+	};
+};}
