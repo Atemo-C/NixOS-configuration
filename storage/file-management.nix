cd39d6c6f0689e093a25a0c809054e347f1b83bf
Atemo-C <c.roux1993@gmail.com>
2025-09-03 23:29:40 +0200

    Fixed silly behaviour (grr, nix…)

--- storage/file-management.nix
+++ storage/file-management.nix
@@ -28,20 +28,22 @@
 		xfconf.enable = thunar.enable;
 	};
 
-	# Whether to enable LSD, an alternative to the `ls` command.
-	home-manager.users.${config.userName}.programs.lsd.enable = true;
+	home-manager.users.${config.userName} = {
+		# Whether to enable LSD, an alternative to the `ls` command.
+		programs.lsd.enable = true;
 
-	# Default size of the separator between icons and normal text.
-	home-manager.users.${config.userName}.programs.lsd.settings.icons.separator = "  ";
+		# Default size of the separator between icons and normal text.
+		programs.lsd.settings.icons.separator = "  ";
 
-	# Link configuration files.
-	home-manager.users.${config.userName}.systemd.user.tmpfiles.rules = [
-		# Mimeapps configuration.
-		"L %h/.config/mimeapps.list - - - - /etc/nixos/storage/files/mimeaps.list"
-	] ++ lib.optional config.programs.thunar.enable
+		# Link configuration files.
+		systemd.user.tmpfiles.rules = [
+			# Mimeapps configuration.
+			"L %h/.config/mimeapps.list - - - - /etc/nixos/storage/files/mimeaps.list"
+		] ++ lib.optional config.programs.thunar.enable
 
-		# Custom actions for the Thunar file manager.
-		"L %h/.config/Thunar/uca.xml - - - - /etc/nixos/storage/files/thunar-custom-actions.xml";
+			# Custom actions for the Thunar file manager.
+			"L %h/.config/Thunar/uca.xml - - - - /etc/nixos/storage/files/thunar-custom-actions.xml";
+	};
 
 	# Add `lsd` shell abbreviations.
 	programs.fish.shellAbbrs = lib.mkIf (config.programs.fish.enable &&
