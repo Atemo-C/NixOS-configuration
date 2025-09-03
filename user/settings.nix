b07defc9e761c531e25adb13419b3a6b473ac02b
Atemo-C <c.roux1993@gmail.com>
2025-09-03 16:19:37 +0200

    Compacted.

--- user/settings.nix
+++ user/settings.nix
@@ -1,20 +1,10 @@
-{ config, ... }: {
-	users.users.${config.userName} = {
-		# Short description (title) of the user account, typically the user's full name.
-		description = "${config.userTitle}";
+{ config, ... }: { users.users.${config.userName} = {
+	# Short description (title) of the user account, typically the user's full name.
+	description = "${config.userTitle}";
 
-		# Axtra groups to add the user to.
-		extraGroups = [
-			"disk"
-			"input"
-			"plugdev"
-			"render"
-			"storage"
-			"video"
-			"wheel"
-		];
+	# Extra groups to add the user to.
+	extraGroups = [ "disk" "input" "plugdev" "render" "storage" "video" "wheel" ];
 
-		# Set the user as a normal user.
-		isNormalUser = true;
-	};
-}
+	# Set the user as a normal user.
+	isNormalUser = true;
+}; }
