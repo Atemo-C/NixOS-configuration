a8b718ec93752cb3a923903641b665bfe66cb5f0
Atemo-C <c.roux1993@gmail.com>
2025-09-03 16:38:13 +0200

    Compacted and simplified logic.

--- virtualisation/host/docker.nix
+++ virtualisation/host/docker.nix
@@ -1,21 +1,18 @@
-{ config, lib, ... }: {
-	virtualisation.docker = rec {
-		# Whether to enable Docker.
+{ config, lib, ... }: { virtualisation.docker = {
+	# Whether to enable Docker.
+	enable = true;
+
+	# Whether to start dockerd on boot. Required for the `--restart=always` argument to work.
+	enableOnBoot = true;
+
+	rootless = rec {
+		# Whether to enable Docker in a rootless mode.
 		enable = true;
 
-		# Whether to start dockerd on boot.
-		# Required for the `--restart=always` argument to work.
-		enableOnBoot = lib.mkIf enable true;
+		# If using a compatible NVIDIA GPU, whether to enable the CDI feature.
+		daemon.settings.features.cdi = lib.mkIf (enable && config.hardware.nvidia-container-toolkit.enable) true;
 
-		rootless = lib.mkIf enable rec {
-			# Whether to enable Docker in a rootless mode.
-			enable = true;
-
-			# If using a compatible NVIDIA GPU, whether to enable the CDI feature.
-			daemon.settings.features.cdi = lib.mkIf (enable && config.hardware.nvidia-container-toolkit.enable) true;
-
-			# Whether to point `DOCKER_HOST` to rootless Docker instance for normal users by default.
-			setSocketVariable = true;
-		};
+		# Whether to point `DOCKER_HOST` to rootless Docker instance for normal users by default.
+		setSocketVariable = true;
 	};
-}
+};}
