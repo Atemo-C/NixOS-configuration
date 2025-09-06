49449c2e8729173b20fa55d84b4633c9d880b8ee
Atemo-C <c.roux1993@gmail.com>
2025-09-06 15:36:22 +0200

    Follows styling of other modules more closely.

--- desktop/waybar.nix
+++ desktop/waybar.nix
@@ -1,15 +1,13 @@
-{ config, lib, ... }: lib.mkIf config.programs.niri.enable {
-	home-manager.users.${config.userName} = rec {
-		# Whether to enable the Waybar bar.
-		programs.waybar.enable = true;
+{ config, lib, ... }: lib.mkIf config.programs.niri.enable { home-manager.users.${config.userName} = {
+	# Whether to enable the Waybar bar.
+	programs.waybar.enable = true;
 
-		# Whether to enable Waybar systemd integration; Useful for auto-starting with the desktop.
-		programs.waybar.systemd.enable = true;
+	# Whetehr to enable Waybar systemd integration; Useful for auto-starting with the desktop.
+	programs.waybar.systemd.enable = true;
 
-		# Link the configuration files for Waybar.
-		systemd.user.tmpfiles.rules = lib.optionals programs.waybar.enable [
-			"L %h/.config/waybar/config - - - - /etc/nixos/desktop/files/waybar/config.json"
-			"L %h/.config/waybar/style.css - - - - /etc/nixos/desktop/files/waybar/style.css"
-		];
-	};
-}
+	# Link the configuration files for Waybar.
+	systemd.user.tmpfiles.rules = lib.optionals config.home-manager.users.${config.userName}.programs.waybar.enable [
+		"L %h/.config/waybar/config - - - - /etc/nixos/desktop/files/waybar/config.json"
+		"L %h/.config/waybar/style.css - - - - /etc/nixos/desktop/files/waybar/style.css"
+	];
+}; }
