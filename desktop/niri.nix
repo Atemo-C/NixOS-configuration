98aa6864d7980f5fd2daf0482302238a64284802
Atemo-C <c.roux1993@gmail.com>
2025-09-06 15:31:47 +0200

    More consistent styling with the rest of the modules.

--- desktop/niri.nix
+++ desktop/niri.nix
@@ -1,11 +1,12 @@
-{ config, lib, pkgs, ... }: let niri = config.programs.niri.enable; fish = config.programs.fish.enable; in {
+{ config, lib, pkgs, ... }: {
 	# Whether to enable the Niri Wayland compositor.
 	programs.niri.enable = true;
 
 	# Shell abbreviation for starting Niri.
-	programs.fish.shellAbbrs = lib.mkIf (niri && fish) { n = "niri-session"; };
+	programs.fish.shellAbbrs = lib.mkIf ( config.programs.niri.enable && config.programs.fish.enable)
+	{ n = "niri-session"; };
 
-	environment = lib.mkIf niri {
+	environment = lib.mkIf config.programs.niri.enable {
 		# Ozone Wayland support for Chromium and Electron-based programs.
 		sessionVariables.NIXOS_OZONE_WL = "1";
 
@@ -16,4 +17,4 @@
 	# Link the configuration file of Niri.
 	systemd.user.tmpfiles.users.${config.userName}.rules = lib.optional config.programs.niri.enable
 	"L %h/.config/niri/config.kdl - - - - /etc/nixos/desktop/files/niri.kdl";
-}
+};
