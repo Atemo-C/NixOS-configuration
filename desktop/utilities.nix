58fcc0e86eac1e4ba65a31c850e489da98a7a552
Atemo-C <c.roux1993@gmail.com>
2025-10-05 12:05:15 +0200

    `gnome-keyring` is now enabled as a service.

--- desktop/utilities.nix
+++ desktop/utilities.nix
@@ -1,7 +1,11 @@
-{ config, lib, pkgs, ... }: lib.mkIf config.programs.niri.enable { environment.systemPackages = with pkgs; [
-	cmd-polkit    # Polkit authentification utility to be used with dmenu-like menus.
-	gnome-keyring # Keyring utility from the GNOME desktop.
-	hyprpaper     # Wayland wallpaper utility.
-	nautilus      # Nautilus file manager; Only here for file picking as Niri uses the GNOME XDG Portal.
-	zenity        # Display graphical dialogs from the command-line and shell scripts.
-]; }
+{ config, lib, pkgs, ... }: lib.mkIf config.programs.niri.enable {
+	environment.systemPackages = with pkgs; [
+		cmd-polkit    # Polkit authentification utility to be used with dmenu-like menus.
+		hyprpaper     # Wayland wallpaper utility.
+		nautilus      # Nautilus file manager; Only here for file picking as Niri uses the GNOME XDG Portal.
+		zenity        # Display graphical dialogs from the command-line and shell scripts.
+	];
+
+	# Keyring utility from the GNOME desktop.
+	services.gnome.gnome-keyring.enable = true;
+};
