65e7cb7acb17a1bbef727e069a5161351e984fc1
Atemo-C <c.roux1993@gmail.com>
2025-10-05 13:36:29 +0200

    Reworked the `input/utilities.nix` module, and added `ydotool`.

--- input/utilities.nix
+++ input/utilities.nix
@@ -1,8 +1,17 @@
-{ config, lib, pkgs, ... }: lib.mkIf config.programs.niri.enable { environment.systemPackages = with pkgs; [
-	evhz          # Show mouse refresh rate.
-	evsieve       # Utility for mapping events from Linux event devices.
-	jstest-gtk    # Graphical joystic tester.
-	#sc-controller # User-mode driver and GUI for configuring any controller.
-	usbutils      # Tools for working with USB devices, such as `lsusb`.
-	xclicker      # Autoclicker for XWayland programs.
-]; }
+{ config, lib, pkgs, ... }: {
+	environment.systemPackages = with pkgs; [
+		evsieve       # Utility for mapping events from Linux event devices.
+		usbutils      # Tools for working with USB devices, such as `lsusb`.
+	] ++ lib.optionals config.programs.niri.enable (with pkgs; [
+		evhz          # Show mouse refresh rate.
+		jstest-gtk    # Graphical joystic tester.
+		#sc-controller # User-mode driver and GUI for configuring any controller.
+		xclicker      # Autoclicker for XWayland programs.
+	]);
+
+	# Whether to enable ydotool system service and ydotool for members of `programs.ydotool.group`.
+	programs.ydotool.enable = true;
+
+	# Add the user to the `ydotool` group.
+	users.users.${config.userName}.extraGroups = "ydotool";
+}
