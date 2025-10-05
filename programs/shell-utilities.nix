615b81eb7d406776fcadda5653a69c827099be78
Atemo-C <c.roux1993@gmail.com>
2025-10-05 12:27:38 +0200

    `bat` and `git` are now `enabled` instead of just installed.

--- programs/shell-utilities.nix
+++ programs/shell-utilities.nix
@@ -1,13 +1,20 @@
-{ pkgs, ... }: { environment.systemPackages = with pkgs; [
-	bat        # A `cat`(1) clone with syntax highlighting and Git integration.
-	bc         # GNU software calculator.
-	calcurse   # A calendar and scheduling application for the command line.
-	dash       # A POSIX-compliant implementation of `/bin/sh` that aims to be as small as possible.
-	gh         # GitHub CLI tool.
-	git        # Distributed version control system.
-	jq         # Lightweight and flexible command-line JSON processor.
-	parallel   # Shell tool for executing jobs in parallel.
-	shellcheck # Shell script analysis tool.
-	tlrc       # Simplified and community-driven man pages, written in Rust.
-	wget       # Tool for retrieving files using HTTP, HTTPS, and FTP.
-]; }
+{ config, pkgs, ... }: {
+	environment.systemPackages = with pkgs; [
+		bc         # GNU software calculator.
+		calcurse   # A calendar and scheduling application for the command line.
+		dash       # A POSIX-compliant implementation of `/bin/sh` that aims to be as small as possible.
+		gh         # GitHub CLI tool.
+		jq         # Lightweight and flexible command-line JSON processor.
+		parallel   # Shell tool for executing jobs in parallel.
+		shellcheck # Shell script analysis tool.
+		tlrc       # Simplified and community-driven man pages, written in Rust.
+		wget       # Tool for retrieving files using HTTP, HTTPS, and FTP.
+	];
+	programs = {
+		# Whether to enable bat, a `cat (1)` clone with syntax highlighting and Git integration.
+		bat.enable = true;
+
+		# Whether to enable git, a distributed version control system.
+		git.enable = true;
+	};
+}
