a483106256637917396f65ee013c1196f840a7e2
Atemo-C <c.roux1993@gmail.com>
2025-10-29 22:41:51 +0100

    Online music abbreviations now much shorter.

--- uncategorized/audio.nix
+++ uncategorized/audio.nix
@@ -58,24 +58,40 @@
 		};
 
 		# Shell abbreviations for listening to various online audio streams with MPV.
-		fish.shellAbbrs = lib.mkIf config.programs.fish.enable {
-			lofi-asian      = ''mpv --quiet --no-video "https://www.youtube.com/watch?v=Na0w3Mz46GA"'';
-			lofi-escape     = ''mpv --quiet --no-video "https://www.youtube.com/watch?v=S_MOd40zlYU"'';
-			lofi-guitar     = ''mpv --quiet --no-video "https://www.youtube.com/watch?v=E_XmwjgRLz8"'';
-			lofi-jazz       = ''mpv --quiet --no-video "https://www.youtube.com/watch?v=HuFYqnbVbzY"'';
-			lofi-medieval   = ''mpv --quiet --no-video "https://www.youtube.com/watch?v=IxPANmjPaek"'';
-			lofi-piano      = ''mpv --quiet --no-video "https://www.youtube.com/watch?v=TtkFsfOP9QI"'';
-			lofi-pomodoro   = ''mpv --quiet --no-video "https://www.youtube.com/watch?v=1oDrJba2PSs"'';
-			lofi-pov        = ''mpv --quiet --no-video "https://www.youtube.com/watch?v=uFlzUaisbig"'';
-			lofi-purr       = ''mpv --quiet --no-video "https://www.youtube.com/watch?v=xORCbIptqcc"'';
-			lofi-rain       = ''mpv --quiet --no-video "https://www.youtube.com/watch?v=-OekvEFm1lo"'';
-			lofi-sad        = ''mpv --quiet --no-video "https://www.youtube.com/watch?v=P6Segk8cr-c"'';
-			lofi-sleep      = ''mpv --quiet --no-video "https://www.youtube.com/watch?v=28KRPhVzCus"'';
-			lofi-study      = ''mpv --quiet --no-video "https://www.youtube.com/watch?v=jfKfPfyJRdk"'';
-			lofi-summer     = ''mpv --quiet --no-video "https://www.youtube.com/watch?v=SXySxLgCV-8"'';
-			lofi-synthwave  = ''mpv --quiet --no-video "https://www.youtube.com/watch?v=4xDzrJKXOOY"'';
-			nightinthewoods = ''mpv --quiet --no-video "https://www.youtube.com/watch?v=AsLKfqA73uE"'';
-			tunic           = ''mpv --quiet --no-video "https://www.youtube.com/watch?v=gzWd5hjcaPo"'';
-		};
+		fish.shellAbbrs = lib.mkIf config.programs.fish.enable (
+			let
+				# Base MPV command.
+				mpvcmd = ''mpv --quiet --no-video "https://www.youtube.com/watch?v="'';
+
+				# List of audio streams.
+				streams = [
+					{ name = "lofi-asian";      id = "Na0w3Mz46GA"; }
+					{ name = "lofi-escape";     id = "S_MOd40zlYU"; }
+					{ name = "lofi-guitar";     id = "E_XmwjgRLz8"; }
+					{ name = "lofi-jazz";       id = "HuFYqnbVbzY"; }
+					{ name = "lofi-medieval";   id = "IxPANmjPaek"; }
+					{ name = "lofi-piano";      id = "TtkFsfOP9QI"; }
+					{ name = "lofi-pomodoro";   id = "1oDrJba2PSs"; }
+					{ name = "lofi-pov";        id = "uFlzUaisbig"; }
+					{ name = "lofi-purr";       id = "xORCbIptqcc"; }
+					{ name = "lofi-rain";       id = "-OekvEFm1lo"; }
+					{ name = "lofi-sad";        id = "P6Segk8cr-c"; }
+					{ name = "lofi-sleep";      id = "28KRPhVzCus"; }
+					{ name = "lofi-study";      id = "jfKfPfyJRdk"; }
+					{ name = "lofi-summer";     id = "SXySxLgCV-8"; }
+					{ name = "lofi-synthwave";  id = "4xDzrJKXOOY"; }
+					{ name = "nightinthewoods"; id = "AsLKfqA73uE"; }
+					{ name = "tunic";           id = "gzWd5hjcaPo"; }
+				];
+
+				# Generate the full abbreviations.
+				abbrs = builtins.listToAttrs (
+					map (s: {
+						name = s.name;
+						value = ''${mpvcmd}${s.id}"'';
+					}) streams
+				);
+			in abbrs
+		);
 	};
 }
