fd4543ef6c68711f156d1c98779f13762248e542
Atemo-C <c.roux1993@gmail.com>
2025-12-13 15:20:31 +0100

    Updated Vintage Story to `1.21.6`.

--- uncategorized/temporary.nix
+++ uncategorized/temporary.nix
@@ -53,15 +53,16 @@
 		})
 
 		# Updated Vintagestory version.
-#		(self: super: {
-#			vintagestory = super.vintagestory.overrideAttrs (oldAttrs: rec {
-#				version = "1.21.5";
-#				src = super.fetchurl {
-#					url = "https://cdn.vintagestory.at/gamefiles/stable/vs_client_linux-x64_${version}.tar.gz";
-#					hash = "sha256-dG1D2Buqht+bRyxx2ie34Z+U1bdKgi5R3w29BG/a5jg=";
-#				};
-#			});
-#		})
+		# https://github.com/NixOS/nixpkgs/issues/470441
+		(self: super: {
+			vintagestory = super.vintagestory.overrideAttrs (oldAttrs: rec {
+				version = "1.21.6";
+				src = super.fetchurl {
+					url = "https://cdn.vintagestory.at/gamefiles/stable/vs_client_linux-x64_${version}.tar.gz";
+					hash = "sha256-i1IsxT+pjkKbJjHwnMcyb12nxe21mwvnoRGHMabMETY=";
+				};
+			});
+		})
 	];
 
 #	# [Fixed] https://github.com/NixOS/nixpkgs/issues/361592
