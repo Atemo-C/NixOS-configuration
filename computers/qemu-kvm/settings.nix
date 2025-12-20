b05644f76b72a3c00af3beb6d7b133f026645712
Atemo-C <c.roux1993@gmail.com>
2025-12-20 13:58:58 +0100

    Added a settings module for QEMU+KVM guest virtual machines.

---
+++ computers/qemu-kvm/settings.nix
@@ -0,0 +1,48 @@
+{ ... }: {
+	boot = {
+		# Define the LUKS-encrypted storage devices (root and swap).
+		initrd.luks.devices = {
+			root.device = "/dev/disk/by-uuid/UUID-HERE";
+			swap.device = "/dev/disk/by-uuid/UUID-HERE";
+		};
+
+		# Whether to let the installation process modify EFI boot variables.
+		# If you have errors when updating NixOS after it has been installed,
+		# as in, the bootloader fails to "install" again, it is safe to turn this off.
+		loader.efi.canTouchVariables = true;
+	};
+
+	# Filesystem options.
+	fileSystems = {
+		"/".options = [ "compress=zstd:3" ];
+		"/home".options = [ "compress=zstd:3" ];
+		"/nix"options = [ "compress=zstd:3" "noatime" ];
+	};
+
+	# Set the computer's name on the network.
+	networking.hostName = "nixos-vm";
+
+	services = {
+		# Whether to enable the Spice guest vdagent daemon.
+		spice-vdagentd.enable = true;
+
+		# Whether to enable the QEMU guest agent.
+		qemuGuest.enable = true;
+
+		# Keyboard layout settings.
+		# To see a complete list of layouts, variants, and other settings:
+		# • https://gist.github.com/jatcwang/ae3b7019f219b8cdc6798329108c9aee
+		#
+		# To see why this list cannot easily be seen within NixOS:
+		# • https://github.com/NixOS/nixpkgs/issues/254523
+		# • https://github.com/NixOS/nixpkgs/issues/286283
+		xserver.xkb = {
+			layout = "us,fr";
+			variant = "intl,";
+		};
+	};
+
+	# Disable the ModemManager service to improve boto times and save resources.
+	# Only enable if using cellular data (tethering from a mobile device does not count).
+	systemd.services.ModemManager.enable = false;
+}
