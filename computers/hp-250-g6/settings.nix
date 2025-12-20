bdc1314dd99ecbdc9a605ad1d16b9c570ec8181d
Atemo-C <c.roux1993@gmail.com>
2025-12-20 13:57:12 +0100

    Added configuration for my HP 250 G6 laptop.

---
+++ computers/hp-250-g6/settings.nix
@@ -0,0 +1,40 @@
+{ ... }: {
+	boot = {
+		# Define the LUKS-encrypted storage devices (root and swap).
+		initrd.luks.devices = {
+			root.device = "/dev/disk/by-uuid/80ef44a6-7ee0-4001-bc09-7b5fcd11b46e";
+			swap.device = "/dev/disk/by-uuid/56c4a6c9-e2d5-4b02-a13d-45ad9302a19e";
+		};
+
+		# Whether to let the installation process modify EFI boot variables.
+		# If you have errors when updating NixOS after it has been installed,
+		# as in, the bootloader fails to "install" again, it is safe to turn this off.
+		loader.efi.canTouchEfiVariables = false;
+	};
+
+	# Filesystem options.
+	fileSystems = {
+		"/".options = [ "compress=zstd:3" ];
+		"/home".options = [ "compress=zstd:3" ];
+		"/nix"options = [ "compress=zstd:3" "noatime" ];
+	};
+
+	# Set the computer's name on the network.
+	networking.hostName = "HP-250-G6";
+
+	# Keyboard layout settings.
+	# To see a complete list of layouts, variants, and other settings:
+	# • https://gist.github.com/jatcwang/ae3b7019f219b8cdc6798329108c9aee
+	#
+	# To see why this list cannot easily be seen within NixOS:
+	# • https://github.com/NixOS/nixpkgs/issues/254523
+	# • https://github.com/NixOS/nixpkgs/issues/286283
+	services.xserver.xkb = {
+		layout = "fr,us";
+		variant = ",intl";
+	};
+
+	# Disable the ModemManager service to improve boto times and save resources.
+	# Only enable if using cellular data (tethering from a mobile device does not count).
+	systemd.services.ModemManager.enable = false;
+}
