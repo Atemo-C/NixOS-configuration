aeb78cf8a716c76a94ff91911cb9dc4b6f5a29a5
Atemo-C <c.roux1993@gmail.com>
2026-02-03 21:09:06 +0100

    Removed graphics packages, since they are configured in the device's `settings.nix` module.

--- gpu/shared.nix
+++ gpu/shared.nix
@@ -4,14 +4,4 @@
 
 	# Whether to also install 32-bit drivers for 32-bit programs.
 	enable32Bit = true;
-
-	# Extra packages for hardware acceleration.
-	# There should be little to no conflicts when some/all are installed at once.
-	# Still, it is recommended to instead configure this in your device's `settings.nix` module.
-	extraPackages = lib.optionals config.hardware.graphics.enable (with pkgs; [
-#		intel-media-driver    # VA-API for modern Intel GPUs.
-#		intel-vaapi-driver    # VA-API for older Intel GPUs using the i965 driver.
-#		intel-compute-runtime # OpenCL driver for Intel GPUs.
-#		rocmPackages.clr.icd  # OpenCL driver for AMD GPUs.
-	]);
 }; }
