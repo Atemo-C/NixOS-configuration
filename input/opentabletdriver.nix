52f7042e587cd815b04961bca73ba4a4200a54f7
Atemo-C <c.roux1993@gmail.com>
2026-02-03 21:11:47 +0100

    Removed useless option.

--- input/opentabletdriver.nix
+++ input/opentabletdriver.nix
@@ -1,9 +1,3 @@
-{ ... }: { hardware.opentabletdriver = {
-	# Whether to enable OpenTabletDriver.
-	# This automatically replaces the default graphic tablet drivers if conflicting.
-	enable = true;
-
-	# Whether to enable OpenTabletDriver systemd integration.
-	# This allows it to automatically start with the desktop.
-	systemd.enable = true;
-}; }
+# Whether to enable OpenTabletDriver udev rules, user service,
+# and blacklist kernel modules known to conflict with OpenTabletDriver.
+{ ... }: { hardware.opentabletdriver.enable = true; }
