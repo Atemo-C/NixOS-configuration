527cc6af0d3bc2362a19258f116ecbff2fb1620c
Atemo-C <c.roux1993@gmail.com>
2026-02-03 21:01:48 +0100

    Simplifed/shortened the module to fit the styling of other extra Nix modules.

--- extra-modules/atemo/config/username.nix
+++ extra-modules/atemo/config/username.nix
@@ -1,26 +1,13 @@
 { lib, ... }: { options = {
-	# Define the `userName` option.
 	userName = lib.mkOption {
-		# Set the type of the module (string).
 		type = lib.types.str;
-
-		# The default name of the user account.
-		# [a-z] [A-Z] [0-9] [ - _ ]
-		default = "atemo";
-
-		# Description of the module.
-		description = "The name of the user account. [a-z] [A-Z] [0-9] [ - _ ]";
+		default = "user-name";
+		description = "The name of the user account. [a-Z] [0-9] [-_]";
 	};
 
-	# Define the `userTitle` option.
 	userTitle = lib.mkOption {
-		# Set the type of the module (string).
 		type = lib.types.str;
-
-		# The default title of the user.
-		default = "Atemo Cajaku";
-
-		# Description of the module.
-		description = "The description (title) of the user.";
+		default = "Fancy user name";
+		description = "The description (title, fancier name) of the user.";
 	};
-};}
+}; }
