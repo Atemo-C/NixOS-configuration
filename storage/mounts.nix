5386b8b4ab5f4695f74a98446f646321e247b3c1
Atemo-C <c.roux1993@gmail.com>
2026-02-06 00:49:48 +0100

    Added the `mounts.nix` and `160GB-HDD.nix` modules.

---
+++ storage/mounts.nix
@@ -0,0 +1,15 @@
+{ config, ... }: {
+	fileSystems = {
+		# Mount for 1TB Toshiba DT01ACA100 7200RPM HDD.
+		"/run/media/${config.userName}/1TB-GPT-TOSHIBA" = {
+			device  = "/dev/disk/by-id/ata-TOSHIBA_DT01ACA100_448164BNS-part1";
+			fsType  = "btrfs";
+			options = [ "defaults" "rw" "nofail" "users" "exec" ];
+		};
+	};
+
+	# Import more advanced mount options for specific storage devices.
+	imports = [
+		"/etc/nixos/storage/160GB-HDD.nix"
+	];
+}
