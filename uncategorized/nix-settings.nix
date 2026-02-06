ab7dcd3f3c5f6433b7838c6dfd2230afb68c013d
Atemo-C <c.roux1993@gmail.com>
2026-02-06 20:05:06 +0100

    Added the `nix-settings.nix` module.

---
+++ uncategorized/nix-settings.nix
@@ -0,0 +1,24 @@
+{ config, ... }: {
+	# Which version of NixOS was initially installed on the current system.
+	# There is no need to change it post-installation, even when upgrading to a newer NixOS version.
+	# Only change it if you are fully re-installing with a different version.
+	system.stateVersion = "25.11";
+
+	nix.settings = {
+		# Whether to hard-link files in the store that have identical contents.
+		# This helps save disk space.
+		auto-optimise-store = true;
+
+		# Increase the size of the download buffer for Nix.
+		download-buffer-size = 629145600;
+
+		# Enable the nix-command feature.
+		experimental-features = [ "nix-command" ];
+	};
+
+	# Make the `/etc/nixos/` directory and its files owned by the user.
+	# This is for easier editing; Remove if this is a security concern for you.
+	# Note that some scripts and other functionaly of this configuration relies on
+	# this staying as it is; It will be up to you to adapt them.
+	systemd.tmpfiles.rules = [ "Z /etc/nixos 0755 ${config.userName} users - -"];
+}
