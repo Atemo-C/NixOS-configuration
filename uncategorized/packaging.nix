20968d16b7d6ed57faac655542c602858c31b04a
Atemo-C <c.roux1993@gmail.com>
2026-02-06 20:15:41 +0100

    Added the `packaging.nix` module.

---
+++ uncategorized/packaging.nix
@@ -0,0 +1,29 @@
+{ config, lib, pkgs, ... }: rec {
+	# Whether to allow installation of unfree (proprietary) software.
+	nixpkgs.config.allowUnfree = true;
+
+	# If the above is enabled, also apply this rule to temporary nix shell instances.
+	environment.sessionVariables.NIXPKGS_ALLOW_UNFREE = lib.mkIf nixpkgs.config.allowUnfree "1";
+
+	# Whether to enable the Flatpak packaging system.
+	# The GTK desktop needs to be configured; However, Niri does it already for us here.
+	services.flatpak.enable = true;
+
+	# Import the Nix Flatpak module if using Flatpaks.
+	imports = lib.mkIf services.flatpak.enable [ /etc/nixos/extra-modules/external/nix-flatpak.nix ];
+
+	# Add shell abbreviations for managing Flatpak.
+	programs.fish.shellAbbrs = lib.mkIf services.flatpak.enable rec {
+		# Upgrade flatpaks and remove unsused ones.
+		update-flatpak  = "flatpak-update -y && flatpak remove --unsused -y";
+		flatpak-update  = update-flatpak;
+		upgrade-flatpak = update-flatpak;
+		flatpak-upgrade = update-flatpak;
+
+		# Removing flatpaks.
+		flatpak-remove = "flatpak remove";
+		remove-flatpak = flatpak-remove;
+		flatpak-purge  = "flatpak remove --delete-data";
+		purge-flatpak  = flatpak-purge;
+	};
+}
