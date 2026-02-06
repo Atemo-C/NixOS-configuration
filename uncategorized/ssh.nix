9b3b35b510653f2043413c2f282d6de8fc7d2401
Atemo-C <c.roux1993@gmail.com>
2026-02-06 21:38:06 +0100

    Added the `ssh.nix` module.

---
+++ uncategorized/ssh.nix
@@ -0,0 +1,16 @@
+{ config, ... }: { services.openssh = {
+	# Whether to enable the OpenSSH daemon.
+	enable = true;
+
+	# Which ports the SSH daemon sohuld listen to.
+	ports = [ 6321 ];
+
+	# Login is allowed only for the listed users.
+	settings.AllowUsers = [ "${config.userName}" ];
+
+	# Whether the root user can login with SSH.
+	settings.PermitRootLogin = "no";
+
+	# Whether to only start an instance for each incoming connection.
+	startWhenNeeded = true;
+}; }
