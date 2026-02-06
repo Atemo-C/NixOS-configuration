e5aa29fb2c4284b165f072f37eb5f58e6fc477a0
Atemo-C <c.roux1993@gmail.com>
2026-02-06 12:59:28 +0100

    Added the `icons.nix` and `settings.nix` theming modules.

---
+++ theming/icons.nix
@@ -0,0 +1,77 @@
+{ config, pkgs, ... }: { home-manager.users = {
+	# Icon and cursor theming for the normal user.
+	${config.userName} = rec {
+		home.pointerCursor = {
+			# Whether to enable GTK configuration generation for `home.pointerCursor`.
+			gtk.enable = true;
+
+			# Package providing the cursor theme.
+			package = pkgs.bibata-cursors;
+
+			# The cursor name within the package.
+			name = "Bibata-Modern-Ice";
+
+			# The cursor size.
+			size = 24;
+		};
+
+		gtk = {
+			cursorTheme = {
+				# Package providing the cursor theme.
+				package = home.pointerCursor.package;
+
+				# The cursor name within the package.
+				name = home.pointerCursor.name;
+
+				# The cursor size.
+				size = home.pointerCursor.size;
+			};
+
+			iconTheme = {
+				# Package providing the icon theme.
+				package = pkgs.flat-remix-icon-theme;
+
+				# The name of the icon theme within the package.
+				name = "Flat-Remix-Blue-Dark";
+			};
+		};
+	};
+
+	# Icon and cursor themnig for the root user.
+	root = rec {
+		home.pointerCursor = {
+			# Whether to enable GTK configuration generation for `home.pointerCursor`.
+			gtk.enable = true;
+
+			# Package providing the cursor theme.
+			package = pkgs.bibata-cursors;
+
+			# The cursor name within the package.
+			name = "Bibata-Modern-Amber";
+
+			# The cursor size.
+			size = 24;
+		};
+
+		gtk = {
+			cursorTheme = {
+				# Package providing the cursor theme.
+				package = home.pointerCursor.package;
+
+				# The cursor name within the package.
+				name = home.pointerCursor.name;
+
+				# The cursor size.
+				size = home.pointerCursor.size;
+			};
+
+			iconTheme = {
+				# Package providing the icon theme.
+				package = pkgs.flat-remix-icon-theme;
+
+				# The name of the icon theme within the package.
+				name = "Flat-Remix-Red-Dark";
+			};
+		};
+	};
+}; }
---
+++ theming/settings.nix
@@ -0,0 +1,100 @@
+{ config, lib, pkgs, ... }: {
+	environment.sessionVariables = {
+		# Tell Adwaita-based programs to prefer a dark theme.
+		ADW_DEBUG_COLOR_SCHEME = "prefer-dark";
+		#HDY_DEBUG_COLOR_SCHEME = "prefer-dark";
+
+		# Tell QT-based programs to use the GTK3 platformtheme.
+		QT_QPA_PLATFORMTHEME = "gtk3";
+	};
+
+	home-manager.users = {
+		# GTK theming for the normal user.
+		${config.userName} = {
+			dconf.settings = {
+				# Disable window buttons (minimize, maximize, close).
+				"org/gnome/desktop/wm/preferences" = lib.mkIf config.programs.niri.enable { button-layout = "icon,appmenu"; };
+
+				# Whether to automatically hide scrollbars.
+				"org/gnome/desktop/interface" = { overlay-scrolling = false; };
+
+				# Whether to show status shapes in buttons etc.
+				"org/gnome/desktop/a11y/interface" = { show-status-shapes = true; };
+			};
+
+			gtk = {
+				# Whether to enable GTK configuration.
+				enable = true;
+
+				theme = {
+					# Package providing the theme.
+					package = pkgs.flat-remix-gtk;
+
+					# The name of the theme to use within the package.
+					name = "Flat-Remix-GTK-Blue-Darkest-Solid";
+				};
+
+				# Extra configuration for GTK2 programs.
+				gtk2.extraConfig = "
+					gtk-primary-button-wraps-slider = 1
+					gtk-toolbar-style = 3
+					gtk-menu-images = 1
+					gtk-button-images = 1
+					gtk-enable-mnemonics = 0
+				";
+
+				# Extra configuration for GTK3 programs.
+				gtk3.extraConfig = {
+					gtk-application-prefer-dark-theme = true;
+					gtk-button-images = true;
+					gtk-menu-images = true;
+					gtk-toolbar-style = 3;
+				};
+			};
+		};
+
+		# GTK theming for the root user.
+		root = {
+			dconf.settings = {
+				# Disable window buttons (minimize, maximize, close).
+				"org/gnome/desktop/wm/preferences" = lib.mkIf config.programs.niri.enable { button-layout = "icon,appmenu"; };
+
+				# Whether to automatically hide scrollbars.
+				"org/gnome/desktop/interface" = { overlay-scrolling = false; };
+
+				# Whether to show status shapes in buttons etc.
+				"org/gnome/desktop/a11y/interface" = { show-status-shapes = true; };
+			};
+
+			gtk = {
+				# Whether to enable GTK configuration.
+				enable = true;
+
+				theme = {
+					# Package providing the theme.
+					package = pkgs.flat-remix-gtk;
+
+					# The name of the theme to use within the package.
+					name = "Flat-Remix-GTK-Red-Darkest-Solid";
+				};
+
+				# Extra configuration for GTK2 programs.
+				gtk2.extraConfig = "
+					gtk-primary-button-wraps-slider = 1
+					gtk-toolbar-style = 3
+					gtk-menu-images = 1
+					gtk-button-images = 1
+					gtk-enable-mnemonics = 0
+				";
+
+				# Extra configuration for GTK3 programs.
+				gtk3.extraConfig = {
+					gtk-application-prefer-dark-theme = true;
+					gtk-button-images = true;
+					gtk-menu-images = true;
+					gtk-toolbar-style = 3;
+				};
+			};
+		};
+	};
+}
