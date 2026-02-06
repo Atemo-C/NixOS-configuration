fd1c89a7a1bc1305b1e5b45b6d0adf9cf860c912
Atemo-C <c.roux1993@gmail.com>
2026-02-06 22:06:16 +0100

    Added the virtualisation host modules.

---
+++ user/virtualisation/virt-manager.nix
@@ -0,0 +1,10 @@
+{ config, lib, pkgs, ... }: {
+	# Whether to enable libvirtd, a daemon that manages virtual machines.
+	virtualisation.libvirtd.enable = true;
+
+	# Add the user to the `libvirtd` group.
+	users.users.${config.userName}.extraGroups = lib.optional config.virtualisation.libvirtd.enable "libvirtd";
+
+	# Whether to enable the Virt Manager virtual machine manager.
+	programs.virt-manager.enable = lib.mkIf config.virtualisation.libvirtd.enable true;
+}
