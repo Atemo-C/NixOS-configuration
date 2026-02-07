f8dc4491440a235f118048c21706a30cacee7341
Atemo-C <c.roux1993@gmail.com>
2026-02-07 01:56:42 +0100

    Changed the state version, since this now assumes a fresh 26.05 installation.

--- uncategorized/nix-settings.nix
+++ uncategorized/nix-settings.nix
@@ -2,7 +2,7 @@
 	# Which version of NixOS was initially installed on the current system.
 	# There is no need to change it post-installation, even when upgrading to a newer NixOS version.
 	# Only change it if you are fully re-installing with a different version.
-	system.stateVersion = "25.11";
+	system.stateVersion = "26.05";
 
 	nix.settings = {
 		# Whether to hard-link files in the store that have identical contents.
