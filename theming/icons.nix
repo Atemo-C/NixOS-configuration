2556e233b1ecb239cc42f56b2c1641c0aae993b1
Atemo-C <c.roux1993@gmail.com>
2026-02-07 22:24:25 +0100

    Added the `ICON_THEME_PATH` environment variable.

--- theming/icons.nix
+++ theming/icons.nix
@@ -1,77 +1,82 @@
-{ config, pkgs, ... }: { home-manager.users = {
-	# Icon and cursor theming for the normal user.
-	${config.userName} = rec {
-		home.pointerCursor = {
-			# Whether to enable GTK configuration generation for `home.pointerCursor`.
-			gtk.enable = true;
+{ config, pkgs, ... }: {
+	home-manager.users = {
+		# Icon and cursor theming for the normal user.
+		${config.userName} = rec {
+			home.pointerCursor = {
+				# Whether to enable GTK configuration generation for `home.pointerCursor`.
+				gtk.enable = true;
 
-			# Package providing the cursor theme.
-			package = pkgs.bibata-cursors;
-
-			# The cursor name within the package.
-			name = "Bibata-Modern-Ice";
-
-			# The cursor size.
-			size = 24;
-		};
-
-		gtk = {
-			cursorTheme = {
 				# Package providing the cursor theme.
-				package = home.pointerCursor.package;
+				package = pkgs.bibata-cursors;
 
 				# The cursor name within the package.
-				name = home.pointerCursor.name;
+				name = "Bibata-Modern-Ice";
 
 				# The cursor size.
-				size = home.pointerCursor.size;
+				size = 24;
 			};
 
-			iconTheme = {
-				# Package providing the icon theme.
-				package = pkgs.flat-remix-icon-theme;
+			gtk = {
+				cursorTheme = {
+					# Package providing the cursor theme.
+					package = home.pointerCursor.package;
 
-				# The name of the icon theme within the package.
-				name = "Flat-Remix-Blue-Dark";
+					# The cursor name within the package.
+					name = home.pointerCursor.name;
+
+					# The cursor size.
+					size = home.pointerCursor.size;
+				};
+
+				iconTheme = {
+					# Package providing the icon theme.
+					package = pkgs.flat-remix-icon-theme;
+
+					# The name of the icon theme within the package.
+					name = "Flat-Remix-Blue-Dark";
+				};
+			};
+		};
+
+		# Icon and cursor themnig for the root user.
+		root = rec {
+			home.pointerCursor = {
+				# Whether to enable GTK configuration generation for `home.pointerCursor`.
+				gtk.enable = true;
+
+				# Package providing the cursor theme.
+				package = pkgs.bibata-cursors;
+
+				# The cursor name within the package.
+				name = "Bibata-Modern-Amber";
+
+				# The cursor size.
+				size = 24;
+			};
+
+			gtk = {
+				cursorTheme = {
+					# Package providing the cursor theme.
+					package = home.pointerCursor.package;
+
+					# The cursor name within the package.
+					name = home.pointerCursor.name;
+
+					# The cursor size.
+					size = home.pointerCursor.size;
+				};
+
+				iconTheme = {
+					# Package providing the icon theme.
+					package = pkgs.flat-remix-icon-theme;
+
+					# The name of the icon theme within the package.
+					name = "Flat-Remix-Red-Dark";
+				};
 			};
 		};
 	};
 
-	# Icon and cursor themnig for the root user.
-	root = rec {
-		home.pointerCursor = {
-			# Whether to enable GTK configuration generation for `home.pointerCursor`.
-			gtk.enable = true;
-
-			# Package providing the cursor theme.
-			package = pkgs.bibata-cursors;
-
-			# The cursor name within the package.
-			name = "Bibata-Modern-Amber";
-
-			# The cursor size.
-			size = 24;
-		};
-
-		gtk = {
-			cursorTheme = {
-				# Package providing the cursor theme.
-				package = home.pointerCursor.package;
-
-				# The cursor name within the package.
-				name = home.pointerCursor.name;
-
-				# The cursor size.
-				size = home.pointerCursor.size;
-			};
-
-			iconTheme = {
-				# Package providing the icon theme.
-				package = pkgs.flat-remix-icon-theme;
-
-				# The name of the icon theme within the package.
-				name = "Flat-Remix-Red-Dark";
-			};
-		};
-	};
-}; }
+	# Define the ICON_THEME_PATH environment variable.
+	environment.variables.ICON_THEME_PATH="${config.home-manager.users.${config.userName}.home.homeDirectory}/.nix-profile/share/icons/${config.home-manager.users.${config.userName}.gtk.iconTheme.name}/";
+}
