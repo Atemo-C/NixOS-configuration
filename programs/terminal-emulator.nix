a7a610cd6c573a961d8f65945de41c6009513839
Atemo-C <c.roux1993@gmail.com>
2026-02-07 23:53:46 +0100

    Added `xdg-terminal-exec`.

--- programs/terminal-emulator.nix
+++ programs/terminal-emulator.nix
@@ -1,21 +1,27 @@
-{ config, ... }: { programs.foot = {
-	# Whether to enable the Foot terminal emulator.
-	enable = true;
+{ config, ... }: { programs = {
+	foot = {
+		# Whether to enable the Foot terminal emulator.
+		enable = true;
 
-	# Foot's settigs.
-	settings = {
-		# Opacity of the window background.
-		# The colorscheme can be edited in the `/etc/nixos/theming/terminal-colors.nix` module.
-		colors.alpha = "0.9";
+		# Foot's settigs.
+		settings = {
+			# Opacity of the window background.
+			# The colorscheme can be edited in the `/etc/nixos/theming/terminal-colors.nix` module.
+			colors.alpha = "0.9";
 
-		# Use the default monospace font with a custom size.
-		# Fonts are handled in the `/etc/nixos/theming/fonts.nix` module.
-		main.font = "monospace:size=12";
+			# Use the default monospace font with a custom size.
+			# Fonts are handled in the `/etc/nixos/theming/fonts.nix` module.
+			main.font = "monospace:size=12";
 
-		# Modified search key binding that feels more natural.
-		key-bindings.search-start = "Control+Shift+f";
+			# Modified search key binding that feels more natural.
+			key-bindings.search-start = "Control+Shift+f";
 
-		# NUmber of lines to keep in memory.
-		scrollback.lines = 10000;
+			# NUmber of lines to keep in memory.
+			scrollback.lines = 10000;
+		};
 	};
+
+	# Whether to enable `xdg-terminal-exec`;
+	# The proposed Default Terminal Execution Specification.
+	xdg.terminal-exec.enable = true;
 }; }
