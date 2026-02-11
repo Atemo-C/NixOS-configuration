c979d7f3ad3c6a2cb44e5ebc7ecb6d3d3742e6b1
Atemo-C <c.roux1993@gmail.com>
2026-02-11 23:06:54 +0100

    Added a `.desktop` file for the calculator script.

--- programs/accessories.nix
+++ programs/accessories.nix
@@ -1,7 +1,14 @@
-{ ... }: { programs = {
-	# Mind-mapping utility.
-	minder.install = true;
+{ ... }: {
+	programs = {
+		# Mind-mapping utility.
+		minder.install = true;
 
-	# Offline password manager.
-	keepassxc.install = true;
-}; }
+		# Offline password manager.
+		keepassxc.install = true;
+	};
+
+	# Link the calculator's .desktop file.
+	systemd.user.tmpfiles.users.${config.userName}.rules = [
+		"L %h/.local/share/applications/atemo-calculator.desktop - - - - /etc/nixos/files/atemo-calculator.desktop"
+	];
+}
