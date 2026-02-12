94d31f2f80547347d33162c75f792d7a45d51663
Atemo-C <c.roux1993@gmail.com>
2026-02-12 23:40:31 +0100

    Added mount options for my 1TB Toshiba HDD.

---
+++ storage/Toshiba-1TB-HDD.nix
@@ -0,0 +1,30 @@
+# https://blog.pankajraghav.com/2024/09/17/AUTOMOUNT.html
+# https://github.com/NixOS/nixpkgs/issues/74281
+# Thank you!
+{ config, ... }: {
+	# Mount for encrypted 1TB HDD.
+	fileSystems."/run/media/${config.userName}/Toshiba-1TB-HDD";
+	fsType = "btrfs";
+	options = [
+		"defaults"
+		"rw"
+		"nofail"
+		"noauto"
+		"users"
+		"exec"
+		"compress=zstd:3"
+		"x-systemd.automount"
+		"x-systemd.device-timeout=8"
+	];
+
+	# Non-essential encrypted drive (boot will not fail if these are not present).
+	environment.etc.crypttab.text = ''
+		Toshiba-1TB-HDD /dev/disk/by-uuid/3927d66d-0fb7-46f5-9aa9-e6363be5dfae none luks,nofail,noauto
+	'';
+
+	# Udev rules to unlock the encrypted drive when detected.
+	services.udev.extraRules = ''
+		SUBSYSTEM=="block" ENV{ID_WWN}=="0x5000039ffbc08b6c",\
+		ENV{SYSTEMD_WANTS}="systemd-cryptsetup@Toshiba-1TB-HDD.service"
+	'';
+}
--- storage/mounts.nix
+++ storage/mounts.nix
@@ -1,15 +1,16 @@
 { config, ... }: {
-	fileSystems = {
-		# Mount for 1TB Toshiba DT01ACA100 7200RPM HDD.
-		"/run/media/${config.userName}/1TB-GPT-TOSHIBA" = {
-			device  = "/dev/disk/by-id/ata-TOSHIBA_DT01ACA100_448164BNS-part1";
-			fsType  = "btrfs";
-			options = [ "defaults" "rw" "nofail" "users" "exec" ];
-		};
-	};
+#	fileSystems = {
+#		# Mount for 1TB Toshiba DT01ACA100 7200RPM HDD.
+#		"/run/media/${config.userName}/1TB-GPT-TOSHIBA" = {
+#			device  = "/dev/disk/by-id/ata-TOSHIBA_DT01ACA100_448164BNS-part1";
+#			fsType  = "btrfs";
+#			options = [ "defaults" "rw" "nofail" "users" "exec" ];
+#		};
+#	};
 
 	# Import more advanced mount options for specific storage devices.
 	imports = [
 		./160GB-HDD.nix
+		./Toshiba-1TB-HDD
 	];
 }
