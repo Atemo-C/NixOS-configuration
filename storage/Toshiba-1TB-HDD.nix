58ab402b93db941405a063fc6bf76f30ef8eb091
Atemo-C <c.roux1993@gmail.com>
2026-02-13 01:31:24 +0100

    Added missing UUID.

--- storage/Toshiba-1TB-HDD.nix
+++ storage/Toshiba-1TB-HDD.nix
@@ -4,6 +4,7 @@
 { config, ... }: {
 	# Mount for encrypted 1TB HDD.
 	fileSystems."/run/media/${config.userName}/Toshiba-1TB-HDD" = {
+		device = "/dev/disk/by-uuid/3927d66d-0fb7-46f5-9aa9-e6363be5dfae";
 		fsType = "btrfs";
 		options = [
 			"defaults"
