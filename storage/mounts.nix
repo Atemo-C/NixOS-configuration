aabeff74e6ee9257ac91b7a3942126ccf950235e
Atemo-C <c.roux1993@gmail.com>
2026-02-13 17:55:49 +0100

    Added mount for my 500GB HDD.

--- storage/mounts.nix
+++ storage/mounts.nix
@@ -1,16 +1,5 @@
-{ config, ... }: {
-#	fileSystems = {
-#		# Mount for 1TB Toshiba DT01ACA100 7200RPM HDD.
-#		"/run/media/${config.userName}/1TB-GPT-TOSHIBA" = {
-#			device  = "/dev/disk/by-id/ata-TOSHIBA_DT01ACA100_448164BNS-part1";
-#			fsType  = "btrfs";
-#			options = [ "defaults" "rw" "nofail" "users" "exec" ];
-#		};
-#	};
-
-	# Import more advanced mount options for specific storage devices.
-	imports = [
-		./160GB-HDD.nix
-		./Toshiba-1TB-HDD
-	];
-}
+{ config, ... }: { imports = [
+	./160GB-HDD.nix
+	./Barracuda-500GB-HDD.nix
+	./Toshiba-1TB-HDD
+]; }
