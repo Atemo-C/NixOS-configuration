f6db566e4f52b1addadfd0cb17a55b439de39c0d
Atemo-C <c.roux1993@gmail.com>
2026-03-09 18:43:20 +0100

    Initial text for `configuration.nix`.

---
+++ configuration.nix
@@ -0,0 +1,8 @@
+# If you have not already, please read this configuration's README.
+#──────────────────────────────────────────────────────────────────
+#
+# This module imports all other relevant modules used by the system.
+# You can choose to select/unselect one simply by uncommenting/commenting it.
+# Some modules may import their own submodules, outside of here.
+
+{ ... }: { imports = []; }
