116ba0f5c07f04a3dfbcc7fe4348c4be21c97c20
Atemo-C <c.roux1993@gmail.com>
2026-03-17 17:57:23 +0100

    Added a desktop entry for the SmartPAD RGB editor.

--- extra-modules/atemo_cajaku/packages/midiplussmartpadrgbeditor/package.nix
+++ extra-modules/atemo_cajaku/packages/midiplussmartpadrgbeditor/package.nix
@@ -1,4 +1,10 @@
-{ lib, stdenv, fetchFromGitHub, python3Packages }: python3Packages.buildPythonApplication rec {
+{
+	lib,
+	stdenv,
+	fetchFromGitHub,
+	python3Packages,
+	makeDesktopItem,
+}: python3Packages.buildPythonApplication rec {
 	# Package name.
 	pname = "midiplus-smartpad-rgb-editor";
 
@@ -54,6 +60,19 @@
 			'self.user_data_base_path = os.path.join(os.environ.get("XDG_DATA_HOME", os.path.expanduser("~/.local/share")), "${pname}")'
 	'';
 
+	# Create a .desktop file for the program.
+	desktopItem = makeDesktopItem {
+		desktopName = "MidiPlus SmartPad RGB Editor";
+		genericName = "MidiPlus SmartPAD RGB Editor";
+		name = "MidiPlus SmartPAD RGB Editor";
+		comment = "Desktop application designed to control the RGB LEDs of the MidiPlus SmartPAD.";
+		exec = "smartpad-rgb-editor";
+		icon = "color-picker";
+		categories = [ "Utility" ];
+		terminal = false;
+		keywords = [ "midi" "midiplus" "smartpad" "rgb" "editor" "macro" ];
+	};
+
 	meta = with lib; {
 		# Package description.
 		description = "A desktop application designed to conrtol RGB LEDs of the MidiPlus SmartPad and similar 8×8 MIDI controllers; With development assistance from Google's Gemini 2.5.";
@@ -65,7 +84,7 @@
 		license = licenses.mit;
 
 		# Maintainer of this module.
-		maintainers = [ atemo-c ];
+		maintainers = [ bob ];
 
 		# Platforms this was built and tested on.
 		# This *should* be able to build on some other platforms, but I have not tried.
