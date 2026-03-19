421a791b02f616a1aa8383499e805d892fa9e353
Atemo-C <c.roux1993@gmail.com>
2026-03-19 13:00:04 +0100

    No more need for a manual version override.

--- desktop/noctalia-shell.nix
+++ desktop/noctalia-shell.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }: {
 	# Whether to install the Noctalia shell,
 	# a sleek and minimal desktop shell thoughtfully crafted for Wayland, built with Quickshell.
-	programs.noctalia-shell.install = true;
+	environment.systemPackages = with pkgs; [ noctalia-shell ];
 
 	# Link Noctalia's configuration directory.
 	systemd.user.tmpfiles.users.${config.user.name}.rules = [
