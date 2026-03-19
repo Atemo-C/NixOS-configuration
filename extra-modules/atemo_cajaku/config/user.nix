eb3ce0bbe08ad36a26620a69c813fc00dc5e284a
Atemo-C <c.roux1993@gmail.com>
2026-03-19 23:34:06 +0100

    Fixed wrong syntax and updated/created screenshot module.

--- extra-modules/atemo_cajaku/config/user.nix
+++ extra-modules/atemo_cajaku/config/user.nix
@@ -1,11 +1,11 @@
 { lib, ... }: { options.user = {
-	name = lib.mkOption = {
+	name = lib.mkOption {
 		type = lib.types.str;
 		default = "user-name";
 		description = "The name of the user account. [a-Z] [0-9] [-_]";
 	};
 
-	description = lib.mkOption = {
+	description = lib.mkOption {
 		type = lib.types.str;
 		default = "Fancy user name";
 		description = "The description (title, fancier name) of the user.";
---
+++ extra-modules/atemo_cajaku/programs/niri-screenshot.nix
@@ -0,0 +1,7 @@
+{ config, lib, pkgs, ... }: let cfg = config.programs.niri-screenshot; in {
+	meta.maintainers = with lib.maintainers; [ atemo-c ];
+
+	options.programs.niri-screenshot.enable = lib.mkEnableOption "the niri-screenshot utility, a small shell script to enhance Niri's built-in screenshot utility.";
+
+	config.imports = lib.optional cfg.enable ../packages/niri-screenshot.nix;
+}
