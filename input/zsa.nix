83366cfe0215e9ade7d700f2fbe1d74339d9c8f6
Atemo-C <c.roux1993@gmail.com>
2026-03-20 10:49:05 +0100

    Conditional installation of ZSA utilities.

--- input/zsa.nix
+++ input/zsa.nix
@@ -1,14 +1,14 @@
-{ pkgs, ... }: {
-	# Whether to enable udev rules for keyboards from ZSA;
+{ config, lib, pkgs, ... }: rec {
+	# Enable udev rules for keyboards from ZSA;
 	# Like the ErgoDox EZ, Plank EZ, Moonlander Mark I, or Voyager.
 	# The programs in this module require this to be enabled to work properly.
 	hardware.keyboard.zsa.enable = true;
 
-	environment.systemPackages = with pkgs; [
+	environment.systemPackages = lib.optionals hardware.keyboard.zsa.enable (with pkgs; [
 		# Graphical flashing and utility program for ZSA keyboards.
 		keymapp
 
 		# CLI flashing utility for ZSA keyboards.
 		wally-cli
-	];
+	]);
 }
