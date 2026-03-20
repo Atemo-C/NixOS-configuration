839b981694e821ca1a07f717e4390bb98f8df449
Atemo-C <c.roux1993@gmail.com>
2026-03-20 10:46:21 +0100

    Some comment tweaks.

--- input/input-settings.nix
+++ input/input-settings.nix
@@ -1,22 +1,17 @@
-# This module configures input settings for various input devices, notably keyboard and mice.
 { ... }: { services = {
 	libinput = {
-		# Set the mouse pointer acceleration profile to the given one.
-		# adaptive | flat | custom
+		# Disable pointer acceleration for mices.
 		mouse.accelProfile = "flat";
 
 		touchpad = {
-			# Set the touchpad pointer acceleration profile to the given one.
-			# adaptive | flat | custom
+			# Disable pointer acceleration for touchpads.
 			accelProfile = "flat";
 
-			# Whether to enable tap-to-click behavior.
+			# Enable tap-to-click behavior.
 			tapping = true;
 		};
 	};
 
-	# Delay in milliseconds before a held-down key repeats;
-	# Rate in characters per second to repeat when holding down a key.
 	xserver = {
 		# Delay in milliseconds before a held-down key repeats.
 		autoRepeatDelay = 224;
