f4f94b2cf4ac9f403d0827ec655118a4b2845fc6
Atemo-C <c.roux1993@gmail.com>
2026-04-29 22:16:14 +0200

    Fixes and updates (the READMEs will have to be updated).

--- uncategorized/audio.nix
+++ uncategorized/audio.nix
@@ -1,15 +1,17 @@
-{ config, lib, ... }: {
-	services.pipewire = {
+{ config, lib, ... }: lib.mkMerge [
+	{ services.pipewire = {
 		enable = true;
+
 		alsa = {
 			enable = true;
-			support32Bti = true;
-			jack.enable = true;
-			pulse.enable = true;
+			support32Bit = true;
 		};
-	};
 
-	config = lib.mkIf config.services.pipewire.enable {
+		jack.enable = true;
+		pulse.enable = true;
+	}; }
+
+	(lib.mkIf config.services.pipewire.enable {
 		services.playerctld.enable = true;
 		security.rtkit.enable = true;
 
@@ -21,42 +23,39 @@
 			pwvucontrol.enable = true;
 			qpwgraph.enable = true;
 			soundfont.fluid.enable = true;
+
+			fish.shellAbbrs = (
+				let
+					mpv-ytb = ''mpv --quiet --no-video "https://www.youtube.com/watch?v='';
+
+					streams = [
+						{ name = "lofi-asian";      id = "Na0w3Mz46GA"; }
+						{ name = "lofi-escape";     id = "S_MOd40zlYU"; }
+						{ name = "lofi-guitar";     id = "E_XmwjgRLz8"; }
+						{ name = "lofi-jazz";       id = "HuFYqnbVbzY"; }
+						{ name = "lofi-medieval";   id = "IxPANmjPaek"; }
+						{ name = "lofi-piano";      id = "TtkFsfOP9QI"; }
+						{ name = "lofi-pomodoro";   id = "1oDrJba2PSs"; }
+						{ name = "lofi-pov";        id = "uFlzUaisbig"; }
+						{ name = "lofi-purr";       id = "xORCbIptqcc"; }
+						{ name = "lofi-rain";       id = "-OekvEFm1lo"; }
+						{ name = "lofi-sad";        id = "P6Segk8cr-c"; }
+						{ name = "lofi-sleep";      id = "28KRPhVzCus"; }
+						{ name = "lofi-study";      id = "jfKfPfyJRdk"; }
+						{ name = "lofi-summer";     id = "SXySxLgCV-8"; }
+						{ name = "lofi-synthwave";  id = "4xDzrJKXOOY"; }
+						{ name = "nightinthewoods"; id = "AsLKfqA73uE"; }
+						{ name = "tunic";           id = "gzWd5hjcaPo"; }
+					];
+
+					abbrs = builtins.listToAttrs (
+						map (stream: {
+							name = stream.name;
+							value = ''${mpv-ytb}${stream.id}"'';
+						}) streams
+					);
+				in abbrs
+			);
 		};
-
-		fish.shellAbbrs = (
-			let
-				# Base MPV command.
-				mpv-ytb = ''mpv --quiet --no-video "https://www.youtube.com/watch?v='';
-
-				# List of audio streams.
-				streams = [
-					{ name = "lofi-asian";      id = "Na0w3Mz46GA"; }
-					{ name = "lofi-escape";     id = "S_MOd40zlYU"; }
-					{ name = "lofi-guitar";     id = "E_XmwjgRLz8"; }
-					{ name = "lofi-jazz";       id = "HuFYqnbVbzY"; }
-					{ name = "lofi-medieval";   id = "IxPANmjPaek"; }
-					{ name = "lofi-piano";      id = "TtkFsfOP9QI"; }
-					{ name = "lofi-pomodoro";   id = "1oDrJba2PSs"; }
-					{ name = "lofi-pov";        id = "uFlzUaisbig"; }
-					{ name = "lofi-purr";       id = "xORCbIptqcc"; }
-					{ name = "lofi-rain";       id = "-OekvEFm1lo"; }
-					{ name = "lofi-sad";        id = "P6Segk8cr-c"; }
-					{ name = "lofi-sleep";      id = "28KRPhVzCus"; }
-					{ name = "lofi-study";      id = "jfKfPfyJRdk"; }
-					{ name = "lofi-summer";     id = "SXySxLgCV-8"; }
-					{ name = "lofi-synthwave";  id = "4xDzrJKXOOY"; }
-					{ name = "nightinthewoods"; id = "AsLKfqA73uE"; }
-					{ name = "tunic";           id = "gzWd5hjcaPo"; }
-				];
-
-				# Generate the full abbreviations.
-				abbrs = builtins.listToAttrs (
-					map (stream: {
-						name = stream.name;
-						value = ''${mpv-ytb}${stream.id}"'';
-					}) streams
-				);
-			in abbrs
-		);
-	};
-}
+	}; )
+]
--- uncategorized/boot.nix
+++ uncategorized/boot.nix
@@ -1,37 +1,38 @@
-{ config, lib, pkgs, ... }: {
-	boot = {
-		initrd.systemd.enable = true;
-		kernelPackages = pkgs.linuxPackages_zen;
-		kernelParams = [ "preempt=full" ];
+{ config, lib, pkgs, ... }: lib.mkMerge [
+	{
+		boot = {
+			initrd.systemd.enable = true;
+			kernelPackages = pkgs.linuxPackages_zen;
+			kernelParams = [ "preempt=full" ];
 
-		loader = {
-			limine = {
-				enable = true;
-				maxGenerations = 64;
+			loader = {
+				limine = {
+					enable = true;
+					maxGenerations = 64;
+				};
+
+				timeout = 1;
 			};
 
-			timeout = 1;
+			plymouth = {
+				enable = true;
+				theme = "deus_ex";
+				themePackages = with pkgs; [
+					(adi1090x-plymouth-themes.override { selected_themes = [ "deus_ex" ]; })
+				];
+			};
 		};
 
-		plymouth = {
-			enable = true;
-			theme = "deus_ex";
-			themePackages = with pkgs; [
-				(adi1090x-plymouth-themes.override { selected_themes = [ "deus_ex" ]; })
-			];
-		};
-	};
+		programs.efibootmgr.enable = lib.mkIf config.boot.loader.limine.efiSupport true;
+	}
 
-	services.kmscon = {
-		enable = false;
-		extraConfig = "mouse";
-	};
-
-	programs.efibootmgr.enable = lib.mkIf config.boot.loader.limine.efiSupport true;
-
-	config.boot = lib.mkIf config.boot.plymouth.enable {
+	(lib.mkIf config.boot.plymouth.enable { boot = {
 		consoleLogLevel = 3;
 		initrd.verbose = false;
-		kernelParams = [ "quiet" "udev.log_level=3" "systemd.show_status=auto" ];
-	};
-}
+		kernelParams = [
+			"quiet"
+			"udev.log_level=3"
+			"systemd.show_status=auto"
+		];
+	}; })
+]
