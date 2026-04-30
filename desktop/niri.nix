252bd217b35a61233404c4c17b2554b5dbf7b814
Atemo-C <c.roux1993@gmail.com>
2026-04-30 16:51:01 +0200

    Overhauled the Nix-settings module and insignificant tweak to Niri.

--- desktop/niri.nix
+++ desktop/niri.nix
@@ -1,12 +1,12 @@
 { config, lib, ... }: lib.mkMerge [
-	{programs.niri = {
+	{ programs.niri = {
 		enable = true;
 		linkConfiguration = true;
-	};}
+	}; }
 
 	(lib.mkIf config.programs.niri.enable { programs = {
 		fish.shellAbbrs.n = "niri-session";
-		niri-screenshot.enable = true;
+		niri-screensot.enable = true;
 
 		noctalia-shell = {
 			enable = true;
--- uncategorized/nix-settings.nix
+++ uncategorized/nix-settings.nix
@@ -1,23 +1,17 @@
-{ config, lib, ... }: {
+{ config, lib, ... }: lib.mkMerge [
+{
 	nix.settings = {
 		auto-optimise-store = true;
 		download-buffer-size = 629145600;
-		experimental-features = [ "nix-command" ];
+		experiemntal-features = [ "nix-command" ];
 	};
 
 	programs = {
 		hydra-check.enable = true;
 		nix-output-monitor.enable = true;
 
-		fish.shellAbbrs = rec {
-			nix-test = ''set -x CURRENTDIR $(pwd) && cd /tmp/ && run0 nixos-rebuild test --log-format internal-json -v 2>&1 | nom --json; cd "$CURRENTDIR"'';
-			nix-update-now = "run0 nixos-rebuild switch --log-format internal-json -v 2>&1 | nom --json";
-			nix-update-boot = "run0 nixos-rebuild boot --log-format internal-json -v 2>&1 | nom --json";
-			nix-upgrade-now = "run0 nixos-rebuild switch --upgrade --log-format internal-json -v 2>&1 | nom --json";
-			nix-upgrade-boot = "run0 nixos-rebuild boot --upgrade --log-format internal-json -v 2>&1 | nom --json";
-			nix-clean = "run0 nix-collect-garbage -d --log-format internal-json -v 2>&1 | nom --json && nix-collect-garbage -d --log-format internal-json -v 2>&1 | nom --json";
+		fish.shellAbbrs = {
 			nix-list-generation = "run0 nixos-rebuild list -generations";
-			mk-nixiso = "run0 nix-build '<nixpkgs/nixos>' -A config.system.build.isoImage -I nixos-config=iso.nix --log-format internal-json -v 2>&1 | nom --json";
 			rm-nixiso = "run0 nix-store --delete --ignore-liveness $(readlink -f result/)";
 			download-nixos = ''wget -v "https://channels.nixos.org/nixos-unstable/latest-nixos-graphical-x86_64-linux.iso'';
 			download-nixos-minimal = ''wget -v "https://channels.nixos.org/nixos-unstable/latest-nixos-minimal-x86_64-linux.iso'';
@@ -26,9 +20,31 @@
 
 	security = {
 		sudo.enable = false;
-		run0.enableSudoAlias = lib.mkIf (! config.security.sudo.enable) true;
+		run0.enableSudoAlias = lib.mkIf (!config.security.sudo.enable) true;
 	};
 
 	system.stateVersion = "26.05";
 	systemd.tmpfiles.rules = [ "Z /etc/nixos 0770 root wheel - -" ];
 }
+
+(if config.programs.nix-output-monitor.enable
+	then programs.fish.shellAbbrs = {
+		nix-test = ''set -x CURRENTDIR $(pwd) && cd /tmp/ && run0 nixos-rebuild test --log-format internal-json -v 2>&1 | nom --json; cd "$CURRENTDIR"'';
+		nix-update-now = "run0 nixos-rebuild switch --log-format internal-json -v 2>&1 | nom --json";
+		nix-update-boot = "run0 nixos-rebuild boot --log-format internal-json -v 2>&1 | nom --json";
+		nix-upgrade-now = "run0 nixos-rebuild switch --upgrade --log-format internal-json -v 2>&1 | nom --json";
+		nix-upgrade-boot = "run0 nixos-rebuild boot --upgrade --log-format internal-json -v 2>&1 | nom --json";
+		nix-clean = "run0 nix-collect-garbage -d --log-format internal-json -v 2>&1 | nom --json && nix-collect-garbage -d --log-format internal-json -v 2>&1 | nom --json";
+		mk-nixiso = "run0 nix-build '<nixpkgs/nixos>' -A config.system.build.isoImage -I nixos-config=iso.nix --log-format internal-json -v 2>&1 | nom --json";
+	}
+
+	else programs.fish.shellAbbrs = {
+		nix-update-now = "run0 nixos-rebuild switch --log-format bar-with-logs";
+		nix-update-boot = "run0 nixos-rebuild boot --log-format bar-with-logs";
+		nix-upgrade-now = "run0 nixos-rebuild switch --upgrade --log-format bar-with-logs";
+		nix-upgrade-boot = "run0 nixos-rebuild boot --upgrade --log-format bar-with-logs";
+		nix-clean = "run0 nix-collect-garbage -d && nix-collect-garbage -d";
+		mk-nixiso = "run0 nix-build '<nixpkgs/nixos>' -A config.system.build.isoImage -I nixos-config=configuration.nix --log-format bar-with-logs"
+	};
+)
+]
