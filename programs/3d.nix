5b086e5fd00c131a6944c911521dc8a6fa913aed
Atemo-C <c.roux1993@gmail.com>
2026-07-16 19:13:50 +0200

    Removed temporary fixes.

--- programs/3d.nix
+++ programs/3d.nix
@@ -1,15 +1,8 @@
-{ pkgs, ... }: {
-	environment.systemPackages = with pkgs; [
-		# 3D creation, animation, and publishing system.
-		blender
+{ pkgs, ... }: { environment.systemPackages = with pkgs; [
+	# 3D creation, animation, and publishing system.
+	blender
 
-		# Fast and minimalist 3D viewer using VTK.
-		# Is used by some thumbnailes to generate thumbnails of 3D files.
-#		f3d
-	];
-
-	# Temporary applying https://github.com/NixOS/nixpkgs/pull/541146
-	nixpkgs.overlays = [(final: prev: {
-		pdal = final.callPackage ./pdal/package.nix {};
-	})];
-}
+	# Fast and minimalist 3D viewer using VTK.
+	# Is used by some thumbnailes to generate thumbnails of 3D files.
+	f3d
+]; }
