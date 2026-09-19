07845230d1282776ce212fe4f374c281bd5b57c1
Atemo-C <c.roux1993@gmail.com>
2026-09-19 14:30:46 +0200

    Updated two comments in the README to make things clearer.

--- configuration.nix
+++ configuration.nix
@@ -7,7 +7,7 @@
 
 	# This module allows defining which GPU your system is using.
 	# `hardware.activeGpu` should be set in your device's `gpu.nix` module,
-	# at `/etc/nixos/computers/<your-computer/gpu.nix`, and this module
+	# at `/etc/nixos/computers/<your-computer/gpu.nix`, and the `gpu.nix` module
 	# should be imported in your device's `settings.nix` module.
 	./extra-modules/config/gpu-check.nix
 
@@ -19,7 +19,7 @@
 
 	# This module applies your keyboard layout settings globally.
 	# Your keyboard layout should be set in your device's `input.nix` module,
-	# at `/etc/nixos/computers/<your-comupter>/input.nix`, and this module
+	# at `/etc/nixos/computers/<your-comupter>/input.nix`, and the `input.nix` module
 	# should be imported in your device's `settings.nix` module.
 	./input/keyboard-layout.nix
 
